a3169b9055a9f0ea9d459e7d08edf0a7550ff52f
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-06-26 02:13:24 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/pensa/pens-27k8271x.js
@@ -0,0 +1,32 @@
+$_GET({
+  title: 'Pensa 27',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Iūlius colōnō imperat ut mercēdem solv<div class="area" data-key="at"></div>. Ille dominum ōrat ut patientiam habe<div class="area" data-key="at"></div>: “Nōlī postulāre ut tantam pecūniam statim solv<div class="area" data-key="am"></div>!” Dominus colōnō imperat ut tace<div class="area" data-key="at"></div> et surg<div class="area" data-key="at"></div>, tum “Prīmum cūrā,” inquit, “ut uxor et līberī vale<div class="area" data-key="ant"></div>, tum vērō cūrā ut agrōs bene col<div class="area" data-key="ās"></div> et mercēdem solv<div class="area" data-key="ās"></div>!”</div>
+      <div><span style="white-space: pre;">	</span>Dominus colōnōs monet ut labōr<div class="area" data-key="ent"></div> nēve quiēsc<div class="area" data-key="ant"></div>: “Moneō vōs ut labōr<div class="area" data-key="ētis"></div> nēve quiēsc<div class="area" data-key="ātis"></div>!”</div>
+      <div><span style="white-space: pre;">	</span>Māter fīliam monet ut cauta <div class="area" data-key="sit"></div>: “Moneō tē ut cauta <div class="area" data-key="sīs"></div>!” Fābula nōs monet nē temerāriī <div class="area" data-key="sīmus"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Verba: spargere <div class="area" data-key="spars"></div>isse <div class="area" data-key="spars"></div>um; rapere <div class="area" data-key="rapu"></div>isse <div class="area" data-key="rapt"></div>um; neglegere <div class="area" data-key="neglēx"></div>isse <div class="area" data-key="neglēct"></div>um; dēsinere <div class="area" data-key="dēsi"></div>isse; quiēscere <div class="area" data-key="quiēv"></div>isse; crēscere <div class="area" data-key="crēv"></div>isse; poscere <div class="area" data-key="poposc"></div>isse; prōdesse <div class="area" data-key="prōfu"></div>isse.</div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Mēnse Augustō <div class="area" data-key="frūmentum"></div> metitur, deinde <div class="area" data-key="agrī"></div> arantur et novum frūmentum <div class="area" data-key="seritur"></div>. Agricola quī <div class="area" data-key="arat"></div> post arātrum ambulat duōs bovēs <div class="area" data-key="prae"></div> sē agēns; arātrum est <div class="area" data-key="īnstrūmentum"></div> quō agrī arantur. Agricola quī serit nūllō īnstrūmentō <div class="area" data-key="ūtitur"></div> et sēmen manū <div class="area" data-key="spargit"></div>. Quī <div class="area" data-key="serit"></div> falce ūtitur. Agricola est vir cuius <div class="area" data-key="negōtium"></div> est agrōs <div class="area" data-key="colere"></div>. Ex Aegyptō, quae terra <div class="area" data-key="fertilis"></div> est, magna <div class="area" data-key="cōpia"></div> frūmentī in Italiam <div class="area" data-key="invehitur"></div>. Frūgēs vīneārum sunt <div class="area" data-key="ūvae"></div>, ex quibus <div class="area" data-key="vīnum"></div> efficitur. <div class="area" data-key="calor"></div> sōlis vīneīs <div class="area" data-key="prōdest"></div>, <div class="area" data-key="frīgus"></div> vīneīs <div class="area" data-key="nocet"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Iūlius in <div class="area" data-key="praediō"></div> suō Albānō nōn labōrat, sed <div class="area" data-key="quiēscit"></div>. <div class="area" data-key="rūs"></div> quiētum et <div class="area" data-key="amoenum"></div> eum dēlectat. Colōnus aliēnōs agrōs <div class="area" data-key="prō"></div> dominō absentī colit.</div>
+      <div><span style="white-space: pre;">	</span>Pāstor est vir quī pecus <div class="area" data-key="pāscit"></div> et cūrat. <div class="area" data-key="cūra"></div> pecoris magnum negōtium est, nōn <div class="area" data-key="ōtium"></div>, ut pigrī pāstōrēs <div class="area" data-key="cēnsent"></div>. Pāstōrēs nōn tam industriē <div class="area" data-key="labōrant"></div> quam agricolae. Pāstōris officium est cūrāre <div class="area" data-key="nē"></div> ovēs aberrent <div class="area" data-key="nēve"></div> ā lupō rapiantur.</div>
+    </div>
+    <div class="row">
+      <div>Quid est negōtium agricolae?</div>
+      <div>Quandō frūmentum metitur?</div>
+      <div>Num arātor ipse arātrum trahit?</div>
+      <div>Quid est pābulum pecoris?</div>
+      <div>Unde frūmentum in Italiam invehitur?</div>
+      <div>Quae regiō Āfricae fertilissima est?</div>
+      <div>Cūr necesse est agrōs rigāre?</div>
+      <div>Quae sunt frūgēs vīneārum?</div>
+      <div>Num Iūlius ipse in agrīs labōrat?</div>
+      <div>Omnēsne colōnī mercēdem solvērunt?</div>
+      <div>Quot sēstertiōs colōnus Iūliō dēbet?</div>
+      <div>Num uxor colōnī officium suum neglegit?</div>
+      <div>Quid est officium pāstōris?</div>
+      <div>Estne Iūlius dominus inhūmānus?</div>
+    </div>
+  `
+});
