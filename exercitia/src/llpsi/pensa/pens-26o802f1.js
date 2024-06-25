7e9ea9bfb2ca3ee47e58f53108b0228dad47b7bb
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-06-26 01:46:02 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/pensa/pens-26o802f1.js
@@ -0,0 +1,27 @@
+$_GET({
+  title: 'Pensa 26',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Nāvēs necessāriae sunt ad nāvig<div class="area" data-key="um"></div>. Iūlia dēlectātur in hortō ambul<div class="area" data-key="andō"></div> et flōrēs carp<div class="area" data-key="endō"></div>. Mēdus cōnsilium fugi<div class="area" data-key="endī"></div> excōgitāvit; Lȳdia eum iūvit in fugi<div class="area" data-key="endō"></div>. Paulum satis est ad beātē vīv<div class="area" data-key="endum"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Verba: iungere <div class="area" data-key="iūnx"></div>isse <div class="area" data-key="iūnct"></div>um; fīgere <div class="area" data-key="fīx"></div>isse <div class="area" data-key="fīx"></div>um; mergere <div class="area" data-key="mers"></div>isse <div class="area" data-key="mers"></div>um; ūrere <div class="area" data-key="uss"></div>isse <div class="area" data-key="ust"></div>um; movēre <div class="area" data-key="mōv"></div>isse <div class="area" data-key="mōt"></div>um; capere <div class="area" data-key="cēp"></div>isse <div class="area" data-key="capt"></div>um; invenīre <div class="area" data-key="invēn"></div>isse <div class="area" data-key="invent"></div>um; cōnsīdere <div class="area" data-key="cōnsēd"></div>isse; iuvāre <div class="area" data-key="iūv"></div>isse; accidere <div class="area" data-key="accid"></div>isse.</div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Daedalus exitum labyrinthī <div class="area" data-key="invenīre"></div> nōn poterat nec <div class="area" data-key="quisquam"></div> eum in fugiendō <div class="area" data-key="iuvāre"></div> poterat. <div class="area" data-key="quoniam"></div> igitur aliae viae clausae erant, vir <div class="area" data-key="audāx"></div> per āera <div class="area" data-key="effugere"></div> cōnstituit. <div class="area" data-key="cōnsilium"></div> patris fīlium dēlectāvit. Tum Daedalus ālās cōnfēcit ex <div class="area" data-key="pennīs"></div> quās cērā iūnxit et in lacertīs <div class="area" data-key="fīxit"></div>. Postquam hoc <div class="area" data-key="opus"></div> perfēcit, Daedalus fīlium suum <div class="area" data-key="artem"></div> volandī docuit: “<div class="area" data-key="imitāre"></div> mē! Movē ālās <div class="area" data-key="sūrsum"></div> <div class="area" data-key="deorsum"></div> hōc modō! Ars volandī <div class="area" data-key="haud"></div> difficilis est. Sed antequam hinc <div class="area" data-key="ēvolābimus"></div>, hoc tē moneō: nōlī volāre in <div class="area" data-key="īnfimō"></div> āere prope mare nec in <div class="area" data-key="summō"></div> āere prope sōlem. Iam sequere me! <div class="area" data-key="carcerem"></div> nostrum relinquimus, <div class="area" data-key="līberī"></div> sumus!”</div>
+      <div><span style="white-space: pre;">	</span>Īcarus in summum caelum ascendēns nōn sōlum Graeciam, <div class="area" data-key="vērum"></div> etiam Asiam ac <div class="area" data-key="paene"></div> tōtum <div class="area" data-key="orbem"></div> terrārum dēspiciēbat. Tum vērō id quod pater timuerat <div class="area" data-key="accidit"></div>: <div class="area" data-key="ignis"></div> sōlis propinquī cēram <div class="area" data-key="mollīvit"></div> atque pennās <div class="area" data-key="ussit"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Ecce fābula mīrābilis dē puerō <div class="area" data-key="temerāriō"></div> quī <div class="area" data-key="lībertātem"></div> quaerēns mortem invēnit.</div>
+    </div>
+    <div class="row">
+      <div>Quis Daedalum in labyrinthum inclūdī iussit?</div>
+      <div>Quōmodo Daedalus effugere cōnstituit?</div>
+      <div>Cūr hominēs volāre nōn possunt?</div>
+      <div>Ex quibus rēbus Daedalus ālās cōnfēcit?</div>
+      <div>Cūr ālae Īcarum sustinēre nōn poterant?</div>
+      <div>Quid pater et fīlius volantēs vīdērunt?</div>
+      <div>Estne Peloponnēsus magna īnsula?</div>
+      <div>Quārē Īcarus in summum caelum ascendit?</div>
+      <div>Quid tum puerō accidit?</div>
+      <div>Ubi corpus puerī inventum est?</div>
+      <div>Num haec fābula dālectandī causā modo nārrātur?</div>
+    </div>
+  `
+});
