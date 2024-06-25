f9c3ff784c4746362e131729616220c437774c6a
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-06-26 02:33:29 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/pensa/pens-28t215p4.js
@@ -0,0 +1,31 @@
+$_GET({
+  title: 'Pensa 28',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Servus dominum ōrābat nē sē verberā<div class="area" data-key="ret"></div>, sed dominus imperāvit ut tacē<div class="area" data-key="ret"></div> et surg<div class="area" data-key="eret"></div>, tum aliīs servīs imperāvit ut eum prehend<div class="area" data-key="erent"></div> et tenē<div class="area" data-key="rent"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Mēdus ā dominō fūgit, ut amīcam suam vidē<div class="area" data-key="ret"></div> et semper cum eā es<div class="area" data-key="set"></div>. Mēdus: “Ā dominō fūgī, ut amīcam meam vidē<div class="area" data-key="rem"></div> et semper cum eā es<div class="area" data-key="sem"></div>.”</div>
+      <div><span style="white-space: pre;">	</span>Mīnōs imperāvit ut Daedalus et Īcarus in labyrinthum inclūd<div class="area" data-key="erentur"></div>. Īcarus: “Quis imperāvit ut nōs inclūd<div class="area" data-key="erēmur"></div>?” Daedalus: “Mīnōs imperāvit ut ego inclūd<div class="area" data-key="erer"></div> et ut tū mēcum inclūd<div class="area" data-key="erēris"></div>.”</div>
+      <div><span style="white-space: pre;">	</span>Verba: dīvidere <div class="area" data-key="dīvīs"></div>isse <div class="area" data-key="dīvīs"></div>um; ēicere <div class="area" data-key="ēiēc"></div>isse <div class="area" data-key="ēiect"></div>um; prōmere <div class="area" data-key="prōmps"></div>isse <div class="area" data-key="prōmpt"></div>um; vīvere <div class="area" data-key="vīx"></div>isse; discere <div class="area" data-key="didic"></div>isse; persuādēre <div class="area" data-key="persuās"></div>isse; surgere <div class="area" data-key="surrēx"></div>isse.</div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Lȳdia Rōmae vīvere <div class="area" data-key="māvult"></div> quam in Graeciā, sed Mēdus multīs prōmissīs eī <div class="area" data-key="persuāsit"></div> ut sēcum proficīscerētur. In Italiā Mēdus dominō sevērō <div class="area" data-key="serviēbat"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Nāvis nōndum extrā <div class="area" data-key="perīculum"></div> est. Multa perīcula nautīs <div class="area" data-key="impendent"></div>. <div class="area" data-key="praedōnēs"></div> maritimī nāvēs persequuntur; nūllum mare <div class="area" data-key="tūtum"></div> est ā praedōnibus.</div>
+      <div><span style="white-space: pre;">	</span>Hominēs mortālēs nāscuntur et <div class="area" data-key="moriuntur"></div>, diī vērō <div class="area" data-key="immortālēs"></div> sunt. Nūllus deus Rōmānus hominem mortuum ab īnferīs <div class="area" data-key="suscitāre"></div> potest; nē Iuppiter quidem tantam <div class="area" data-key="potestātem"></div> habet, etsī ille deus maximus <div class="area" data-key="habētur"></div>. Trēs diī ūniversum <div class="area" data-key="mundum"></div> inter sē dīvīsērunt.</div>
+      <div><span style="white-space: pre;">	</span>Chrīstus in oppidō Bethlehem <div class="area" data-key="nātus"></div> est. Ille verbīs sōlīs efficiēbat ut hominēs <div class="area" data-key="caecī"></div> vidērent, <div class="area" data-key="surdī"></div> audīrent, <div class="area" data-key="mūtī"></div> loquerentur, <div class="area" data-key="claudī"></div> ambulārent.</div>
+    </div>
+    <div class="row">
+      <div>Fretum Siculum quid est?</div>
+      <div>Ubi nātus est Chrīstus?</div>
+      <div>Quid Iaīrus Chrīstum rogāvit?</div>
+      <div>Quae perīcula nautīs impendent?</div>
+      <div>Cūr nautae praedōnēs maritimōs metuunt?</div>
+      <div>Quālī dominō Mēdus in Italiā serviēbat?</div>
+      <div>Quārē Mēdus ā dominō suō fūgit?</div>
+      <div>Num Lȳdia laetō animō Rōmā profecta est?</div>
+      <div>Quōmodo Mēdus eī persuāsit ut sēcum venīret?</div>
+      <div>Cūr Mēdus nescit quid respondeat?</div>
+      <div>Quem Mēdus accēdere videt?</div>
+      <div>Tūne in Graeciā vīvere māvīs quam in patriā tuā?</div>
+    </div>
+  `
+});
