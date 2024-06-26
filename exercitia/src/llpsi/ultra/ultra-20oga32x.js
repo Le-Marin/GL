24c7806323fa72c3b80a8e07ffd45b70577b4fd1
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-06-26 15:35:23 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-20oga32x.js
@@ -0,0 +1,35 @@
+$_GET({
+  title: 'Ultra 20',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Прописать недостающие формы глаголов, сохраняя данные залог, лицо и число.</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>praeteritum : praesēns : futūrum</i></span></div>
+      <div><br></div>
+      <div><div class="area" data-key="vindicābar"></div> : <div class="area">vindicat</div> : <div class="area" data-key="vindicābit"></div></div>
+      <div><div class="area">movēbam</div> : <div class="area" data-key="moveō"></div> : <div class="area" data-key="movēbō"></div></div>
+      <div><div class="area" data-key="effluēbant"></div> : <div class="area" data-key="effluunt"></div> : <div class="area">effluent</div></div>
+      <div><div class="area">lābēbāris</div> : <div class="area" data-key="lāberis"></div> : <div class="area" data-key="lābēris"></div></div>
+      <div><div class="area" data-key="agēbāmus"></div> : <div class="area">agimus</div> : <div class="area" data-key="agēmus"></div></div>
+      <div><div class="area" data-key="erātis"></div> : <div class="area" data-key="estis"></div> : <div class="area">eritis</div></div>
+      <div><div class="area" data-key="iūdicābās"></div> : <div class="area">iūdicās</div> : <div class="area" data-key="iūdicābis"></div></div>
+      <div><div class="area">vehēbar</div> : <div class="area" data-key="vehor"></div> : <div class="area" data-key="vehar"></div></div>
+      <div><div class="area" data-key="accipiēbat"></div> : <div class="area" data-key="accipit"></div> : <div class="area">accipiet</div></div>
+      <div><div class="area">trānscurrēbant</div> : <div class="area" data-key="trānscurrunt"></div> : <div class="area" data-key="trānscurrent"></div><br><br></div>
+    </div>
+    <div class="row">
+      <div><b>2. Изменить глаголы, данные в настоящем времени, на глаголы в будущем.</b></div>
+      <div><br></div>
+      <div>1. Deus Nōēmō: “<div class="area" data-key="pōam">Pōnō</div> arcum meum in nūbibus, et <div class="area" data-key="erit">est</div> signum quod faciō vōbīscum.”</div>
+      <div>2. Milerēs Sabīnae: “Melius <div class="area" data-key="perībimus">perīmus</div> quam sine alterīs vestrum <div class="area" data-key="vīvēmus">vīvimus</div>!”</div>
+      <div>3. Tālis arx ab hostibus nunquam <div class="area" data-key="expugnābitur">expugnātur</div>.</div>
+      <div>4. Serva: “Iuvāte mē, et viam vōbīs <div class="area" data-key="mōnstrābō">mōnstrō</div> quā in urbem clam penetrāre <div class="area" data-key="poteritis">potestis</div>.”</div>
+      <div>5. Epistulā acceptā, uxor statim respōnsum <div class="area" data-key="scrībet">scrībit</div>.</div>
+      <div>6. Quae hodiē tantō cōnstant crās bis tantō <div class="area" data-key="cōnstābunt">cōnstant</div>.</div>
+      <div>7. Māter Achillī: “Sī in Graeciam <div class="area" data-key="ībis">īs</div>, magnam <div class="area" data-key="inveniēs">invenīs</div> glōriam, sed vītam <div class="area" data-key="āmittēs">āmittis</div>.</div>
+      <div>8. Mīlitēs ducī: “Ubīque tē <div class="area" data-key="sequēmur">sequimur</div>; dūce nōs quō <div class="area" data-key="vocābit">vocat</div> officium.”</div>
+      <div>9. Victī, hostēs ad currum <div class="area" data-key="ligābuntur">ligantur</div> et per mediam urbem <div class="area" data-key="trahentur">trahuntur</div>.</div>
+      <div>10. Gubernātor: “Sī <div class="area" data-key="oriētur">oritur</div> tempestās et mare, quod nunc serēnum est, turbidum <div class="area" data-key="fīet">fit</div>, nāvis mea in portū <div class="area" data-key="manēbit">manet</div>.”</div>
+    </div>
+  `
+});
