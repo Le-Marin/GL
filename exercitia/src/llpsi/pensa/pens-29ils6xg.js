019ff4e0c8739acdaa3d451ee11e3d429810822e
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-06-30 01:52:31 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/pensa/pens-29ils6xg.js
@@ -0,0 +1,29 @@
+$_GET({
+  title: 'Pensa 29',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Magister puerōs monet <div class="area" data-key="ut"></div> pulchrē scrīb<div class="area" data-key="ant"></div>. Sextus tam pulchrē scrībit <div class="area" data-key="ut"></div> magister eum laud<div class="area" data-key="et"></div>. Magister ipse calamum sūmit, <div class="area" data-key="ut"></div> litterās scrīb<div class="area" data-key="at"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Daedalus ālās cōnfēcit <div class="area" data-key="ut"></div> ē labyrinthō ēvol<div class="area" data-key="āret"></div>. Īcarus tam altē volāvit <div class="area" data-key="ut"></div> sōlī appropinqu<div class="area" data-key="āret"></div>, quamquam pater eum monuerat <div class="area" data-key="nē"></div> temerārius es<div class="area" data-key="set"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Herī Quīntus arborem ascendit, <div class="area" data-key="ut"></div> nīdum quaer<div class="area" data-key="eret"></div>, etsī pater eum monuerat <div class="area" data-key="ut"></div> cautus es<div class="area" data-key="set"></div>. Medicus Quīntō imperāvit <div class="area" data-key="ut"></div> oculōs claud<div class="area" data-key="eret"></div>, <div class="area" data-key="nē"></div> cultrum medicī vid<div class="area" data-key="ēret"></div>. Quīntus tam pallidus erat <div class="area" data-key="ut"></div> Syra eum mortuum esse put<div class="area" data-key="āret"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Sōl ita lūcēbat <div class="area" data-key="ut"></div> pāstor umbram pet<div class="area" data-key="eret"></div>, <div class="area" data-key="nē"></div> in sōle ambul<div class="area" data-key="āret"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Tantus atque tālis deus est Iuppiter <div class="area" data-key="ut"></div> Optimus Maximus appell<div class="area" data-key="ētur"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Verba: vehere <div class="area" data-key="vēx"></div>isse <div class="area" data-key="vect"></div>um; pōnere <div class="area" data-key="posu"></div>isse <div class="area" data-key="posit"></div>um; āmittere <div class="area" data-key="āmīs"></div>isse <div class="area" data-key="āmiss"></div>um; allicere <div class="area" data-key="allēx"></div>isse <div class="area" data-key="allect"></div>um; ēripere <div class="area" data-key="ēripu"></div>isse <div class="area" data-key="ērept"></div>um; secāre <div class="area" data-key="secu"></div>isse <div class="area" data-key="sect"></div>um; suādēre <div class="area" data-key="suās"></div>isse; dēsilīre <div class="area" data-key="dēsilu"></div>isse; canere <div class="area" data-key="cecin"></div>isse; crēdere <div class="area" data-key="crēdid"></div>isse; cōnfitērī <div class="area" data-key="cōnfess"></div>um esse; lābī <div class="area" data-key="lāps"></div>um esse.</div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Orpheus, fidicen <div class="area" data-key="nōbilis"></div>, tam pulchrē canēbat ut ferae <div class="area" data-key="appropinquārent"></div> ac fluviī <div class="area" data-key="rapidī"></div> cōnsisterent. Etiam ad <div class="area" data-key="Īnferōs"></div> dēscendit, ut <div class="area" data-key="inde"></div> uxōrem suam <div class="area" data-key="redūceret"></div>. Nēmō tam <div class="area" data-key="ignārus"></div> est ut Orpheum ignōret.</div>
+      <div><span style="white-space: pre;">	</span>Arīōn quoque omnibus <div class="area" data-key="nōtus"></div> est. Cum ille magnās <div class="area" data-key="dīvitiās"></div> sēcum in nāve habēret, nautae pauperēs hominī dīvitī <div class="area" data-key="invidēbant"></div> eumque necāre cōnstituērunt. Arīōn, cum <div class="area" data-key="vītam"></div> suam in perīculō esse sentīret, pecūniam nautīs <div class="area" data-key="dōnāvit"></div> eōsque ōrāvit ut sibi <div class="area" data-key="parcerent"></div>. Precibus <div class="area" data-key="permōtī"></div> nautae eī permīsērunt ut ante mortem <div class="area" data-key="carmen"></div> caneret. Hōc factō, Arīōn in mare <div class="area" data-key="dēsiluit"></div>; sed delphīnus eum in <div class="area" data-key="dorsō"></div> sedentem ad lītus vēxit. Ita ille servātus est, cum iam salūtem <div class="area" data-key="dēspērāret"></div>. Nautae, cum Arīonem <div class="area" data-key="appārēre"></div> vidērent, <div class="area" data-key="maleficium"></div> suum cōnfessī sunt.</div>
+    </div>
+    <div class="row">
+      <div>Quōmodo mercātōrēs lucrum faciunt?</div>
+      <div>Cūr mercātor Rōmānus trīstis est?</div>
+      <div>Quārē mercēs ēiectae sunt?</div>
+      <div>Quid mercātor deōs precātur?</div>
+      <div>Quārē ad Īnferōs dēscendit Orpheus?</div>
+      <div>Num nautae Arīonem gladiīs interfēcērunt?</div>
+      <div>Quōmodo Arīōn servātus est?</div>
+      <div>Quid nōs monet haec fābula?</div>
+      <div>Cūr Polycratēs ānulum suum abiēcit?</div>
+      <div>Ubi ānulus eius inventus est?</div>
+    </div>
+  `
+});
---
+++ exercitia/src/llpsi/pensa/pens-30bpsm95.js
@@ -0,0 +1,27 @@
+$_GET({
+  title: 'Pensa 30',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Syra: “Iam dormī, Quīnte! Cum bene dormīv<div class="area" data-key="eris"></div>, valēbis.” Quīntus: “Nōn dormiam antequam tū mihi fābulam nārrāv<div class="area" data-key="eris"></div>. Cum fābulam audīv<div class="area" data-key="erō"></div>, bene dormiam. Cum bene dormīv<div class="area" data-key="erō"></div>, brevī sānus erō, nisi medicus mē necāv<div class="area" data-key="erit"></div>!”</div>
+      <div><span style="white-space: pre;">	</span>Patria salva erit sī mīlitēs nostrī fortiter pugnāv<div class="area" data-key="erint"></div>. Dux: “Nisi vōs fortiter pugnāv<div class="area" data-key="eritis"></div>, mīlitēs, hostēs castra nostra expugnābunt.” Mīlitēs: “Num quid nōbīs dabitur sī fortiter pugnāv<div class="area" data-key="erimus"></div>?”</div>
+      <div><span style="white-space: pre;">	</span>Verba: induere <div class="area" data-key="indu"></div>isse <div class="area" data-key="indūt"></div>um; ēligere <div class="area" data-key="ēlēg"></div>isse <div class="area" data-key="ēlēct"></div>um; coquere <div class="area" data-key="cox"></div>isse <div class="area" data-key="coct"></div>um; sternere <div class="area" data-key="strāv"></div>isse <div class="area" data-key="strāt"></div>um; fundere <div class="area" data-key="fūd"></div>isse <div class="area" data-key="fūs"></div>um; miscēre <div class="area" data-key="miscu"></div>isse <div class="area" data-key="mixt"></div>um; exhaurīre <div class="area" data-key="exhausī"></div>isse <div class="area" data-key="exhaust"></div>um; revertī <div class="area" data-key="revert"></div>isse / <div class="area" data-key="revers"></div>um esse; ūtī <div class="area" data-key="ūs"></div>um esse.</div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Iūlius Cornēlium et Orontem, amīcōs et <div class="area" data-key="hospitēs"></div> suōs, cum uxōribus ad <div class="area" data-key="cēnam"></div> vocāvit. Cum hospitēs veniunt, Iūlius in <div class="area" data-key="balneō"></div> lavātur. Aemilia eōs <div class="area" data-key="salvēre"></div> iubet et marītum suum <div class="area" data-key="tardum"></div> excūsat. Cornēlius ad vīllam suam reversus ōtiō <div class="area" data-key="fruitur"></div>. Orontēs, quī ex longō <div class="area" data-key="itinere"></div> revertit, vītam rūsticam nōn <div class="area" data-key="iūcundam"></div>, sed <div class="area" data-key="molestam"></div> esse cēnset. Hospitēs in ātriō exspectant, dum cibus <div class="area" data-key="parātur"></div>. Servus quī in <div class="area" data-key="culīnā"></div> cibum coquit <div class="area" data-key="cocus"></div> appellātur. In <div class="area" data-key="triclīniō"></div> sunt trēs lectī: lectus summus, medius, <div class="area" data-key="īmus"></div>; in singulīs lectīs <div class="area" data-key="singulī"></div> aut <div class="area" data-key="bīnī"></div> aut ternī <div class="area" data-key="convīvae"></div> accubant. Rōmānī in lectīs cubantēs <div class="area" data-key="cēnant"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Tandem puer ‘cēnam parātam esse’ <div class="area" data-key="nūntiat"></div>. In mēnsā sunt <div class="area" data-key="vāsa"></div> et pōcula ex <div class="area" data-key="argentō"></div> facta. Cibus omnibus <div class="area" data-key="placet"></div>, maximē autem <div class="area" data-key="carō"></div> laudātur. <div class="area" data-key="ministrī"></div> vīnum in pōcula <div class="area" data-key="fundunt"></div>. Orontēs vīnum merum <div class="area" data-key="pōtat"></div>, cēterī convīvae aquam vīnō <div class="area" data-key="miscent"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Sine cibō homō <div class="area" data-key="diū"></div> vīvere potest, sine aquā <div class="area" data-key="paulisper"></div> tantum. <div class="area" data-key="famēs"></div> mala rēs est, sed multō peior est <div class="area" data-key="sitis"></div>. Ex malīs minimum <div class="area" data-key="ēligere"></div> oportet.</div>
+    </div>
+    <div class="row">
+      <div>Quī sunt Cornēlius et Orontēs?</div>
+      <div>Ubi est Iūlius cum hospitēs adveniunt?</div>
+      <div>Quid est balneum?</div>
+      <div>Nōnne iūcunda est vīta rūstica?</div>
+      <div>Quid est cocī negōtium?</div>
+      <div>Num Rōmānī in sellīs sedentēs cēnant?</div>
+      <div>Quot lectī sunt in triclīniō?</div>
+      <div>Quot convīvae in singulīs lectīs accubant?</div>
+      <div>Ex quā māteriā pōcula et vāsa facta sunt?</div>
+      <div>Tūne vīnum aquā calidā mixtum bibis?</div>
+    </div>
+  `
+});
