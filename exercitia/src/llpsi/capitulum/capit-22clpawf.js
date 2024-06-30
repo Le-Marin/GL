2a703de367b29fff3adc2251c410add6507650e1
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-06-30 23:26:25 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/capitulum/capit-22clpawf.js
@@ -0,0 +1,99 @@
+$_GET({
+  title: 'Exercitia Latina 22',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>I</div>
+      <div><br></div>
+      <div><b>Exercitium 1</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-eus</div> <div class="area">-eua</div> <div class="area">-eum</div></div></div>
+      <div><br></div>
+      <div>1. Ānulus ex aurō factus = ānulus aur<div class="area" data-key="eus"></div></div>
+      <div>2. Baculum ē lignō factum = baculum lign<div class="area" data-key="eum"></div></div>
+      <div>3. Catēna ex ferrō facta = catēna ferr<div class="area" data-key="ea"></div></div>
+      <div>4. Nummī ex aurō factī = nummī aur<div class="area" data-key="eī"></div></div>
+      <div>5. Stilus ferreus = stilus <div class="area" data-key="ex"></div> <div class="area" data-key="ferrō"></div> <div class="area" data-key="factus"></div></div>
+      <div>6. Pōculum aureum = pōculum <div class="area" data-key="ex"></div> <div class="area" data-key="aurō"></div> <div class="area" data-key="factum"></div></div>
+      <div>7. Forēs ligneae = forēs <div class="area" data-key="ex"></div> <div class="area" data-key="lignō"></div> <div class="area" data-key="factae"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 2</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">admittere</div> <div class="area">anteā</div> <div class="area">aurum</div> <div class="area">cardō</div> <div class="area">catēna</div> <div class="area">cōnstāre</div> <div class="area">cūstōdīre</div> <div class="area">extrā</div> <div class="area">faber</div> <div class="area">ferōx</div> <div class="area">foris</div> <div class="area">heus</div> <div class="area">iānitor</div> <div class="area">inter sē</div> <div class="area">intrā</div> <div class="area">lignum</div> <div class="area">līmen</div> <div class="area">posteā</div> <div class="area">pretium</div> <div class="area">prius</div> <div class="area">quīn</div> <div class="area">(num) quis</div> <div class="area">rogitāre</div> <div class="area">sīcut</div> <div class="area">tabellārius</div> <div class="area">tandem</div> <div class="area">vincīre</div></div></div>
+      <div><br></div>
+      <div>1. Magna iānua <div class="area" data-key="cōnstat"></div> ē duābus <div class="area" data-key="foribus"></div>, quae in <div class="area" data-key="cardinibus"></div> vertuntur.</div>
+      <div>2. Sub foribus est <div class="area" data-key="līmen"></div>.</div>
+      <div>3. <div class="area" data-key="iānitor"></div> dīcitur servus quī hominēs advenientēs in ātrium <div class="area" data-key="admittit"></div>.</div>
+      <div>4. Iānitor cum cane suō vīllam dominī <div class="area" data-key="cūstōdit"></div>.</div>
+      <div>5. Canis, quī prope tam <div class="area" data-key="ferōx"></div> est quam lupus, catēnā <div class="area" data-key="vincītur"></div>.</div>
+      <div>6. <div class="area" data-key="anteā"></div> nōn modō canēs, sed etiam iānitōrēs vinciēbantur.</div>
+      <div>7. <div class="area" data-key="catēna"></div> cōnstat ē multīs ānulīs ferreīs quī <div class="area" data-key="inter sē"></div> coniunguntur.</div>
+      <div>8. Ānulus Lȳdiae nōn ex ferrō, sed ex <div class="area" data-key="aurō"></div> factus est.</div>
+      <div>9. Ānulus aureus magnī <div class="area" data-key="pretiī"></div> est.</div>
+      <div>10. Iānua ē <div class="area" data-key="lignō"></div> facta est <div class="area" data-key="sīcut"></div> baculum Iūliī.</div>
+      <div>11. Servus quī epistulās fert <div class="area" data-key="tabellārius"></div> appellātur.</div>
+      <div>12. Tabellārius clāmat: “<div class="area" data-key="heus"></div> iānitor! <div class="area" data-key="quīn"></div> aperīs? Num <div class="area" data-key="quis"></div> hīc est?”</div>
+      <div>13. <div class="area" data-key="tandem"></div> iānitor surgit et tabellārium <div class="area" data-key="rogitat"></div>.</div>
+      <div>14. Tabellārius: “<div class="area" data-key="prius"></div> admitte mē! <div class="area" data-key="posteā"></div> respondēbō.”</div>
+      <div>15. Hostēs nōn sunt <div class="area" data-key="intrā"></div> vāllum, sed <div class="area" data-key="extrā"></div> vāllum.</div>
+      <div>16. Vulcānus est deus <div class="area" data-key="fabrōrum"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 4</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-tum</div> <div class="area">-tū</div></div></div>
+      <div><br></div>
+      <div>1. Post merīdiem Iūlius ambulā<div class="area" data-key="tum"></div> īre solet.</div>
+      <div>2. Postquam ambulāvit, lavā<div class="area" data-key="tum"></div> it.</div>
+      <div>3. Vesperī hominēs dormī<div class="area" data-key="tum"></div> eunt.</div>
+      <div>4. Māne amīcī ad vīllam veniunt dominum salūtā<div class="area" data-key="tum"></div>.</div>
+      <div>5. Mēdus et Lȳdia in Graeciam eunt habitā<div class="area" data-key="tum"></div>.</div>
+      <div>6. Hostēs castra oppugnā<div class="area" data-key="tum"></div> veniunt.</div>
+      <div>7. Tabellārius nōn mittitur pecūniam postulā<div class="area" data-key="tum"></div>.</div>
+      <div>8. Facile est vōcem magistrī audīre = Vōx magistrī est facilis audī<div class="area" data-key="tū"></div>.</div>
+      <div>9. Difficile est nōmina barbara dīcere = Nōmina barbara difficilia sunt dic<div class="area" data-key="tū"></div>.</div>
+      <div>10. Quae est dea pulcherrima? Id difficile scī<div class="area" data-key="tū"></div> est.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>II</div>
+      <div><br></div>
+      <div><b>Exercitium 5</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">ferre</div></div></div>
+      <div><br></div>
+      <div>1. Mēdus, quī rēs Mārcī fer<div class="area" data-key="re"></div> solet, hodiē abest.</div>
+      <div>2. Itaque Mārcus ad lūdum ambulāns rēs suās ipse fer<div class="area" data-key="t"></div>.</div>
+      <div>3. Titus et Sextus semper rēs suās ipsī fer<div class="area" data-key="unt"></div>.</div>
+      <div>4. Titus: “Cūr tū ipse rēs tuās fer<div class="area" data-key="s"></div> hodiē, Mārce?”</div>
+      <div>5. Mārcus: “Ipse rēs meās fer<div class="area" data-key="ō"></div>, quia Mēdus abest; sed cūr vōs semper rēs vestrās ipsī fer<div class="area" data-key="tis"></div>?”</div>
+      <div>6. Titus: “Nōs rēs nostrās ipsī fer<div class="area" data-key="imus"></div>, quia in ipsō oppidō Tūsculō habitāmus, nōn procul ab oppidō.”</div>
+      <div>7. Mārcus: “Venīte mēcum ac rēs meās fer<div class="area" data-key="te"></div>!”</div>
+      <div>8. Sextus: “Rēs tuās fer<div class="area" data-key="re"></div> nōlumus. Ipse rēs tuās <div class="area" data-key="fer"></div>, Mārce!”</div>
+      <div>9. Tabula, quae ā Mēdō fer<div class="area" data-key="rī"></div> solet, hodiē ā Mārcō fer<div class="area" data-key="tur"></div>.</div>
+      <div>10. Epistulae ā tabellāriīs fer<div class="area" data-key="untur"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 6</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">accēdere</div> <div class="area">arbitrārī</div> <div class="area">cavēre</div> <div class="area">cēdere</div> <div class="area">dērīdēre</div> <div class="area">forās</div> <div class="area">forīs</div> <div class="area">fremere</div> <div class="area">imāgō</div> <div class="area">iste</div> <div class="area">monēre</div> <div class="area">mordēre</div> <div class="area">nūper</div> <div class="area">pallium</div> <div class="area">pellere</div> <div class="area">prehendere</div> <div class="area">prōcēdere</div> <div class="area">recēdere</div> <div class="area">removēre</div> <div class="area">resistere</div> <div class="area">retinēre</div> <div class="area">rumpere</div> <div class="area">scindere</div> <div class="area">sinere</div> <div class="area">solvere</div> <div class="area">terrēre</div> <div class="area">tremere</div></div></div>
+      <div><br></div>
+      <div>1. Canis, quī tabellārium <div class="area" data-key="forīs"></div> stantem videt, dentēs ostendit ac <div class="area" data-key="fremit"></div>: “Rrrr...”</div>
+      <div>2. Canis tabellārium <div class="area" data-key="mordēre"></div> vult, sed catēnā <div class="area" data-key="retinētur"></div>.</div>
+      <div>3. Iānitor eum dē cane ferōcī  <div class="area" data-key="monet"></div>: “<div class="area" data-key="cavē"></div>! Canis tē mordēbit.”</div>
+      <div>4. Tabellārius nōn intrat, sed in līmine <div class="area" data-key="resistit"></div>.</div>
+      <div>5. “Nōlī canem tuum <div class="area" data-key="solvere"></div>!” inquit.</div>
+      <div>6. In solō intrā līmen est <div class="area" data-key="imāgō"></div> canis ferōcis.</div>
+      <div>7. Tabellārius propius ad canem <div class="area" data-key="accēdere"></div> audet; neque imāgō canis neque canis vērus eum <div class="area" data-key="terret"></div>.</div>
+      <div>8. Canis catēnam <div class="area" data-key="rumpit"></div> et vestem tabellāriī dentibus <div class="area" data-key="prehendit"></div>.</div>
+      <div>9. <div class="area" data-key="pallium"></div> novum, quod Tlēpolemus <div class="area" data-key="nūper"></div> ēmit, ā cane ferōcī <div class="area" data-key="scinditur"></div>.</div>
+      <div>10. Tabellārius ex ōstiō <div class="area" data-key="cēdere"></div> nōn potest, quia canis eum tenet.</div>
+      <div>11. Homō territus nec <div class="area" data-key="recēdere | prōcēdere"></div> nec <div class="area" data-key="prōcēdere | recēdere"></div> audet.</div>
+      <div>12. Canis eum locō sē movēre nōn <div class="area" data-key="sinit"></div>.</div>
+      <div>13. Iānitor virum territum <div class="area" data-key="dērīdet"></div>: “Hahahae! Quīn intrās?”</div>
+      <div>14. Tlēpolemus intrāre cōnātur, sed canis īrātus eum ex ōstiō <div class="area" data-key="pellit"></div>.</div>
+      <div>15. “Vincī <div class="area" data-key="istum"></div> canem!” inquit tabellārius tōtō corpore <div class="area" data-key="tremēns"></div>.</div>
+      <div>16. Iānitor: “Nōlī <div class="area" data-key="arbitrārī"></div> mē canem solvisse!”</div>
+      <div>17. Iānitor catēnam prehendit et canem ā Tlēpolemō <div class="area" data-key="removet"></div>.</div>
+      <div>18. Tlēpolemus: “<div class="area" data-key="sine"></div> mē intrāre! Nōlī mē <div class="area" data-key="forās"></div> in imbrem pellere!”</div>
+    </div>
+  `
+});
