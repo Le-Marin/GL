1026beecc4acdd7de879c396a15d44c477d71f55
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-08-30 18:27:41 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-21etxo73.js
@@ -0,0 +1,41 @@
+$_GET({
+  title: 'Ultra 21',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Прописать формы глаголов, сохраняя данные залог, лицо и число (или безличную форму инфинитива). В случае неопределенности использовать только мужской род и только единственное число. При необходимости сверяться с онлайн-словарем (3-е спряжение, неправильные глаголы).</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>praesēns</i> : <i>perfectum</i></span></div>
+      <div><br></div>
+      <div><div class="area">cōgitāmus</div> : <div class="area" data-key="cōgitāvimus"></div></div>
+      <div><div class="area">mollīre</div> : <div class="area" data-key="mollīvisse"></div></div>
+      <div><div class="area" data-key="intuētur"></div> : <div class="area">intuitus est</div></div>
+      <div><div class="area">vehuntur</div> : <div class="area" data-key="vectī sunt"></div></div>
+      <div><div class="area">iaciō</div> : <div class="area" data-key="iēcī"></div></div>
+      <div><div class="area" data-key="docētis"></div> : <div class="area">docuistis</div></div>
+      <div><div class="area">proficīscimur</div> : <div class="area" data-key="profectī sumus"></div></div>
+      <div><div class="area">timeō</div> : <div class="area" data-key="timuī"></div></div>
+      <div><div class="area" data-key="oppugnāminī"></div> : <div class="area">oppugnātī estis</div></div>
+      <div><div class="area" data-key="habērī"></div> : <div class="area">habitum esse</div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>2. Изменить глаголы, данные в настоящем времени, на глаголы в перфекте. При необходимости сверяться с онлайн-словарем (3-е спряжение, неправильные или просто незнакомые глаголы).</b></div>
+      <div><br></div>
+      <div>1. Agamemnōn, īram deae placāns, fīliam suam Īphigenīam <div class="area" data-key="sacrificāvit">sacrificat</div>.</div>
+      <div>2. Rōma conditōris nōmine <div class="area" data-key="appellāta est">appellātur</div>.</div>
+      <div>3. Iam advesperāscēbat cum tandem <div class="area" data-key="audītus est">audītur</div> procul strepitus rotārum.</div>
+      <div>4. Apollō Hyacinthī: “Ubi prīmum <div class="area" data-key="cōnspexī">cōnspiciō</div>, statim tē <div class="area" data-key="adamāvī">adamō</div>.”</div>
+      <div>5. Nummī <div class="area" data-key="numerātī sunt">numerantur</div>, et Iūlius <div class="area" data-key="intellēxit">intelligit</div> aliquot deesse.</div>
+      <div>6. Vellus aureum per multōs annōs ā dracōne <div class="area" data-key="cūstōdītum est">cūstōdītur</div>.</div>
+      <div>7. Rēx Proca Numitōrem et Amūlium <div class="area" data-key="prōcreāvit">prōcreat</div> nec huic, sed illī rēgnum <div class="area" data-key="relīquit">relinquit</div>.</div>
+      <div>8. Dominus servō: “Sī ā deīs <div class="area" data-key="servātus es">servāris</div>, tē iterum pūnīre nōlō.”</div>
+      <div>9. Domina ancillīs: “<div class="area" data-key="pūnītae estis">Pūnīminī</div> quia malae <div class="area" data-key="fuistis">estis</div>.”</div>
+      <div>10. Paris amōre <div class="area" data-key="victus est">vincitur</div> Veneremque <div class="area" data-key="iūdicāvit">iūdicat</div> optimam deam esse.</div>
+      <div>11. Frātre interfectō, <div class="area" data-key="horruit">horret</div> Rōmulus, <div class="area" data-key="puduit">pudet</div> eum factī suī, nec hoc <div class="area" data-key="mōnstrāvit">mōnstrat</div>.</div>
+      <div>12. “Dīcitis timōre <div class="area" data-key="caruisse">carēre</div>, sed certē vērum nōn est.”</div>
+      <div>13. Nautae: “Perīcula maris nōn <div class="area" data-key="veritī sumus">verēmur</div> et omnia <div class="area" data-key="superāvimus">superāmus</div>.”</div>
+      <div>14. Valeria <div class="area" data-key="laetāta est">laetātur</div> <div class="area" data-key="dīxit">dīcit</div>que: “Gaudeō tē <div class="area" data-key="rediisse">redīre</div>.”</div>
+      <div>15. “<div class="area" data-key="Audīvistī">Audīs</div>ne? Aiunt nāvem in altō marī <div class="area" data-key="mersam esse">mergī</div>.”</div>
+    </div>
+  `
+});
