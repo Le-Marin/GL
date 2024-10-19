d155afb51d5d6b872c944c2bf31cba689f8dccfa
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-10-19 19:21:29 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-22rshdu8.js
@@ -0,0 +1,52 @@
+$_GET({
+  title: 'Ultra 22',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Перевести с помощью <i>acc. cum inf.</i>, принимая во внимание выраженную идею — одновременности (<i>настоящий инфинитив</i>) или предшествования (<i>перфектный инфинитив</i>).</b></div>
+      <div><br></div>
+      <div><i><span class="ff">Āctīvum:</span></i></div>
+      <div>1. Чувствую, ты что-то скрываешь.</div>
+      <div>S<div class="area" data-key="entiō"></div> <div class="area" data-key="tē"></div> a<div class="area" data-key="liquid"></div> o<div class="area" data-key="ccultāre"></div>.</div>
+      <div>2. Мы знаем, что вы сказали неправду.</div>
+      <div>S<div class="area" data-key="cīmus"></div> <div class="area" data-key="vōs"></div> f<div class="area" data-key="alsa"></div> d<div class="area" data-key="īxisse"></div>.</div>
+      <div>3. Фабий говорит, что не брал чужих денег.</div>
+      <div>F<div class="area" data-key="abius"></div> d<div class="area" data-key="īcit"></div> <div class="area" data-key="sē"></div> nōn s<div class="area" data-key="ūmpsisse"></div> a<div class="area" data-key="liēnam"></div> p<div class="area" data-key="ecūniam"></div>.</div>
+      <div>4. Ясно, что Публий — непорядочный человек.</div>
+      <div>Patet P<div class="area" data-key="ūblium"></div> h<div class="area" data-key="ominem"></div> im<div class="area" data-key="probum"></div> <div class="area" data-key="esse"></div>.</div>
+      <div>5. Еврипид пишет, что Медея была дочерью бога Солнца.</div>
+      <div>Eu<div class="area" data-key="ripidēs"></div> s<div class="area" data-key="crībit"></div> M<div class="area" data-key="ēdēam"></div> <div class="area" data-key="fuisse"></div> f<div class="area" data-key="īliam"></div> d<div class="area" data-key="deī"></div> S<div class="area" data-key="ōlis"></div>.</div>
+      <div>6. Нешто ты думал, я ничего не знаю?</div>
+      <div>N<div class="area" data-key="um"></div> p<div class="area" data-key="utābās"></div> <div class="area" data-key="mē"></div> n<div class="area" data-key="ihil"></div> s<div class="area" data-key="cīre"></div>?</div>
+      <div>7. Клянусь, что ничего не знал.</div>
+      <div>Iūrō <div class="area" data-key="mē"></div> n<div class="area" data-key="ihil"></div> s<div class="area" data-key="cīvisse"></div>.</div>
+      <div>8. Корнелий заверяет, будто никто к нему не обращался.</div>
+      <div>C<div class="area" data-key="ornēlius"></div> a<div class="area" data-key="it"></div> n<div class="area" data-key="ēminem"></div> <div class="area" data-key="sē"></div> <div class="area" data-key="adiisse">adīre</div>.</div>
+      <div>9. Полагаешь ли ты, что весь сенат заблуждается?</div>
+      <div>P<div class="area" data-key="utās"></div>ne t<div class="area" data-key="ōtum"></div> s<div class="area" data-key="enātum"></div> e<div class="area" data-key="rrāre"></div>?</div>
+      <div>10. Необходимо, чтобы генерал немедленно ответил на письмо.</div>
+      <div>N<div class="area" data-key="ecesse"></div> e<div class="area" data-key="st"></div> d<div class="area" data-key="ucem"></div> s<div class="area" data-key="tatim"></div> <div class="area" data-key="ad"></div> e<div class="area" data-key="pistulam"></div> r<div class="area" data-key="espondēre"></div>.</div>
+      <div><br></div>
+      <div><i><span class="ff">Passivum:</span></i></div>
+      <div>1. Желаю, чтобы животных не держали в клетках.</div>
+      <div>V<div class="area" data-key="olō"></div> a<div class="area" data-key="nimālia"></div> nōn t<div class="area" data-key="enērī"></div> <div class="area" data-key="in"></div> c<div class="area" data-key="ellīs"></div>.</div>
+      <div>2. Хочешь, чтобы я следовал за тобой?</div>
+      <div>V<div class="area" data-key="īs"></div> m<div class="area" data-key="ē"></div> <div class="area" data-key="tē"></div> s<div class="area" data-key="equī"></div>?</div>
+      <div>3. Юлий услышал, как его назвали глупцом. </div>
+      <div>I<div class="area" data-key="ūlius"></div> au<div class="area" data-key="dīvit"></div> <div class="area" data-key="sē"></div> st<div class="area" data-key="ultum"></div> a<div class="area" data-key="ppellātum esse"></div>.</div>
+      <div>4. Мы видели, как поднялся шторм, что-то предвещая.</div>
+      <div>V<div class="area" data-key="īdimus"></div> t<div class="area" data-key="empestātem"></div> o<div class="area" data-key="orsam esse"></div> a<div class="area" data-key="liquid"></div> <div class="area" data-key="portendentem">portendere</div>.</div>
+      <div>5. Мать пишет, что корабли вышли из Остии в апрельские календы.</div>
+      <div>M<div class="area" data-key="āter"></div> s<div class="area" data-key="crībit"></div> n<div class="area" data-key="āvēs"></div> Ō<div class="area" data-key="stiā"></div> ē<div class="area" data-key="gressās esse"></div> k<div class="area" data-key="alendīs"></div> A<div class="area" data-key="prīlibus"></div>.</div>
+      <div>6. Каждый (из двоих) подумал, что другой солгал.</div>
+      <div>U<div class="area" data-key="terque"></div> p<div class="area" data-key="utāvit"></div> a<div class="area" data-key="lterum"></div> m<div class="area" data-key="entītum esse"></div>.</div>
+      <div>7. Каждый (из двоих) обвиняет другого, говоря, что тот лжет.</div>
+      <div>U<div class="area" data-key="terque"></div> ac<div class="area" data-key="cūsat"></div> a<div class="area" data-key="lterum"></div> d<div class="area" data-key="īcēns"></div> i<div class="area" data-key="llum"></div> m<div class="area" data-key="entīrī"></div>.</div>
+      <div>8. Говорят, будто амбросия изготовляется из крови самих богов.</div>
+      <div>D<div class="area" data-key="īcunt"></div> a<div class="area" data-key="mbrosiam"></div> <div class="area" data-key="ex"></div> s<div class="area" data-key="anguine"></div> d<div class="area" data-key="eōrum"></div> ef<div class="area" data-key="ficī"></div>.</div>
+      <div>9. Следует, чтобы плохих слуг хорошо наказывали.</div>
+      <div>O<div class="area" data-key="portet"></div> m<div class="area" data-key="alōs"></div> s<div class="area" data-key="ervōs"></div> b<div class="area" data-key="ene"></div> p<div class="area" data-key="ūnīrī"></div>.</div>
+      <div>10. Тит Ливий писал, что третий римский царь был поражен молнией.</div>
+      <div>T<div class="area" data-key="itus"></div> L<div class="area" data-key="ivius"></div> s<div class="area" data-key="crībēbat"></div> t<div class="area" data-key="ertium"></div> r<div class="area" data-key="ēgem"></div> R<div class="area" data-key="ōmānum | ōmānōrum"></div> f<div class="area" data-key="ulgure | ulmine"></div> <div class="area" data-key="ictum esse">icere</div>.</div>
+    </div>
+  `
+});
