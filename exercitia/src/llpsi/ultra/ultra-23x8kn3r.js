87d28bafcc203646db05fd3c45df8098fa4916c3
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-11-23 15:54:52 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-23x8kn3r.js
@@ -0,0 +1,32 @@
+$_GET({
+  title: 'Ultra 23',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Перевести с помощью <i>acc. cum inf.</i>, принимая во внимание идею следования (<i>будущный инфинитив</i>).</b></div>
+      <div><br></div>
+      <div><i>Āctīvum</i>:</div>
+      <div>1. In senātū: “Crēdō ipsōs deōs huic urbī contrā hostēs auxilium <div class="area" data-key="datūrōs esse">dare</div>.”</div>
+      <div>2. Lūcius: “Crās Rōmam <div class="area" data-key="ītūrus sum">ībō</div>”.</div>
+      <div>3. Lucrētia omnia nārrāvit marītō, quī iūrāvit sē Sextum <div class="area" data-key="necātūrum esse">necāre</div>.”</div>
+      <div>4. Iam senex, convocāvit fīliōs et dīxit sē brevī <div class="area" data-key="moritūrum esse">morī</div>.</div>
+      <div>5. Māter fīliābus: “Omnia quae mihi erunt vōbīs <div class="area" data-key="datūra sum">dabō</div>.”</div>
+      <div>6. Novus dominus spopondit sē lēniōrem in servōs <div class="area" data-key="futūrum esse | fore">esse</div>.</div>
+      <div>7. “Quōcumque proficīscēs,” inquiunt sociī Aenēae, “tē <div class="area" data-key="secūtūrī sumus">sequī</div>.”</div>
+      <div>8. Vōce āmissā, sciēbat Ēchō vōcem nōn <div class="area" data-key="redītūram esse">redīre</div>.</div>
+      <div>9. Iūlius sententiam uxōris percontāns: “Nōnne,” inquit, “<div class="area" data-key="dictūra es">dīcēs</div> quid cēnseās?”</div>
+      <div>10. Sacrificiō factō, spērābant omnēs Iūnōnem opem <div class="area" data-key="lātūram esse">ferre</div>.</div>
+      <div><br></div>
+      <div><i>Passīvum</i>:</div>
+      <div>1. Iūnō Paridī prōmīsit rēgnum omnis Asiae eī <div class="area" data-key="datum īrī">darī</div>.</div>
+      <div>2. Ōrāculō cōnsultō, dīxit sacerdōs Appolinis urbem <div class="area" data-key="captum īrī">capī</div>.</div>
+      <div>3. Timuērunt hominēs sē āmentiā <div class="area" data-key="exsecrātum īrī">exsecrārī</div> et oboedīvērunt Dionyssō.</div>
+      <div>4. “Sentiō,” inquit reus, “nōs, frātrēs, <div class="area" data-key="condemnātum īrī">condemnārī</div>.”</div>
+      <div>5. Inūstē accūsātūs,  servus repsondit sē in carcerem nōn <div class="area" data-key="iactum īrī">iacī</div>.</div>
+      <div>6. ...Itaque coniūrātōs dēcrēvērunt Caligulam imperātōrem <div class="area" data-key="caesum īrī">caedī</div>.</div>
+      <div>7. Vīdit gubernātor tempestātem orientem et nāvem <div class="area" data-key="mersum īrī">mergī</div> certō iam scīvit.</div>
+      <div>8. Cassandra prōvīdit Trōiam <div class="area" data-key="dēlētum īrī">dēlērī</div>, nec ūllus eī crēdēbat.</div>
+      <div>9. Etiam Lāocoōn monēbat cīvēs, dīcēns Trōiānōs <div class="area" data-key="pūnītum īrī">pūnīrī</div> prō crēdulitāte.</div>
+      <div>10. Orestēs, patris mortem ultus, scīvit et ipsī, prō parricidiō, ultiōnem <div class="area" data-key="lātum īrī">ferre</div>.</div>
+    </div>
+  `
+});
