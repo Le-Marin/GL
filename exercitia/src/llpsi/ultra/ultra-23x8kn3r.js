ceb224143c621256665c3182d7cc66a8bbf96a51
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-11-28 21:17:26 +0300

    Update ultra-23x8kn3r.js

--- exercitia/src/llpsi/ultra/ultra-23x8kn3r.js
+++ exercitia/src/llpsi/ultra/ultra-23x8kn3r.js
@@ -11,7 +11,7 @@ $_GET({
       <div>4. Iam senex, convocāvit fīliōs et dīxit sē brevī <div class="area" data-key="moritūrum esse">morī</div>.</div>
       <div>5. Māter fīliābus: “Omnia quae mihi erunt vōbīs <div class="area" data-key="datūra sum">dabō</div>.”</div>
       <div>6. Novus dominus spopondit sē lēniōrem in servōs <div class="area" data-key="futūrum esse | fore">esse</div>.</div>
-      <div>7. “Quōcumque proficīscēs,” inquiunt sociī Aenēae, “tē <div class="area" data-key="secūtūrī sumus">sequī</div>.”</div>
+      <div>7. “Quōcumque proficīscēs,” inquiunt sociī Aenēae, “tē <div class="area" data-key="secūtūrī sumus">sequēminī</div>.”</div>
       <div>8. Vōce āmissā, sciēbat Ēchō vōcem nōn <div class="area" data-key="redītūram esse">redīre</div>.</div>
       <div>9. Iūlius sententiam uxōris percontāns: “Nōnne,” inquit, “<div class="area" data-key="dictūra es">dīcēs</div> quid cēnseās?”</div>
       <div>10. Sacrificiō factō, spērābant omnēs Iūnōnem opem <div class="area" data-key="lātūram esse">ferre</div>.</div>
@@ -21,8 +21,8 @@ $_GET({
       <div>2. Ōrāculō cōnsultō, dīxit sacerdōs Appolinis urbem <div class="area" data-key="captum īrī">capī</div>.</div>
       <div>3. Timuērunt hominēs sē āmentiā <div class="area" data-key="exsecrātum īrī">exsecrārī</div> et oboedīvērunt Dionyssō.</div>
       <div>4. “Sentiō,” inquit reus, “nōs, frātrēs, <div class="area" data-key="condemnātum īrī">condemnārī</div>.”</div>
-      <div>5. Inūstē accūsātūs,  servus repsondit sē in carcerem nōn <div class="area" data-key="iactum īrī">iacī</div>.</div>
-      <div>6. ...Itaque coniūrātōs dēcrēvērunt Caligulam imperātōrem <div class="area" data-key="caesum īrī">caedī</div>.</div>
+      <div>5. Inūstē accūsātus,  servus repsondit sē in carcerem nōn <div class="area" data-key="iactum īrī">iacī</div>.</div>
+      <div>6. ...Itaque coniūrātī cōnsēnsērunt Caligulam imperātōrem <div class="area" data-key="caesum īrī">caedī</div>.</div>
       <div>7. Vīdit gubernātor tempestātem orientem et nāvem <div class="area" data-key="mersum īrī">mergī</div> certō iam scīvit.</div>
       <div>8. Cassandra prōvīdit Trōiam <div class="area" data-key="dēlētum īrī">dēlērī</div>, nec ūllus eī crēdēbat.</div>
       <div>9. Etiam Lāocoōn monēbat cīvēs, dīcēns Trōiānōs <div class="area" data-key="pūnītum īrī">pūnīrī</div> prō crēdulitāte.</div>
@@ -31,20 +31,20 @@ $_GET({
     <div class="row">
       <div><b>2. Образовать от инфинитивов <i>ppp</i> — перфектные страдательные причастия. В последних пяти предложениях даются отложительные глаголы: те же причастия, образованные от них, имеют не страдательное, но действительное значение совершенного вида (<i>сказав</i>, <i>став</i> и т. п.).</b></div>
       <div><br></div>
-      <div>1.<span style="white-space: pre;">	</span>Rēx Tullus, fulmine <div class="area" data-key="ictus">icere</div>, cum domō suā cōnflagrāvit (сгорать).</div>
+      <div>1.<span style="white-space: pre;">	</span>Rēx Tullus, fulmine <div class="area" data-key="ictus">icere</div>, cum domō suā cōnflagrāvit.</div>
       <div>2.<span style="white-space: pre;">	</span>Nōn ex latere, sed ex marmore <div class="area" data-key="facta">facere</div> splendēbant tēcta.</div>
       <div>3.<span style="white-space: pre;">	</span>In bracchiō togā nōn <div class="area" data-key="opertō">operīre</div> armillam gerere solēbat.</div>
       <div>4.<span style="white-space: pre;">	</span>Prōditor, multīs scūtīs <div class="area" data-key="oppressus">opprimere</div>, terribilī morte mortuus est.</div>
       <div>5.<span style="white-space: pre;">	</span>Tandem, metū <div class="area" data-key="victus">vincere</div>, rēx deō pāruit.</div>
       <div>6.<span style="white-space: pre;">	</span>Timōre <div class="area" data-key="perturbāta">perturbāre</div>, nōn poterat respondēre īnfēlīx mulier.</div>
-      <div>7.<span style="white-space: pre;">	</span><div class="area" data-key="adductī">adducere</div> ad rēgem, lēgātī prōcubuērunt ad pedēs illīus.</div>
+      <div>7.<span style="white-space: pre;">	</span><div class="area" data-key="adductī">addūcere</div> ad rēgem, lēgātī prōcubuērunt ad pedēs illīus.</div>
       <div>8.<span style="white-space: pre;">	</span>Ā Iove <div class="area" data-key="rapta">rapere</div>, Eurōpa deō nūpsit.</div>
       <div>9.<span style="white-space: pre;">	</span>Ariadna, ā Tēseō <div class="area" data-key="relicta">relinquere</div>, uxor Dionȳsī facta est.</div>
       <div>10.<span style="white-space: pre;">	</span>Legimus apud Ovidium Eurydicēn ā serpente <div class="area" data-key="morsam">mordēre</div> morī Orpheumque...</div>
       <div>11.<span style="white-space: pre;">	</span>Rōmā sub noctem <div class="area" data-key="profectī">proficīscī</div>, īnsequentī diē iam pervēnērunt Tūsculum.</div>
       <div>12.<span style="white-space: pre;">	</span>Haec <div class="area" data-key="locūta">loquī</div>, Dīdō rogum ascendit.</div>
       <div>13.<span style="white-space: pre;">	</span><div class="area" data-key="dēgressus">dēgredī</div> Appennīnō, Hannibal ad Placentiam castra mōvit.</div>
-      <div>14.<span style="white-space: pre;">	</span>Iuppiter Ganymēdī: “Quid dīcēs mihi amōrem suum <div class="area" data-key="fassō">fatērī</div>?”</div>
+      <div>14.<span style="white-space: pre;">	</span>Iuppiter Ganymēdī: “Quid dīcēs mihi amōrem meum <div class="area" data-key="fassō">fatērī</div>?”</div>
       <div>15.<span style="white-space: pre;">	</span>Canis timidus, audācior <div class="area" data-key="factus">fierī</div>, tandem accessit.</div>
       <div><br></div>
     </div>
