053a0e81198fac10b78bfd613f51ae6d273c4c43
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-12-16 23:47:01 +0300

    Add files via upload

--- exercitia/src/llpsi/ultra/ultra-01v8lp1s.js
+++ exercitia/src/llpsi/ultra/ultra-01v8lp1s.js
@@ -2,29 +2,54 @@ $_GET({
   title: 'Ultra 1',
   html: /*html*/`
     <div class="row">
-      <div><b>1. Дать форму множественного числа существительных.</b></div>
-      <div><br></div>
-      <div>amīc<b>us</b> → <div class="area" data-key="amīcī"></div></div>
-      <div>chart<b>a</b> → <div class="area" data-key="chartae"></div></div>
-      <div>terr<b>a</b> → <div class="area" data-key="terrae"></div></div>
-      <div>bell<b>um</b> → <div class="area" data-key="bella"></div></div>
-      <div>urs<b>us</b> → <div class="area" data-key="ursī"></div></div>
-      <div>sphaer<b>a</b> → <div class="area" data-key="sphaerae"></div></div>
-      <div>popul<b>us</b> → <div class="area" data-key="populī"></div></div>
-      <div>dōn<b>um</b> → <div class="area" data-key="dōna"></div></div>
+      <div><b>1. Dā plūrālem. </b>Дать форму множественного числа.<br><br></div>
+      <div>1. amīc<b>us</b> → <div class="area" data-key="amīcī"></div></div>
+      <div>2. chart<b>a</b> → <div class="area" data-key="chartae"></div></div>
+      <div>3. terr<b>a</b> → <div class="area" data-key="terrae"></div></div>
+      <div>4. bell<b>um</b> → <div class="area" data-key="bella"></div></div>
+      <div>5. urs<b>us</b> → <div class="area" data-key="ursī"></div></div>
+      <div>6. sphaer<b>a</b> → <div class="area" data-key="sphaerae"></div></div>
+      <div>7. popul<b>us</b> → <div class="area" data-key="populī"></div></div>
+      <div>8. dōn<b>um</b> → <div class="area" data-key="dōna"></div></div>
       <div><br></div>
     </div>
     <div class="row">
-      <div><b>2. Поставить существительные во множественное число и согласовать с ними прилагательные.</b></div>
+      <div><b>2. Pōne substantīvum in plūrālem et iunge fōrmam adiectīvī rēctam.</b> Поставить существительное во множественное число и согласовать с ним прилагательное.</div>
       <div><br></div>
-      <div>coll<b>um</b> <div class="area">long<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="colla"></div> <div class="area" data-key="longa"></div></div>
-      <div>terr<b>a</b> <div class="area">longinqu<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="terrae"></div> <div class="area" data-key="longinquae"></div></div>
-      <div>form<b>a</b> <div class="area">vag<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="formae"></div> <div class="area" data-key="vagae"></div></div>
-      <div>de<b>us</b> <div class="area">ignōt<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="deī"></div> <div class="area" data-key="ignōtī"></div></div>
-      <div>fāt<b>um</b> <div class="area">horrid<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="fāta"></div> <div class="area" data-key="horrida"></div></div>
-      <div>aqu<b>a</b> <div class="area">frīgid<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="aquae"></div> <div class="area" data-key="frīgidae"></div></div>
-      <div>vent<b>us</b> <div class="area">calid<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="ventī"></div> <div class="area" data-key="calidī"></div></div>
-      <div>vēl<b>um</b> <div class="area">ātr<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="vēla"></div> <div class="area" data-key="ātra"></div></div>
+      <div>1. coll<b>um</b> <div class="area">long<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="colla"></div> <div class="area" data-key="longa"></div></div>
+      <div>2. terr<b>a</b> <div class="area">longinqu<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="terrae"></div> <div class="area" data-key="longinquae"></div></div>
+      <div>3. form<b>a</b> <div class="area">vag<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="formae"></div> <div class="area" data-key="vagae"></div></div>
+      <div>4. de<b>us</b> <div class="area">ignōt<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="deī"></div> <div class="area" data-key="ignōtī"></div></div>
+      <div>5. fāt<b>um</b> <div class="area">horrid<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="fāta"></div> <div class="area" data-key="horrida"></div></div>
+      <div>6. aqu<b>a</b> <div class="area">frīgid<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="aquae"></div> <div class="area" data-key="frīgidae"></div></div>
+      <div>7. vent<b>us</b> <div class="area">calid<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="ventī"></div> <div class="area" data-key="calidī"></div></div>
+      <div>8. vēl<b>um</b> <div class="area">ātr<b>us</b> -<b>a</b> -<b>um</b></div> → <div class="area" data-key="vēla"></div> <div class="area" data-key="ātra"></div><br><br></div>
+    </div>
+    <div class="row">
+      <div><b>3. Mūtā nōminātīvum in ablātīvum.</b> Изменить номинатив на аблатив.</div>
+      <div><br></div>
+      <div>1. in <div class="area" data-key="Capitōiō">Capitōli<b>um</b></div></div>
+      <div>2. in <div class="area" data-key="laniēnā">laniēn<b>a</b></div></div>
+      <div>3. in <div class="area" data-key="balneō">balne<b>um</b></div></div>
+      <div>4. in <div class="area" data-key="īnsulā Myconō">īnsul<b>a</b> Mycon<b>us</b></div></div>
+      <div>5. in <div class="area" data-key="campō Mārtiō">camp<b>us</b> Mārti<b>us</b></div></div>
+      <div>6. in <div class="area" data-key="vīcō angustō">vīc<b>us</b> angust<b>us</b></div></div>
+      <div>7. in <div class="area" data-key="aerāriō pūblicō">aerāri<b>um</b> pūblic<b>um</b></div></div>
+      <div>8. in <div class="area" data-key="Galliā trānsalpīnā">Galli<b>a</b> trānsalpīn<b>a</b></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <b>❗ </b><b>4. Verte in sermōnem Latīnum. Sī necesse, ūtere dictiōnāriō.</b> Перевести на латынь. По необходимости используйте словарь.<b><br></b>
+      <div><br>1. <i>Лондон находится в Англии.</i></div>
+      <div>L<div class="area" data-key="ondīnium | ondinium"></div> in A<div class="area" data-key="ngliā"></div> <div class="area" data-key="est"></div>.</div>
+      <div>2. <i>Тигр и Евфрат — реки.</i></div>
+      <div>T<div class="area" data-key="igris"></div> <div class="area" data-key="et"></div> Eu<div class="area" data-key="phrātēs"></div> f<div class="area" data-key="luviī"></div> <div class="area" data-key="sunt"></div>.</div>
+      <div>3. <i>Ява — это остров в Индийском океане.</i></div>
+      <div>I<div class="area" data-key="ava"></div> <div class="area" data-key="est"></div> ī<div class="area" data-key="nsula"></div> in ō<div class="area" data-key="ceanō"></div> I<div class="area" data-key="ndicō"></div>.</div>
+      <div>4. <i>Разве два и два — пять?</i></div>
+      <div>N<div class="area" data-key="um"></div> d<div class="area" data-key="uo"></div> <div class="area" data-key="et"></div> d<div class="area" data-key="uo"></div> s<div class="area" data-key="unt"></div> <div class="area" data-key="quīnque"></div>?</div>
+      <div>5. <i>Что такое “</i>δημοκρατία”<i>? Это греческое слово.</i></div>
+      <div><div class="area" data-key="quid"></div> <div class="area" data-key="est"></div> “δημοκρατία”? <div class="area" data-key="est"></div> v<div class="area" data-key="ocābulum"></div> G<div class="area" data-key="raecum"></div>.</div>
     </div>
   `
 });
