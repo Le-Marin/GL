b941b3f7576e3c2440417d5839d17893be7c549d
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-12-16 21:29:57 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-24gc5p09.js
@@ -0,0 +1,56 @@
+$_GET({
+  title: 'Ultra 24',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Перевести актив в пассив, также заменив в активных предложениях наст. время на требуемое, а в случае с <i>acc. cum inf.</i> — личную форму на безличную.</b></div>
+      <div><br></div>
+      <div><b>— perfectum</b>:</div>
+      <div>1. Dionȳsus Lycūrgum rēgem <div class="area" data-key="dēmentāvit">dēmentat</div>.</div>
+      <div>• Lycūrg<div class="area" data-key="us"></div> r<div class="area" data-key="ēx"></div> <div class="area" data-key="ā"></div> Dionȳs<div class="area" data-key="ō"></div> dē<div class="area" data-key="mentātus"></div> <div class="area" data-key="est"></div>.</div>
+      <div>2. Oedipus: “Patrem meum <div class="area" data-key="occīdī">occīdō</div>.”</div>
+      <div>• Lāius (iam mortuus): “<div class="area" data-key="Ā"></div> fīli<div class="area" data-key="ō"></div> me<div class="area" data-key="ō"></div> oc<div class="area" data-key="cīsus"></div> <div class="area" data-key="sum"></div>.”</div>
+      <div>3. Eurystheus Herculī: “Equōs mihi <div class="area" data-key="addūxistī">addūcis</div>.”</div>
+      <div>• Eurystheus Herculī: “Equ<div class="area" data-key="ī"></div> mihi ad<div class="area" data-key="ductī"></div> <div class="area" data-key="sunt"></div>.”</div>
+      <div>4. Ēlectra Orestī: “Mātrem nostram Clytaemnēstram <div class="area" data-key="pūnīvimus">pūnīmus</div>.”</div>
+      <div>• Ēlectra Orestī: “M<div class="area" data-key="āter"></div> nostr<div class="area" data-key="a"></div> Clytaemnēstr<div class="area" data-key="a"></div> p<div class="area" data-key="ūnīta"></div> <div class="area" data-key="est"></div>.”</div>
+      <div>5. Argonautae, Iāsone duce, vellus aureum in Graeciam <div class="area" data-key="vexērunt">vehunt</div>.</div>
+      <div>• Vell<div class="area" data-key="us"></div> aure<div class="area" data-key="um"></div>, Iāsone duce, <div class="area" data-key="ab"></div> Argonaut<div class="area" data-key="īs"></div> in Graeciam v<div class="area" data-key="ectum"></div> <div class="area" data-key="est"></div>.</div>
+      <div><br></div>
+      <div><b>— perfectum (<i>acc. cun inf.</i>)</b>:</div>
+      <div>1. Dīcunt Dionȳsum Lycūrgum rēgem <div class="area" data-key="dēmentāvisse">dēmentāre</div>.</div>
+      <div>• Dīcunt Lycūrg<div class="area" data-key="um"></div> r<div class="area" data-key="ēgem"></div> <div class="area" data-key="ā"></div> Dionȳs<div class="area" data-key="ō"></div> dē<div class="area" data-key="mentātum"></div> <div class="area" data-key="esse"></div>.</div>
+      <div>2. Exclāmat Oedipus sē patrem suum <div class="area" data-key="occīdisse">occīdere</div>.</div>
+      <div>• Exclāmat Oedipus p<div class="area" data-key="atrem"></div> su<div class="area" data-key="um"></div> <div class="area" data-key="ā"></div> <div class="area" data-key="sē"></div> oc<div class="area" data-key="cīsum"></div> <div class="area" data-key="esse"></div>.</div>
+      <div>3. Eurystheus gaudet Herculem equōs sibi <div class="area" data-key="addūxisse">addūcere</div>.</div>
+      <div>• Eurystheus gaudet equ<div class="area" data-key="ōs"></div> <div class="area" data-key="ā"></div> Hercul<div class="area" data-key="e"></div> s<div class="area" data-key="ibi"></div> ad<div class="area" data-key="ductōs"></div> <div class="area" data-key="esse"></div>.</div>
+      <div>4. Cōnstat Ēlectram Orestemque mātrem suam Clytaemnēstram <div class="area" data-key="pūnīvisse">pūnīre</div>.</div>
+      <div>• Cōnstat Clytaemnēstr<div class="area" data-key="am"></div> <div class="area" data-key="ā"></div> līber<div class="area" data-key="īs"></div> su<div class="area" data-key="īs"></div>, Ēlectr<div class="area" data-key="ā"></div> Orest<div class="area" data-key="e"></div>que p<div class="area" data-key="ūnītam"></div> <div class="area" data-key="esse"></div>.</div>
+      <div>5. Scīmus Argonautās, Iāsone duce, vellus aureum in Graeciam <div class="area" data-key="vexisse">vehere</div>.</div>
+      <div>• Scīmus vell<div class="area" data-key="us"></div> aure<div class="area" data-key="um"></div>, Iāsone duce, in Graeciam v<div class="area" data-key="ectum"></div> <div class="area" data-key="esse"></div>.</div>
+      <div><br></div>
+      <div><b>— futūrum / futūrum periphrasticum</b>:</div>
+      <div>1. Dionȳsus Lycūrgum rēgem <div class="area" data-key="dēmentābit">dēmentat</div> / <div class="area" data-key="dēmentātūrus"></div> <div class="area" data-key="est"></div>.</div>
+      <div>• Lycūrg<div class="area" data-key="us"></div> r<div class="area" data-key="ēx"></div> <div class="area" data-key="ā"></div> Dionȳs<div class="area" data-key="ō"></div> dē<div class="area" data-key="mentābitur"></div>.</div>
+      <div>2. Ōrāculum Oedipō: “Patrem tuum <div class="area" data-key="occīdēs">occīdis</div> / <div class="area" data-key="occīsūrus"></div> <div class="area" data-key="est"></div>.”</div>
+      <div>• Ōrāculum Lāiō: “<div class="area" data-key="Ā"></div> fīli<div class="area" data-key="ō"></div> tu<div class="area" data-key="ō"></div> oc<div class="area" data-key="cīderis"></div>.”</div>
+      <div>3. Eurystheus Herculī: “Equōs mihi <div class="area" data-key="addūcēs">addūcis</div> / <div class="area" data-key="adductūrus"></div> <div class="area" data-key="es"></div>.”</div>
+      <div>• Eurystheus Herculī: “Equ<div class="area" data-key="ī"></div> mihi ad<div class="area" data-key="dūcentur"></div>.”</div>
+      <div>4. Ēlectra Orestī: “Mātrem nostram Clytaemnēstram <div class="area" data-key="pūniēmus">pūnīmus</div> / <div class="area" data-key="pūnītūrī"></div> <div class="area" data-key="sumus"></div>.”</div>
+      <div>• Ēlectra Orestī: “M<div class="area" data-key="āter"></div> nostr<div class="area" data-key="a"></div> Clytaemnēstr<div class="area" data-key="a"></div> p<div class="area" data-key="ūniētur"></div>.”</div>
+      <div>5. Argonautae, Iāsone duce, vellus aureum in Graeciam <div class="area" data-key="vehent">vehunt</div> / <div class="area" data-key="vectūrī"></div> <div class="area" data-key="sunt"></div>.</div>
+      <div>• Vell<div class="area" data-key="us"></div> aure<div class="area" data-key="um"></div>, Iāsone duce, <div class="area" data-key="ab"></div> Argonaut<div class="area" data-key="īs"></div> in Graeciam v<div class="area" data-key="ehētur"></div>.</div>
+      <div><br></div>
+      <div><b>— futūrum (<i>acc. cum inf.</i>)</b>:</div>
+      <div>1. Dīcunt Dionȳsum Lycūrgum rēgem <div class="area" data-key="dēmentātūrum esse">dēmentāre</div>.</div>
+      <div>• Dīcunt Lycūrg<div class="area" data-key="um"></div> r<div class="area" data-key="ēgem"></div> <div class="area" data-key="ā"></div> Dionȳs<div class="area" data-key="ō"></div> dē<div class="area" data-key="mentātum"></div> <div class="area" data-key="īrī"></div>.</div>
+      <div>2. Fuit ōrāculum Oedipō sē patrem suum <div class="area" data-key="occīsūrum esse">occīdere</div>.</div>
+      <div>• Fuit ōrāculum Lāiō sē <div class="area" data-key="ā"></div> fīli<div class="area" data-key="ō"></div> su<div class="area" data-key="ō"></div> oc<div class="area" data-key="cīsum"></div> <div class="area" data-key="īrī"></div>.</div>
+      <div>3. Eurystheus gaudet Herculem equōs sibi <div class="area" data-key="adductūrum esse">addūcere</div>.</div>
+      <div>• Eurystheus gaudet equ<div class="area" data-key="ōs"></div> <div class="area" data-key="ā"></div> Hercul<div class="area" data-key="e"></div> s<div class="area" data-key="ibi"></div> ad<div class="area" data-key="ductum"></div> <div class="area" data-key="īrī"></div>.</div>
+      <div>4. Cōnstat Ēlectram Orestemque mātrem suam Clytaemnēstram <div class="area" data-key="pūnītūrōs esse">pūnīre</div>.”</div>
+      <div>• Cōnstat Clytaemnēstr<div class="area" data-key="am"></div> <div class="area" data-key="ā"></div> līber<div class="area" data-key="īs"></div> su<div class="area" data-key="īs"></div>, Ēlectr<div class="area" data-key="ā"></div> Orest<div class="area" data-key="e"></div>que p<div class="area" data-key="ūnītum"></div> <div class="area" data-key="īrī"></div>.</div>
+      <div>5. Scīmus Argonautae, Iāsone duce, vellus aureum in Graeciam <div class="area" data-key="vectūrōs esse">vehere</div>.</div>
+      <div>• Scīmus vell<div class="area" data-key="us"></div> aure<div class="area" data-key="um"></div>, Iāsone duce, in Graeciam v<div class="area" data-key="ectum"></div> <div class="area" data-key="īrī"></div>.</div>
+    </div>
+  `
+});
