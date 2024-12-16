95faaa1bad39a59ff056bed31190ac7d78d9c8a9
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-12-16 23:56:41 +0300

    Add files via upload

--- exercitia/src/llpsi/ultra/ultra-01v8lp1s.js
+++ exercitia/src/llpsi/ultra/ultra-01v8lp1s.js
@@ -28,14 +28,14 @@ $_GET({
     <div class="row">
       <div><b>3. Mūtā nōminātīvum in ablātīvum.</b> Изменить номинатив на аблатив.</div>
       <div><br></div>
-      <div>1. in <div class="area" data-key="Capitōiō">Capitōli<b>um</b></div></div>
-      <div>2. in <div class="area" data-key="laniēnā">laniēn<b>a</b></div></div>
-      <div>3. in <div class="area" data-key="balneō">balne<b>um</b></div></div>
-      <div>4. in <div class="area" data-key="īnsulā Myconō">īnsul<b>a</b> Mycon<b>us</b></div></div>
-      <div>5. in <div class="area" data-key="campō Mārtiō">camp<b>us</b> Mārti<b>us</b></div></div>
-      <div>6. in <div class="area" data-key="vīcō angustō">vīc<b>us</b> angust<b>us</b></div></div>
-      <div>7. in <div class="area" data-key="aerāriō pūblicō">aerāri<b>um</b> pūblic<b>um</b></div></div>
-      <div>8. in <div class="area" data-key="Galliā trānsalpīnā">Galli<b>a</b> trānsalpīn<b>a</b></div></div>
+      <div>1. in Capitōli<div class="area" data-key="ō">um</div></div>
+      <div>2. in laniēn<div class="area" data-key="ā">a</div></div>
+      <div>3. in balne<div class="area" data-key="ō">um</div></div>
+      <div>4. in īnsul<div class="area" data-key="ā">a</div> Mycon<div class="area" data-key="ō">us</div></div>
+      <div>5. in camp<div class="area" data-key="ō">us</div> Mārti<div class="area" data-key="ō">us</div></div>
+      <div>6. in vīc<div class="area" data-key="ō">us</div> angust<div class="area" data-key="ō">us</div></div>
+      <div>7. in aerāri<div class="area" data-key="ō">um</div> pūblic<div class="area" data-key="ō">um</div></div>
+      <div>8. in Galli<div class="area" data-key="ā">a</div> trānsalpīn<div class="area" data-key="ā">a</div></div>
       <div><br></div>
     </div>
     <div class="row">
