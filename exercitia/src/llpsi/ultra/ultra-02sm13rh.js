b44f95f7deed406a127c0a6354262cac01945f0d
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2024-12-18 03:39:05 +0300

    Add files via upload

--- exercitia/src/llpsi/ultra/ultra-02sm13rh.js
+++ exercitia/src/llpsi/ultra/ultra-02sm13rh.js
@@ -2,33 +2,47 @@ $_GET({
   title: 'Ultra 2',
   html: /*html*/`
     <div class="row">
-      <div><b>1. Дать форму генитива.</b></div>
+      <div><b>1. Dā genetīvum, numerō contentō.</b> Дать форму генитива, сохранив число.</div>
       <div><br></div>
-      <div>lacrim<b>a</b> → <div class="area" data-key="lacrimae"></div></div>
-      <div>lōr<b>a</b> <span class="ff"><span class="cb">pl</span></span> → <div class="area" data-key="lōrōrum"></div></div>
-      <div>soci<b>ī</b> → <div class="area" data-key="sociōrum"></div></div>
-      <div>mōnstr<b>um</b> → <div class="area" data-key="mōnstrī"></div></div>
-      <div>catēn<b>ae</b> → <div class="area" data-key="catēnārum"></div></div>
-      <div>castr<b>a</b> <span class="ff"><span class="cb">pl</span></span> → <div class="area" data-key="castrōrum"></div></div>
-      <div>mercēnāri<b>us</b> → <div class="area" data-key="mercēnāriī"></div></div>
-      <div>aper → <div class="area" data-key="aprī"></div></div>
+      <div>1. lacrim<b>a</b> → <div class="area" data-key="lacrimae"></div></div>
+      <div>2. lōr<b>a</b> <span class="ff"><span class="cb">pl</span></span> → <div class="area" data-key="lōrōrum"></div></div>
+      <div>3. soci<b>ī</b> → <div class="area" data-key="sociōrum"></div></div>
+      <div>4. mōnstr<b>um</b> → <div class="area" data-key="mōnstrī"></div></div>
+      <div>5. catēn<b>ae</b> → <div class="area" data-key="catēnārum"></div></div>
+      <div>6. castr<b>a</b> <span class="ff"><span class="cb">pl</span></span> → <div class="area" data-key="castrōrum"></div></div>
+      <div>7. mercēnāri<b>us</b> → <div class="area" data-key="mercēnāriī"></div></div>
+      <div>8. aper → <div class="area" data-key="aprī"></div></div>
       <div><br></div>
     </div>
     <div class="row">
-      <div><b>2. Определить форму слов, дать начальную форму.</b></div>
+      <div><b>2. Dēfīnītor fōrma (cāsus numersque), dator nōminātīvus singulāris. </b>Определить форму (падеж и число), дать номинатив единственного числа.</div>
       <div><br></div>
       <div class="sc">
-        <div><i><span class="ff">Exemplum</span></i><span style="white-space: pre;">:	</span><span class="ff">oppid<b>ō</b> magn<b>ō</b></span> <div class="area">abl sg</div> ← <div class="area">oppid<b>um</b> magn<b>um</b></div></div>
+      <div><i><span class="ff">Exemplum</span></i><span style="white-space: pre;">:	</span><span class="ff">oppid<b>ō</b> magn<b>ō</b></span> <div class="area">abl sg</div> ← <div class="area">oppid<b>um</b> magn<b>um</b></div></div>
       </div>
       <div><br></div>
-      <div>silv<b>ā</b> quiēt<b>ā</b> <div class="area" data-key="abl sg"></div> ← <div class="area" data-key="silva quiēta"></div></div>
-      <div>hort<b>ōrum</b> pulchr<b>ōrum</b> <div class="area" data-key="gen pl"></div> ← <div class="area" data-key="hortus pulcher"></div></div>
-      <div>foli<b>a</b> flāv<b>a</b> <div class="area" data-key="nom pl"></div> ← <div class="area" data-key="folium flāvum"></div></div>
-      <div>vir<b>ī</b> timid<b>ī</b> <div class="area" data-key="gen sg | nom pl"></div> ← <div class="area" data-key="vir timidus"></div></div>
-      <div>cist<b>ārum</b> plēn<b>ārum</b> <div class="area" data-key="gen pl"></div> ← <div class="area" data-key="cista plēna"></div></div>
-      <div>fabr<b>ī</b> perīt<b>ī</b> <div class="area" data-key="gen sg | nom pl"></div> ← <div class="area" data-key="faber perītus"></div></div>
-      <div>sax<b>ō</b> dūr<b>ō</b> <div class="area" data-key="abl sg"></div> ← <div class="area" data-key="saxum dūrum"></div></div>
-      <div>umbr<b>ae</b> ōminōs<b>ae</b> <div class="area" data-key="gen sg | nom pl"></div> ← <div class="area" data-key="umbra ōminōsa"></div></div>
+      <div>1. silv<b>ā</b> quiēt<b>ā</b> <div class="area" data-key="abl sg"></div> ← <div class="area" data-key="silva quiēta"></div></div>
+      <div>2. hort<b>ōrum</b> pulchr<b>ōrum</b> <div class="area" data-key="gen pl"></div> ← <div class="area" data-key="hortus pulcher"></div></div>
+      <div>3. foli<b>a</b> flāv<b>a</b> <div class="area" data-key="nom pl"></div> ← <div class="area" data-key="folium flāvum"></div></div>
+      <div>4. vir<b>ī</b> timid<b>ī</b> <div class="area" data-key="gen sg | nom pl"></div> ← <div class="area" data-key="vir timidus"></div></div>
+      <div>5. cist<b>ārum</b> plēn<b>ārum</b> <div class="area" data-key="gen pl"></div> ← <div class="area" data-key="cista plēna"></div></div>
+      <div>6. fabr<b>ī</b> perīt<b>ī</b> <div class="area" data-key="gen sg | nom pl"></div> ← <div class="area" data-key="faber perītus"></div></div>
+      <div>7. sax<b>ō</b> dūr<b>ō</b> <div class="area" data-key="abl sg"></div> ← <div class="area" data-key="saxum dūrum"></div></div>
+      <div>8. umbr<b>ae</b> ōminōs<b>ae</b> <div class="area" data-key="gen sg | nom pl"></div> ← <div class="area" data-key="umbra ōminōsa"></div><br><br></div>
+    </div>
+    <div class="row">
+      <div>❗ <b>3. Verte in sermōnem Latīnum. Sī necesse, ūtere dictiōnāriō.</b> Перевести на латынь. По необходимости используйте словарь.</div>
+      <div><br></div>
+      <div>1. <i>Юпитер — отец многих богов и героев.</i></div>
+      <div>I<div class="area" data-key="uppiter"></div> <div class="area" data-key="est"></div> p<div class="area" data-key="ater"></div> m<div class="area" data-key="ultōrum"></div> d<div class="area" data-key="eōrum"></div> hērōum<div class="area" data-key="que"></div>.</div>
+      <div>2. <i>Гектор — сын Приама, царя Трои.</i></div>
+      <div>H<div class="area" data-key="ector"></div> <div class="area" data-key="est"></div> f<div class="area" data-key="īlius"></div> P<div class="area" data-key="riamī"></div>, rēgis T<div class="area" data-key="rōiae"></div>.</div>
+      <div>3. <i>В библиотеке не много новых книг.</i></div>
+      <div><div class="area" data-key="in"></div> b<div class="area" data-key="ibliothēcā"></div> nōn m<div class="area" data-key="ultī"></div> s<div class="area" data-key="unt"></div> l<div class="area" data-key="ibrī"></div> n<div class="area" data-key="ovī"></div>.</div>
+      <div>4. <i>Вот книга античного поэта.</i></div>
+      <div><div class="area" data-key="ecce"></div> l<div class="area" data-key="iber"></div> p<div class="area" data-key="oētae"></div> a<div class="area" data-key="ntīquī"></div>.</div>
+      <div>5. <i>Кто такие Кастор и Поллукс? Кто их мать?</i></div>
+      <div>Q<div class="area" data-key="uī"></div> <div class="area" data-key="sunt"></div> C<div class="area" data-key="astor"></div> <div class="area" data-key="et"></div> P<div class="area" data-key="ollux"></div>? Q<div class="area" data-key="uae"></div> <div class="area" data-key="est"></div> m<div class="area" data-key="āter"></div> e<div class="area" data-key="ōrum"></div>?<br><br></div>
     </div>
   `
 });
