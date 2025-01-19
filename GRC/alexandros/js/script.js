67a9e77d57839a98001652c4f96026f658fb01fa
Le-Marin <119621011+Le-Marin@users.noreply.github.com>
2025-01-19 19:42:38 +0300

    03

--- GRC/alexandros/js/script.js
+++ GRC/alexandros/js/script.js
@@ -1,17 +1,19 @@
-import tip from './tip.js?v=2c';
+(function() {
+  'use strict';
 
-const thisScript = document.currentScript || [...document.scripts].pop();
-const src = (thisScript || {src: ''}).src;
-const ver = (src.match(/[?&]v=([^&]+)/) || [Date.now()])[1];
+  const thisScript = document.currentScript || [...document.scripts].pop();
+  const src = (thisScript || {src: ''}).src;
+  const ver = (src.match(/[?&]v=([^&]+)/) || [Date.now()])[1];
 
-if (location.protocol === 'file:') loadScript('_edit.js');
-if (!document.hasOwnProperty('_TIP_')) {
-  document._TIP_ = tip;
-  tip.__init__();
-}
+  if (location.protocol === 'file:') loadScript('_edit');
+  if (!document.hasOwnProperty('_TIP_')) {
+    let s = src.split('/').slice(0, -1).join('/');
+    loadScript(s + '/tip');
+  }
 
-function loadScript(src) {
-  const script = document.createElement('script');
-  script.src = src + '?v=' + ver;
-  document.head.appendChild(script).remove();
-}
+  function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src + '.js?v=' + ver;
+    document.head.appendChild(script).remove();
+  }
+})();
