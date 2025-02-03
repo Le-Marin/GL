d9fba4b9879c0e2811483056a29a7e9e1ebf4887
Le-Marin <119621011+Le-Marin@users.noreply.github.com>
2025-02-04 02:16:44 +0300

    add lex

--- lexicon/script.js
+++ lexicon/script.js
@@ -11,7 +11,8 @@
   const getDict = async (key) => vocabulary[key] || await loadDict(key);
 
   const vocabulary = {};
-  const regDictKey = /[a-f]/;
+  // a b c d e f g h i j k l m n o p q r s t u v w x y z
+  const regDictKey = /[a-oq]/;
   const cases = {'ā': 'a', 'ē': 'e', 'ī': 'i', 'ō': 'o', 'ū': 'u', 'ȳ': 'y'};
 
   const cont = document.getElementById('content');
---
+++ lexicon/vocabulary/k.js
@@ -0,0 +1,7 @@
+export default [
+  ["K", "<big>K</big>\n\n<b>K, k</b>\n<i>одиннадцатая буква латинского алфавита, употребительная в древнейшие времена, но постепенно вытесненная буквой</i> c; <i>в сокращениях</i>: K. = Kaeso (Caeso); K. (Kal.) = Calendae."],
+  ["Kaeso", "<big>Kaeso</big>\n\n<b>Kaeso</b> <i>v. l.</i>\n= Caeso."],
+  ["Kalendae", "<big>Kalendae</big>\n\n<b>Kalendae</b> <i>v. l.</i>\n= Calendae."],
+  ["Karthago", "<big>Karthago</big>\n\n<b>Karth&#257;go</b> <i>v. l.</i>\n= Carthago."],
+  ["koppa", "<big>koppa</big>\n\n<b>koppa</b> <i>n indecl.</i>\nкоппа, <i>арх. греч. буква &#985;, превратившаяся у римлян в</i> q (<i>числовое достоинство = 90</i>) <i>Q</i>."]
+]
