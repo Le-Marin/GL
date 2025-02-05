7dc940b95f9a36d089d3092f0cc406957b6ff28c
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-02-05 20:24:52 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/capitulum/capit-26ha2rnv.js
@@ -0,0 +1,116 @@
+$_GET({
+  title: 'Exercitia Latina 26',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>I</div>
+      <div><br></div>
+      <div><b>Exercitium 1</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">celer -eris -ere</div></div></div>
+      <div><br></div>
+      <div>1. Asinus nōn tam cel<div class="area" data-key="er"></div> est quam equus.</div>
+      <div>2. Aquila est avis magna et cel<div class="area" data-key="eris"></div>.</div>
+      <div>3. Nūllus deus tam cel<div class="area" data-key="er"></div> est quam Mercurius.</div>
+      <div>4. Fuga cel<div class="area" data-key="eris"></div> hostes perterritōs servavit.</div>
+      <div>5. Ventus cel<div class="area" data-key="er"></div> et subitus mare turbāvit.</div>
+      <div>6. Nihil tam cel<div class="area" data-key="ere"></div> est quam fulgur.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 2</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">audāx</div> <div class="area">carcer</div> <div class="area">celer</div> <div class="area">cōnficere</div> <div class="area">cōnsequī</div> <div class="area">cōnsilium</div> <div class="area">cōnsūmere</div> <div class="area">effugere</div> <div class="area">fuga</div> <div class="area">haud</div> <div class="area">invenīre</div> <div class="area">iuvāre</div> <div class="area">paene</div> <div class="area">persequī</div> <div class="area">quisquam</div> <div class="area">quoniam</div> <div class="area">reliquus</div> <div class="area">vērum</div></div></div>
+      <div><br></div>
+      <div>1. Rēx Mīnōs Thēseum fugientem <div class="area" data-key="persecūtus"></div> est.</div>
+      <div>2. Nāvis Mīnōis nōn tam <div class="area" data-key="celeris"></div> fuit quam Thēseī.</div>
+      <div>3. Itaque rēx Thēseum <div class="area" data-key="cōnsequī"></div> nōn potuit.</div>
+      <div>4. Daedalus, quī fīlum <div class="area" data-key="cōnfēcit"></div> et Ariadnae dederat, ā rēge in labyrinthum inclūsus est sīcut in <div class="area" data-key="carcere"></div>.</div>
+      <div>5. Hīc Syra, quae <div class="area" data-key="haud"></div> paulum temporis iam <div class="area" data-key="cōnsūmpsit"></div> in nārrandō, fīnem facere vult.</div>
+      <div>6. <div class="area" data-key="vērum"></div> Quīntus, quī magnam fābulae partem iam audīvit, partem <div class="area" data-key="reliquam"></div> quoque audīre cupit.</div>
+      <div>7. Syra igitur dē <div class="area" data-key="fugā"></div> Daedalī et Īcarī nārrat.</div>
+      <div>8. Daedalus exitum labyrinthī <div class="area" data-key="invenīre"></div> nōn potuit, neque <div class="area" data-key="quisquam"></div> eum <div class="area" data-key="iūvit"></div>.</div>
+      <div>9. <div class="area" data-key="quoniam"></div> igitur aliā viā exīre nōn potuit, vir <div class="area" data-key="audāx"></div> per āera <div class="area" data-key="effugere"></div> cōnstituit.</div>
+      <div>10. Hoc <div class="area" data-key="cōnsilium"></div> adhūc ab Īcarō ignōrābātur.</div>
+      <div>11. Puer fessus ‘sē <div class="area" data-key="paene"></div> mortuum esse’ dīxit.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>II</div>
+      <div><br></div>
+      <div><b>Exercitium 4</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-andum -ī -ō</div></div>
+      <div><div class="area">-endum -ī -ō</div></div>
+      <div><div class="area">-endum -ī -ō</div></div>
+      <div><div class="area">-iendum -ī -ō</div></div></div>
+      <div><br></div>
+      <div>1. Nautae parātī sunt ad nāvig<div class="area" data-key="andum"></div>.</div>
+      <div>2. Syra fīnem nārr<div class="area" data-key="andī"></div> facit quia fessa est nārr<div class="area" data-key="andō"></div>.</div>
+      <div>3. Puer autem cupidus est aud<div class="area" data-key="iendī"></div> neque parātus ad dorm<div class="area" data-key="iendum"></div>. </div>
+      <div>4. Stilus necessārius est ad scrīb<div class="area" data-key="endum"></div>.</div>
+      <div>5. Sextus industrius est in scrīb<div class="area" data-key="endō"></div> et in comput<div class="area" data-key="andō"></div>: is enim cupidus disc<div class="area" data-key="endī"></div> est.</div>
+      <div>6. Puerī scrībere discunt scrīb<div class="area" data-key="endō"></div>; ars scrīb<div class="area" data-key="endī"></div> haud facilis est.</div>
+      <div>7. Capitulum XVI est dē marī et dē nāvig<div class="area" data-key="andō"></div>.</div>
+      <div>8. Iūlia dēlectātur in hortō ambul<div class="area" data-key="andō"></div> et flōrēs aspici<div class="area" data-key="endō"></div>.</div>
+      <div>9. Mēdus cōnsilium ā dominō fugi<div class="area" data-key="endī"></div> excōgitāverat.</div>
+      <div>10. Daedalus fīlium, quī vol<div class="area" data-key="andī"></div> studiōsus erat, artem vol<div class="area" data-key="andī"></div> docuit.</div>
+      <div>11. Paulum satis est ad beātē vīv<div class="area" data-key="endum"></div>.</div>
+      <div>12. Quōmodo loquī discimus? Loquī discimus aliōs loquentēs aud<div class="area" data-key="iendō"></div> et imit<div class="area" data-key="andō"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 5</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">ars</div> <div class="area">cautus</div> <div class="area">deorsum</div> <div class="area">ēvolāre</div> <div class="area">excōgitāre</div> <div class="area">fīgere</div> <div class="area">ignis</div> <div class="area">imitārī</div> <div class="area">īnfimus</div> <div class="area">ingēns</div> <div class="area">lacertus</div> <div class="area">levāre</div> <div class="area">līber</div> <div class="area">mollīre</div> <div class="area">nātūra</div> <div class="area">opus</div> <div class="area">orbis</div> <div class="area">penna</div> <div class="area">perficere</div> <div class="area">quidem</div> <div class="area">studiōsus</div> <div class="area">summus</div> <div class="area">sūrsum</div> <div class="area">tamquam</div> <div class="area">trāns</div> <div class="area">ūrere</div></div></div>
+      <div><br></div>
+      <div>1. Quī in carcere inclūsus est nōn est <div class="area" data-key="līber"></div>.</div>
+      <div>2. Avis <div class="area" data-key="trāns"></div> montēs et flūmina volāre potest.</div>
+      <div>3. Aquila circum labyrinthum in magnum <div class="area" data-key="orbem"></div> volābat.</div>
+      <div>4. Daedalus cōnsilium fugae iam <div class="area" data-key="excōgitāvit"></div>: ālās facere cōnstituerat atque ē labyrinthō <div class="area" data-key="ēvolāre"></div>.</div>
+      <div>5. Ālās cōnficere <div class="area" data-key="opus"></div> difficile est, sed Daedalus ālās <div class="area" data-key="ingentēs"></div> cōnfēcit ex <div class="area" data-key="pennīs"></div>, quās cērā in lacertīs <div class="area" data-key="fīxit"></div>.</div>
+      <div>6. Ille <div class="area" data-key="quidem"></div> deus nōn erat, sed tamen arte suā ipsam <div class="area" data-key="nātūram"></div> mūtāre poterat.</div>
+      <div>7. “Opus iam <div class="area" data-key="cōnfectum"></div> est,” inquit Daedalus. “Ecce exemplum <div class="area" data-key="artis"></div> meae novissimum. Iam avēs in volandō <div class="area" data-key="imitārī"></div> possumus.”</div>
+      <div>8. Puer, etsī <div class="area" data-key="studiōsus"></div> erat volandī, sē suprā humum <div class="area" data-key="levāre"></div> nōn potuit.</div>
+      <div>9. Pater fīlium ālās <div class="area" data-key="sūrsum"></div> <div class="area" data-key="deorsum"></div> movēre docuit <div class="area" data-key="tamquam"></div> avis.</div>
+      <div>10. Tum fīlium sīc monuit: “Nōlī volāre in <div class="area" data-key="īnfimō"></div> āere prope mare neque in <div class="area" data-key="summō"></div> aere prope sōlem, nam <div class="area" data-key="ignis"></div> sōlis cēram <div class="area" data-key="mollīre"></div> ac pennās <div class="area" data-key="ūrere"></div> poterit.</div>
+      <div>11. <div class="area" data-key="lacertus"></div> est pars bracchiī superior.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>III</div>
+      <div><br></div>
+      <div><b>Exercitium 7</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>participium perfectī āctīvī (ex dēpōnentibus)</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Thēseus, postquam ē Crētā profectus est, Naxum vēnit</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span>= Thēseus ē Crētā <b>profectus</b> Naxum vēnit.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span>Quīntus, quī cultrum medicī verēbātur, oculōs clausit</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span>= Quīntus cultrum medicī <b>veritus</b> oculōs clausit.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Mīnōs Thēseum <div class="area" data-key="persecūtus"></div> [= postquam Thēseum persecūtus est] domum revertit.</div>
+      <div>2. Daedalus fīlium <div class="area" data-key="ōsculātus"></div> [= postquam fīlium ōsculātus est] “Iam sequere mē!” inquit.</div>
+      <div>3. Ita <div class="area" data-key="locūtus"></div> [= postquam ita locūtus est] Daedalus cum fīliō ēvolāvit.</div>
+      <div>4. Fīlum Ariadnae <div class="area" data-key="secūtus"></div> [= postquam fīlum Ariadnae secūtus est] Thēseus exitum labyrinthī repperit.</div>
+      <div>5. Tabellārius canem <div class="area" data-key="veritus"></div> [= quī canem verēbātur] intrāre nōluit.</div>
+      <div>6. Aegeus fīlium mortuum esse <div class="area" data-key="arbitrātus"></div> [= quī fīlium mortuum esse arbitrābātur] in mare sē iēcit.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 8</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">aberrāre</div> <div class="area">accidere</div> <div class="area">cāsus</div> <div class="area">dēspicere</div> <div class="area">causā</div> <div class="area">lībertās</div> <div class="area">maximē</div> <div class="area">multitūdō</div> <div class="area">orbis (terrārum)</div> <div class="area">paenīnsula</div> <div class="area">propinquus</div> <div class="area">quatere</div> <div class="area">revocāre</div> <div class="area">sīn</div> <div class="area">suspicere</div> <div class="area">temerārius</div> <div class="area">vērum (etiam)</div> <div class="area">vidērī</div></div></div>
+      <div><br></div>
+      <div>1. Peloponnēsus nōn īnsula, sed <div class="area" data-key="paenīnsula"></div> est.</div>
+      <div>2. Daedalus et Īcarus ē labyrinthō ēvolantēs novā <div class="area" data-key="lībertāte"></div> gaudēbant ac <div class="area" data-key="multitūdinem"></div> īnsulārum mīrābantur.</div>
+      <div>3. Īcarus <div class="area" data-key="dēspiciēns"></div> paene tōtum <div class="area" data-key="orbem"></div> terrārum vidēre poterat, <div class="area" data-key="suspiciēns"></div> sōlem lūcentem vīdit.</div>
+      <div>4. Puer <div class="area" data-key="temerārius"></div> in summum caelum ascendit.</div>
+      <div>5. Tum <div class="area" data-key="accidit"></div> id quod pater timuerat: sōl <div class="area" data-key="propinquus"></div> cēram mollīvit et pennās ussit.</div>
+      <div>6. Puer, lacertōs nūdōs <div class="area" data-key="quatiēns"></div>, in mare cecidit.</div>
+      <div>7. Haec fābula nōn sōlum dēlectandī, <div class="area" data-key="vērum"></div> etiam monendī <div class="area" data-key="causā"></div> nārrātur.</div>
+      <div>8. Ex omnibus fābulīs haec dē <div class="area" data-key="cāsū"></div> Īcarī Quīntum <div class="area" data-key="maximē"></div> dēlectat.</div>
+      <div>9. Puer fessus Syram abeuntem nōn <div class="area" data-key="revocat"></div>.</div>
+      <div>10. Rēs quae procul absunt parvae esse <div class="area" data-key="videntur"></div>.</div>
+      <div>11. In somnīs Quīntus volāre sibi <div class="area" data-key="vidētur"></div>.</div>
+      <div>12. Sī industrius es, multum discis; <div class="area" data-key="sīn"></div> piger es, nihil discis.</div>
+      <div>13. Nōlī ā rēctā viā <div class="area" data-key="aberrāre"></div>!</div>
+    </div>
+  `
+});
