a3a0df60375139bbd11c69a22cc59494a9c48d90
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-02-11 22:30:29 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/capitulum/capit-27grwh6s.js
@@ -0,0 +1,137 @@
+$_GET({
+  title: 'Exercitia Latina 27',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>I</div>
+      <div><br></div>
+      <div><b>Exercitium 1</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Quō īnstrūmentō agricola metit? Falce metit.</span></div>
+      <div><span class="ff"><span style="white-space: pre;">		</span>Quī metit, falce ūtitur.</span></div>
+      <div><br></div>
+      <div>1. Discipulus stil<div class="area" data-key="ō"></div> in tabulā scrībit.</div>
+      <div>2. Discipulus quī scrībit, stil<div class="area" data-key="ō"></div> et tabul<div class="area" data-key="ā"></div> ūtitur.</div>
+      <div>3. Mīles gladi<div class="area" data-key="ō"></div> et scūt<div class="area" data-key="ō"></div> et ali<div class="area" data-key="īs"></div> arm<div class="area" data-key="īs"></div> pugnat.</div>
+      <div>4. Mīles pugnāns gladi<div class="area" data-key="ō"></div> et scūt<div class="area" data-key="ō"></div> et ali<div class="area" data-key="īs"></div> arm<div class="area" data-key="īs"></div> ūtitur.</div>
+      <div>5. Quī in aliam terram it, aut nāv<div class="area" data-key="ē"></div> aut curr<div class="area" data-key="ū"></div> aut equ<div class="area" data-key="ō"></div> vehitur.</div>
+      <div>6. Avis volāns āl<div class="area" data-key="īs"></div> ūtitur; homō ambulāns ūtitur ped<div class="area" data-key="ibus"></div>.</div>
+      <div>7. In loquendō lingu<div class="area" data-key="ā"></div> ūtimur, aur<div class="area" data-key="ibus"></div> ūtimur in audiendō.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 2</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">ager</div> <div class="area">agere</div> <div class="area">agricola</div> <div class="area">amoenus</div> <div class="area">arāre</div> <div class="area">arātor</div> <div class="area">arātrum</div> <div class="area">cingere</div> <div class="area">colere</div> <div class="area">cōpia</div> <div class="area">crēscere</div> <div class="area">dēnique</div> <div class="area">falx</div> <div class="area">fertilis</div> <div class="area">frūgēs</div> <div class="area">frūmentum</div> <div class="area">īnstrūmentum</div> <div class="area">invehere</div> <div class="area">mātūrus</div> <div class="area">metere</div> <div class="area">negōtium</div> <div class="area">parum</div> <div class="area">pāscere</div> <div class="area">pecus</div> <div class="area">prae</div> <div class="area">regiō</div> <div class="area">rudis</div> <div class="area">sēmen</div> <div class="area">serere</div> <div class="area">spargere</div> <div class="area">ūtī</div> <div class="area">ūva</div> <div class="area">-ve</div> <div class="area">vīnum</div> <div class="area">vītis</div></div></div>
+      <div><br></div>
+      <div>1. Hortus Iūliī <div class="area" data-key="amoenus"></div> agrīs <div class="area" data-key="cingitur"></div>.</div>
+      <div>2. Mēnse Augustō <div class="area" data-key="frūmentum"></div> metitur; deinde <div class="area" data-key="agrī"></div> arantur.</div>
+      <div>3. Vir cuius <div class="area" data-key="negōtium"></div> est agrōs colere <div class="area" data-key="colōnus"></div> dīcitur.</div>
+      <div>4. Agricola quī <div class="area" data-key="arat"></div> arātrō <div class="area" data-key="ūtitur"></div>.</div>
+      <div>5. Arātrum est <div class="area" data-key="īnstrūmentum"></div> quō agricola arat.</div>
+      <div>6. <div class="area" data-key="arātor"></div> duōs bovēs, quī <div class="area" data-key="arātrum"></div> trahunt, <div class="area" data-key="prae"></div> sē <div class="area" data-key="agit"></div>.</div>
+      <div>7. Agricola quī serit, sēmen manū <div class="area" data-key="spargit"></div>.</div>
+      <div>8. Novum frūmentum <div class="area" data-key="crēscit"></div> ex parvīs <div class="area" data-key="sēminibus"></div>.</div>
+      <div>9. Agricolae mēnse Augustō frūmentum <div class="area" data-key="metere"></div> solent, quia eō mēnse frūmentum <div class="area" data-key="mātūrum"></div> est.</div>
+      <div>10. <div class="area" data-key="falx"></div> est instrūmentum quō frūmentum metitur.</div>
+      <div>11. Agricolae prīmum arant, deinde <div class="area" data-key="serunt"></div>, <div class="area" data-key="dēnique"></div> metunt: ita agricolae agrōs suōs <div class="area" data-key="colunt"></div>.</div>
+      <div>12. Agrī Iūliī, quī magnās <div class="area" data-key="frūgēs"></div> ferunt, <div class="area" data-key="fertilēs"></div> sunt.</div>
+      <div>13. In campīs quī nōn arantur ovēs, porcī, bovēs <div class="area" data-key="pāscuntur"></div>.</div>
+      <div>14. Facilius est <div class="area" data-key="pecus"></div> pāscere quam agrōs colere.</div>
+      <div>15. In Italiā <div class="area" data-key="parum"></div> frūmentī metitur; itaque magna <div class="area" data-key="cōpia"></div> frūmentī ex Āfricā in Italiam <div class="area" data-key="invehitur"></div>.</div>
+      <div>16. Aegyptī bis ter<div class="area" data-key=" ve"></div> in annō metunt.</div>
+      <div>17. Latium dīcitur ea <div class="area" data-key="regiō"></div> in quā urbs Rōma sita est.</div>
+      <div>18. Antīquī Latīnī hominēs <div class="area" data-key="rudēs"></div> ac barbarī erant.</div>
+      <div>19. Vīneae sunt agrī quī <div class="area" data-key="vītēs"></div> ferunt.</div>
+      <div>20. <div class="area" data-key="vīnum"></div> efficitur ex frūgibus vītium, quae <div class="area" data-key="ūvae"></div> dīcuntur.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>II</div>
+      <div><br></div>
+      <div><b>Exercitium 4</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Pater fīlium surgere et st<b>āre</b>, tac<b>ēre</b> et aud<b>īre</b> <i>iubet</i> =</span></div>
+      <div><span class="ff"><span style="white-space: pre;">		</span>Pater fīliō <i>imperat</i> ut surg<b>at</b> et st<b>et</b>, tace<b>at</b> et audi<b>at</b>.</span></div>
+      <div><br></div>
+      <div>1. Magister discipulōs cōnsīdere et tacēre iubet = Magister discipulīs imperat ut <div class="area" data-key="cōnsīdant et taceant"></div>.</div>
+      <div>2. Magister Mārcum surgere et respondēre iubet = <div class="area" data-key="Magister Mārcō imperat"></div> ut <div class="area" data-key="surgat et respondeat"></div>.</div>
+      <div>3. Domina ancillam intrāre iubet = <div class="area" data-key="Domina ancillae imperat"></div> ut <div class="area" data-key="intret"></div>.</div>
+      <div>4. Gubernātor nautās aquam haurīre iubet = <div class="area" data-key="Gubernātor nautīs imperat"></div> ut <div class="area" data-key="aquam hauriant"></div>.</div>
+      <div>5. Dominus servōs industriē labōrāre iubet = <div class="area" data-key="Dominus servīs imperat"></div> ut <div class="area" data-key="labōrent"></div>.</div>
+      <div>6. Iūlius iānitōrem vigilāre et domum bene cūstōdīre iubet = <div class="area" data-key="Iūlius iānitōrī imperat"></div> ut <div class="area" data-key="vigilet et domum bene cūstōdiat"></div>.</div>
+      <div>7. Dux mīlitēs oppidum hostium oppugnāre iubet = <div class="area" data-key="Dux mīlitibus imperat"></div> ut <div class="area" data-key="oppidum hostium oppugnent"></div>.</div>
+      <div>8. Iūlius servum medicum arcessere iubet = <div class="area" data-key="Iūlius servō imperat"></div> ut <div class="area" data-key="medicum arcessat"></div>.</div>
+      <div>9. Medicus Quīntum ōs aperīre et linguam ostendere iubet = <div class="area" data-key="Medicus Quīntō imperat"></div> ut <div class="area" data-key="ōs aperiat et linguam ostendat"></div>.</div>
+      <div>10. Medicus Iūlium bracchium puerī tenēre iubet = <div class="area" data-key="Medicus Iūliō imperat"></div> ut <div class="area" data-key="bracchium puerī teneat"></div>.</div>
+      <div>11. Iūlius servōs cōnsistere et manēre iubet = <div class="area" data-key="Iūlius servīs imperat"></div> ut <div class="area" data-key="cōnsistant et maneant"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 5</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">abs</div> <div class="area">cūra</div> <div class="area">cēnsēre</div> <div class="area">circā</div> <div class="area">colōnus</div> <div class="area">exīstimāre</div> <div class="area">gravida</div> <div class="area">labor</div> <div class="area">labōrāre</div> <div class="area">nē... quidem</div> <div class="area">neglegere</div> <div class="area">ōrāre</div> <div class="area">ōtium</div> <div class="area">patiēns</div> <div class="area">patientia</div> <div class="area">prae</div> <div class="area">praedium</div> <div class="area">precēs</div> <div class="area">prō</div> <div class="area">prōicere</div> <div class="area">quīdam</div> <div class="area">quiēscere</div> <div class="area">rapere</div> <div class="area">rūs</div> <div class="area">suburbānus</div> <div class="area">trīcēsimus</div> <div class="area">urbānus</div> <div class="area">ut</div></div></div>
+      <div><br></div>
+      <div>1. Iūlius <div class="area" data-key="praedium"></div> habet sub monte Albānō.</div>
+      <div>2. Multī dominī Rōmānī, quī ipsī in agrīs nōn <div class="area" data-key="labōrant"></div>, <div class="area" data-key="labōrem"></div> agricolārum indignum esse <div class="area" data-key="exīstimant | cēnsent"></div>.</div>
+      <div>3. Iūlius in praediō suō <div class="area" data-key="suburbānō"></div> nōn labōrat, sed <div class="area" data-key="quiēscit"></div>.</div>
+      <div>4. Itaque sē <div class="area" data-key="prae"></div> agricolīs beātum esse <div class="area" data-key="cēnset | exīstimat"></div>.</div>
+      <div>5. Rōmae Iūlius multa negōtia <div class="area" data-key="urbāna"></div> cūrāre dēbet, neque ūllum <div class="area" data-key="ōtium"></div> eī est.</div>
+      <div>6. Post merīdiem dominus <div class="area" data-key="circā"></div> agrōs et vīneās ambulat.</div>
+      <div>7. Nōn urbs, sed <div class="area" data-key="rūs"></div> amoenum et quiētum Iūlium dēlectat.</div>
+      <div>8. <div class="area" data-key="colōnus"></div> est agricola quī aliēnōs agrōs <div class="area" data-key="prō"></div> dominō colit.</div>
+      <div>9. <div class="area" data-key="quīdam"></div> colōnus DCCC sēstertiōs Iūliō dēbet.</div>
+      <div>10. Iūlius colōnō imperat <div class="area" data-key="ut"></div> mercēdem solvat: “Solve mercēdem, quam iam saepe <div class="area" data-key="abs"></div> tē poposcī!”</div>
+      <div>11. Colōnus pauperrimus, quī <div class="area" data-key="nē"></div> nummum <div class="area" data-key="quidem"></div> possidet, sē ad pedēs dominī <div class="area" data-key="prōicit"></div>; miser colōnus dominum <div class="area" data-key="ōrat"></div> ut patientiam habeat.</div>
+      <div>12. Iūlius: “Vir <div class="area" data-key="patiēns"></div> sum, sed hic fīnis est patientiae meae!”</div>
+      <div>13. Colōnus: “Nōlī mē ab uxōre gravidā <div class="area" data-key="rapere"></div>! Per omnēs deōs tē ōrō.”</div>
+      <div>14. Iūlius <div class="area" data-key="precibus"></div> colōnī movētur: colōnō imperat ut uxōrem <div class="area" data-key="gravidam"></div> cūret ac mercēdem intrā <div class="area" data-key="trīcēsimum"></div> diem solvat.</div>
+      <div>15. <div class="area" data-key="cūra"></div> īnfantium multum temporis magnamque <div class="area" data-key="patientiam"></div> postulat.</div>
+      <div>16. Bona māter īnfantēs suōs nōn <div class="area" data-key="neglegit"></div>, sed bene cūrat.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>III</div>
+      <div><br><b>Exercitium 7</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>coniūnctīvus praesentis āctīvī</i></span></div>
+      <div><br></div>
+      <div>1. Iūlius colōnō imperat ut mercēdem solv<div class="area" data-key="at"></div>: “Imperō tibi ut mercēdem solv<div class="area" data-key="ās"></div>!”</div>
+      <div>2. Iūlius: “Hīs litterīs magister mē monet ut mercēdem solv<div class="area" data-key="am"></div>.”</div>
+      <div>3. Dux mīlitēs monet ut fortiter pugn<div class="area" data-key="ent"></div> et hostēs vinc<div class="area" data-key="ant"></div>: “Moneō vōs ut fortiter pugn<div class="area" data-key="ētis"></div> et hostēs vinc<div class="area" data-key="ātis"></div>!”</div>
+      <div>4. Mīles fortissimus: “Audīte, amīcī: dux nōs monet ut fortiter pugn<div class="area" data-key="ēmus"></div> et hostēs vinc<div class="area" data-key="āmus"></div>.”</div>
+      <div>5. Iūlius ad amīcum absentem scrībit: “Tē ōrō ut ad mē scrīb<div class="area" data-key="ās"></div> aut ipse veni<div class="area" data-key="ās"></div>. Cūrā ut vale<div class="area" data-key="ās"></div> et mē am<div class="area" data-key="ēs"></div>!”</div>
+      <div>6. Iūlia: “Māter semper mē monet ut cauta s<div class="area" data-key="im"></div>, nec umquam monet vōs, puerī, ut cautī s<div class="area" data-key="ītis"></div>.”</div>
+      <div>7. Mārcus: “Māter tē monet ut cauta s<div class="area" data-key="īs"></div> quia tū cauta nōn es; nōs vērō cautī sumus, itaque opus nōn est nōs monēre ut cautī s<div class="area" data-key="īmus"></div>.”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 8</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>coniūnctīvus praesentis passīvī</i></span></div>
+      <div><br></div>
+      <div>1. Dominus imperat ut servus voc<div class="area" data-key="ētur"></div> et interrog<div class="area" data-key=" ētur"></div> (= Dominus servum voc<div class="area" data-key="ārī"></div> et interrog<div class="area" data-key="ārī"></div> iubet).</div>
+      <div>2. Iūlius imperat ut pāstor prehend<div class="area" data-key="ātur"></div> et tene<div class="area" data-key="ātur"></div> (= Iūlius pāstōrem prehend<div class="area" data-key="ī"></div> et ten<div class="area" data-key="ērī"></div> iubet).</div>
+      <div>3. Colōnus: “Cūrā ut ovēs tuae ab agrīs prohibe<div class="area" data-key="antur"></div>!”</div>
+      <div>4. Dux nūntium monet ut cautē ex castrīs ēgredi<div class="area" data-key="ātur"></div> et ōret ut sine morā sibi auxilium mitt<div class="area" data-key="ātur"></div>.</div>
+      <div>5. Dux: “Cavē nē vide<div class="area" data-key="āris"></div> nēve audi<div class="area" data-key="āris"></div> ab hostibus!”</div>
+      <div>6. Māter fīlium monet ut bene lav<div class="area" data-key="ētur"></div>: “Cūrā ut bene lav<div class="area" data-key="ēris"></div>, Mārce, nēve sordidīs manibus proficīsc<div class="area" data-key="āris"></div>!”</div>
+      <div>7. Mārcus: “Dāvus semper cūrat ut ego bene lav<div class="area" data-key="er"></div> nēve sordidīs manibus proficīsc<div class="area" data-key="ar"></div>.”</div>
+      <div>8. Māter fīliōs monet ut bene lav<div class="area" data-key="entur"></div>: “Cūrāte ut bene lav<div class="area" data-key="ēminī"></div>, fīliī, nēve sordidīs manibus proficīsc<div class="area" data-key="āminī"></div>.”</div>
+      <div>9. Mārcus: “Dāvus semper cūrat ut nōs bene lav<div class="area" data-key="ēmur"></div> nēve sordidīs manibus proficīsc<div class="area" data-key="āmur"></div>.”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 9</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">calor</div> <div class="area">frīgus</div> <div class="area">grex</div> <div class="area">immātūrus</div> <div class="area">inhūmānus</div> <div class="area">nē</div> <div class="area">nēquam</div> <div class="area">nēve</div> <div class="area">nocēre</div> <div class="area">prōdesse</div> <div class="area">prohibēre</div> <div class="area">quidquam</div> <div class="area">rūsticus</div> <div class="area">siccus</div></div></div>
+      <div><br></div>
+      <div>1. Iūlius agricolās interrogat dē rēbus <div class="area" data-key="rūsticīs"></div>.</div>
+      <div>2. <div class="area" data-key="calor"></div> sōlis efficit ut vīnum bonum fīat.</div>
+      <div>3. Nihil vīneīs magis <div class="area" data-key="prōdest"></div> quam sōl et calor, neque <div class="area" data-key="quidquam"></div> iīs magis <div class="area" data-key="nocet"></div> quam imber et <div class="area" data-key="frīgus"></div>.</div>
+      <div>4. Solum nōn ūmidum, sed <div class="area" data-key="siccum"></div> est.</div>
+      <div>5. <div class="area" data-key="grex"></div> est magnus numerus bēstiārum quae simul pāscuntur.</div>
+      <div>6. Pāstor cūrat <div class="area" data-key="nē"></div> ovēs ē grege aberrent <div class="area" data-key="nēve"></div> ā lupō rapiantur.</div>
+      <div>7. Servus quī officium suum neglegit <div class="area" data-key="nēquam"></div> est.</div>
+      <div>8. Pāstor ovēs, quae frūmentum <div class="area" data-key="immātūrum"></div> carpere volunt, ab agrīs <div class="area" data-key="prohibēre"></div> dēbet.</div>
+      <div>9. Canis cavet <div class="area" data-key="nē"></div> ūlla ovis ē grege aberret.</div>
+      <div>10. Iūlius, quamquam sevērus exīstimātur, tamen <div class="area" data-key="inhūmānus"></div> nōn est.</div>
+    </div>
+  `
+});
