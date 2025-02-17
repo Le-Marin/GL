8438b211fdc925bd461ca2402d60d9234fa9dcc3
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-02-17 18:56:39 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/capitulum/capit-28p5wtxo.js
@@ -0,0 +1,146 @@
+$_GET({
+  title: 'Exercitia Latina 28',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>I</div>
+      <div><br></div>
+      <div><b>Exercitium 1</b></div>
+      <div><br></div>
+      <div><span class="ff">-gintā → -(g)ēsimus</span></div>
+      <div><span class="ff">-centī / -gentī → -centēsimus / gentēsimus</span></div>
+      <div><br></div>
+      <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>vīgintī annīs post = vīcēsimō annō post</span></div>
+      <div><br></div>
+      <div>1. <div class="area" data-key="trīgintā"></div> [XXX] annīs post = <div class="area" data-key="trīcēsimō"></div> annō post</div>
+      <div>2. <div class="area" data-key="quadrāgintā"></div> [XL] annīs post = <div class="area" data-key="quadrāgēsimō"></div> annō post</div>
+      <div>3. <div class="area" data-key="quīnquāgintā"></div> [L] annīs post = <div class="area" data-key="quīnguāgēsimō"></div> annō post</div>
+      <div>4. <div class="area" data-key="sexāginta"></div> [LX] annīs post = <div class="area" data-key="sexāgēsimō"></div> annō post</div>
+      <div>5. <div class="area" data-key="septuāgintā"></div> [LXX] annīs post = <div class="area" data-key="septuāgēsimō"></div> annō post</div>
+      <div>6. <div class="area" data-key="octōgintā"></div> [LXXX] annīs post = <div class="area" data-key="octōgēsimō"></div> annō post</div>
+      <div>7. <div class="area" data-key="nōnāgintā"></div> [XC] annīs post = <div class="area" data-key="nōnāgēsimō"></div> annō post.</div>
+      <div>8. <div class="area" data-key=" centum"></div> [C] annīs post = <div class="area" data-key="centēsimō"></div> annō post.</div>
+      <div>9. <div class="area" data-key="ducentīs"></div> [CC] annīs post = <div class="area" data-key="ducentēsimō"></div> annō post.</div>
+      <div>10. <div class="area" data-key="trecentīs"></div> [CCC] annīs post = <div class="area" data-key="trecentēsimō"></div> annō post.</div>
+      <div>11. <div class="area" data-key="quadringentīs"></div> [CCCC] annīs post = <div class="area" data-key="quadringentēsimō"></div> annō post.</div>
+      <div>12. <div class="area" data-key="quīngnetīs"></div> [D] annīs post = <div class="area" data-key="quīngentēsimō"></div> annō post.</div>
+      <div>13. <div class="area" data-key="sescentīs"></div> [DC] annīs post = <div class="area" data-key="sescentēsimō"></div> annō post.</div>
+      <div>14. <div class="area" data-key="septingentīs"></div> [DCC] annīs post = <div class="area" data-key="septingentēsimō"></div> annō post.</div>
+      <div>15. <div class="area" data-key="octingentīs"></div> [DCCC] annīs post = <div class="area" data-key="octingentēsimō"></div> annō post.</div>
+      <div>16. <div class="area" data-key="nōngentīs"></div> [CM] annīs post = <div class="area" data-key="nōngentēsimō"></div> annō post.</div>
+      <div>17. <div class="area" data-key="mīlle"></div> [M] annīs post = <div class="area" data-key="mīllēsimō"></div> annō post.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 2</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">adōrāre</div> <div class="area">animus</div> <div class="area">apprehendere</div> <div class="area">caecus</div> <div class="area">claudus</div> <div class="area">dictum</div> <div class="area">disiungere</div> <div class="area">ēicere</div> <div class="area">eō</div> <div class="area">extendere</div> <div class="area">fāma</div> <div class="area">fretum</div> <div class="area">immortālis</div> <div class="area">libellus</div> <div class="area">memorāre</div> <div class="area">morī</div> <div class="area">mortālis</div> <div class="area">mūtus</div> <div class="area">nāscī</div> <div class="area">nātus</div> <div class="area">oboedīre</div> <div class="area">surdus</div> <div class="area">turba</div> <div class="area">ūniversus</div> <div class="area">utrum</div> <div class="area">velut</div></div></div>
+      <div><br></div>
+      <div>1. <div class="area" data-key="fretum"></div> angustum Siciliam ab Italiā <div class="area" data-key="disiungit"></div>.</div>
+      <div>2. Tempestāte maximā necesse fuit mercēs ē nāve <div class="area" data-key="ēicere"></div>.</div>
+      <div>3. <div class="area" data-key="animus"></div> Mēdī verbīs Lȳdiae turbātus est.</div>
+      <div>4. Mēdus: “<div class="area" data-key="utrum"></div> deus an homō est iste dominus tuus, cui ventī <div class="area" data-key="oboedīre"></div> videntur?”</div>
+      <div>5. Iēsūs Chrīstus <div class="area" data-key="nātus"></div> est in oppidō Bethlehem; <div class="area" data-key="eō"></div> vēnērunt rēgēs ex oriente, quī īnfantem <div class="area" data-key="velut"></div> deum <div class="area" data-key="adōrāvērunt"></div>.</div>
+      <div>6. Magna <div class="area" data-key="turba"></div> hominum Chrīstum docentem sequēbātur.</div>
+      <div>7. Ex <div class="area" data-key="ūniversā"></div> Iūdaeā hominēs aegrī, quī <div class="area" data-key="fāmam"></div> dē factīs eius mīrābilibus audīverant, ad Chrīstum conveniēbant.</div>
+      <div>8. Deus, quī semper vīvit, nōn <div class="area" data-key="mortālis"></div>, sed <div class="area" data-key="immortālis"></div> est.</div>
+      <div>9. Hominēs, quī <div class="area" data-key="nāscuntur"></div> ac <div class="area" data-key="moriuntur"></div>, mortālēs sunt.</div>
+      <div>10. <div class="area" data-key="caecus"></div> est quī vidēre nōn potest; <div class="area" data-key="surdus"></div> quī audīre nōn potest; <div class="area" data-key="mūtus"></div> quī loquī nōn potest; <div class="area" data-key="claudus"></div> quī ambulāre nōn potest.</div>
+      <div>11. Mēdus manum <div class="area" data-key="extendit"></div> et <div class="area" data-key="libellum"></div> Lȳdiae <div class="area" data-key="apprehendit"></div>.</div>
+      <div>12. Matthaeus facta et <div class="area" data-key="dicta"></div> Chrīstī <div class="area" data-key="memorat"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>II</div>
+      <div><br></div>
+      <div><b>Exercitium 4</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>coniūnctīvus praeteritī</i></span></div>
+      <div><br></div>
+      <div>1. Iēsūs virō claudō imperāvit ut surg<div class="area" data-key="eret"></div> et ambul<div class="area" data-key="āret"></div>.</div>
+      <div>2. Medicus Quīntō imperāvit ut ōs aper<div class="area" data-key="īret"></div> et linguam ostend<div class="area" data-key="eret"></div>.</div>
+      <div>3. Dux mīlitēs monuit ut fortiter pugn<div class="area" data-key="āret"></div> nēve hostēs tim<div class="area" data-key="ērent"></div>.</div>
+      <div>4. Mīnōs postulāvit ut Athēniēnsēs ad Mīnōtaurum mitt<div class="area" data-key="erentur"></div>.</div>
+      <div>5. Ariadna Thēseum monuit ut cav<div class="area" data-key="ēret"></div> nē ā mōnstrō occīd<div class="area" data-key="erētur"></div>.</div>
+      <div>6. Thēseus Ariadnam rogāvit ut sē ante iānuam opper<div class="area" data-key="īrētur"></div>.</div>
+      <div>7. Mīnōs imperāvit ut Daedalus in labyrinthum inclūd<div class="area" data-key="erētur"></div>.</div>
+      <div>8. Daedalus fīlium suum monuit ut cautus es<div class="area" data-key="set"></div> in volandō.</div>
+      <div>9. Chrīstus efficere poterat ut hominēs caecī vid<div class="area" data-key="ērent"></div>, surdī aud<div class="area" data-key="īrent"></div>, mūtī loqu<div class="area" data-key="erentur"></div>, atque ut aegrī sānī es<div class="area" data-key="sent"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 5</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">anima</div> <div class="area">cessāre</div> <div class="area">cōnstāns</div> <div class="area">ēvolvere</div> <div class="area">habērī</div> <div class="area">Īnferī</div> <div class="area">mundus</div> <div class="area">nāvicula</div> <div class="area">per</div> <div class="area">potestās</div> <div class="area">prīnceps</div> <div class="area">rēgnāre</div> <div class="area">rogāre</div> <div class="area">salvus</div> <div class="area">suscitāre</div> <div class="area">versārī</div> <div class="area">vigilia</div></div></div>
+      <div><br></div>
+      <div>1. Mēdus Lydiam <div class="area" data-key="rogat"></div> ut aliquid sibi legat.</div>
+      <div>2. Lȳdia librum <div class="area" data-key="ēvolvit"></div> et legit de Iaīrō, <div class="area" data-key="prīncipe"></div> Iūdaeōrum, cuius fīliam mortuam Iēsūs <div class="area" data-key="suscitāvit"></div>.</div>
+      <div>3. Mēdus: “<div class="area" data-key="per"></div> deōs immortālēs! Deus tuus maximum <div class="area" data-key="potestātem"></div> habēre vidētur!”</div>
+      <div>4. Trēs diī ūniversum <div class="area" data-key="mundum"></div> inter sē dīvīsisse dīcuntur.</div>
+      <div>5. Iuppiter deus maximus <div class="area" data-key="habētur"></div>.</div>
+      <div>6. Plūtō <div class="area" data-key="rēgnat"></div> apud <div class="area" data-key="Īnferōs"></div>, ubi <div class="area" data-key="animae"></div> mortuōrum velut umbrae <div class="area" data-key="versārī"></div> dīcuntur.</div>
+      <div>7. Quārtā <div class="area" data-key="vigiliā"></div> noctis discipulī, quī in <div class="area" data-key="nāviculā"></div> erant, Iēsum super mare ambulantem vīdērunt.</div>
+      <div>8. Discipulīs turbātīs Chrīstus “<div class="area" data-key="cōnstantēs"></div> estōte!” ait.</div>
+      <div>9. Incipiēns mergī Petrus clāmāvit: “Domine, <div class="area" data-key="salvum"></div> mē fac!”</div>
+      <div>10. Paulō post ventus <div class="area" data-key="cessāvit"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>III</div>
+      <div><br></div>
+      <div><b>Exercitium 7</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>coniūnctīvus praeteritī</i></span></div>
+      <div><br></div>
+      <div>1. Mēdus: “Dominus ā mē postulābat ut semper industrius es<div class="area" data-key="sem"></div>.”</div>
+      <div>2. Lȳdia: “Amīcae meae mē ōrābant ut Rōmae sēcum manē<div class="area" data-key="rem"></div>, sed tū mihi persuāsistī ut tēcum venī<div class="area" data-key="rem"></div>.”</div>
+      <div>3. Mēdus: “Gaudeō quod amīcae tuae tibi persuādēre nōn potuērunt ut Rōmae manē<div class="area" data-key="rēs"></div> nēve mēcum venī<div class="area" data-key="rēs"></div>.”</div>
+      <div>4. Iūlius: “Nōnne vōs monueram, fīliī, nē arborem ascend<div class="area" data-key="erētis"></div>?”</div>
+      <div>5. Quīntus: “Certē nōs monuerās ut cautī es<div class="area" data-key="sēmus"></div> nēve arborem ascend<div class="area" data-key="erēmus"></div>, sed Mārcus mihi persuāsit ut arborem ascend<div class="area" data-key="erem"></div>.”</div>
+      <div>6. Iūlius: “Quōmodo Mārcus tibi persuādēre potuit ut arborem ascend<div class="area" data-key="erēs"></div>, sī ego tē monueram ut cautus es<div class="area" data-key="sēs"></div> nēve id fac<div class="area" data-key="erēs"></div>?”</div>
+      <div>7. Lȳdia: “Laetāminī, nautae! Chrīstus cūrāvit ut vōs salvī es<div class="area" data-key="sētis"></div> atque ē maximō perīculō servā<div class="area" data-key="rēminī"></div>.”</div>
+      <div>8. Nautae: “Fortasse Chrīstus cūrāvit ut tū servā<div class="area" data-key="rēris"></div>, at certē Neptūnus cūrāvit ut ego servā<div class="area" data-key="rer"></div>!”</div>
+      <div>9. Lȳdia: “Chrīstus cūrāvit ut nōs omnēs servā<div class="area" data-key="rēmur"></div>.”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 8</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>coniūnctīvus praesentis vs praeteritī</i></span></div>
+      <div><br></div>
+      <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Quārē Mēdus Rōmam <b>venit</b>? Ut amīcam suam <u>videat</u>.</span></div>
+      <div><span class="ff"><span style="white-space: pre;">		</span>Quārē Mēdus <b>fugit</b>? Nē <u>serviat</u>.</span></div>
+      <div><br></div>
+      <div><span class="ff"><span style="white-space: pre;">		</span>Quārē Mēdus Rōmam <b>vēnit</b>? Ut amīcam suam <u>vidēret</u>.</span></div>
+      <div><span class="ff"><span style="white-space: pre;">		</span>Quārē Mēdus <b>fūgit</b>? Nē <u>servīret</u>.</span></div>
+      <div><br></div>
+      <div>1. Magister calamum prōmit, <div class="area" data-key="ut"></div> scrīb<div class="area" data-key="eret"></div>.</div>
+      <div>2. Mercātōrēs mercēs vēndunt, <div class="area" data-key="ut"></div> pecūniam faci<div class="area" data-key="ant"></div>.</div>
+      <div>3. Pāstor umbram petit, <div class="area" data-key="nē"></div> in sōle ambul<div class="area" data-key="et"></div>.</div>
+      <div>4. Hominēs vestīmenta gerunt, <div class="area" data-key="nē"></div> frīg<div class="area" data-key="eant"></div>.</div>
+      <div>5. Ariadna Thēseō fīlum dedit, <div class="area" data-key="ut"></div> eum serv<div class="area" data-key="āret"></div>.</div>
+      <div>6. Daedalus ālās cōnfēcit, <div class="area" data-key="ut"></div> ē labyrinthō ēvol<div class="area" data-key="āret"></div>.</div>
+      <div>7. Lȳdia oculōs clausit, <div class="area" data-key="nē"></div> fulgura vid<div class="area" data-key="ēret"></div>.</div>
+      <div>8. Multī hominēs aegrī ad Chrīstum conveniēbant, <div class="area" data-key="ut"></div> ab eō sān<div class="area" data-key="ārentur"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 9</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">admīrārī</div> <div class="area">attentus</div> <div class="area">eundī</div> <div class="area">impendēre</div> <div class="area">mālle</div> <div class="area">pecūlium</div> <div class="area">perīculōsus</div> <div class="area">perīculum</div> <div class="area">perīre</div> <div class="area">persuādēre</div> <div class="area">pervenīre</div> <div class="area">potius</div> <div class="area">praedō</div> <div class="area">salvāre</div> <div class="area">servīre</div> <div class="area">spērāre</div> <div class="area">tranquillitās</div> <div class="area">tūtus</div> <div class="area">vītāre</div> <div class="area">vorāgō</div></div></div>
+      <div><br></div>
+      <div>1. Discipulī ad Iēsum accessērunt dīcentēs: “Domine, <div class="area" data-key="salvā"></div> nōs! <div class="area" data-key="perīmus"></div>!”</div>
+      <div>2. Iēsūs ventīs imperāvit, et facta est <div class="area" data-key="tranquillitās"></div> magna.</div>
+      <div>3. Nōn sōlum tempestātēs, sed multa alia <div class="area" data-key="perīcula"></div> nautīs <div class="area" data-key="impendent"></div>.</div>
+      <div>4. <div class="area" data-key="praedōnēs"></div> maritimī nāvēs persequuntur ut mercēs rapiant.</div>
+      <div>5. Nūllum mare <div class="area" data-key="tūtum"></div> est ā praedōnibus.</div>
+      <div>6. Scylla dīcitur saxum <div class="area" data-key="perīculōsum"></div> quō nāvēs franguntur.</div>
+      <div>7. <div class="area" data-key="vorāgō"></div> est locus quō nāvēs flūctibus vorantur.</div>
+      <div>8. Difficile est omnia maris perīcula <div class="area" data-key="vītāre"></div>.</div>
+      <div>9. Mēdus <div class="area" data-key="spērat"></div> sē salvum in Graeciam perventūrum esse.</div>
+      <div>10. Is enim in Graeciā līber vīvere <div class="area" data-key="māvult"></div> quam in Italiā dominō sevērō <div class="area" data-key="servīre"></div>; itaque cupidus est in Graeciam <div class="area" data-key="eundī"></div> [&lt; īre].</div>
+      <div>11. Gubernātor dīcit ‘sē morī <div class="area" data-key="mālle"></div> quam servīre’: “Ego morī <div class="area" data-key="mālō"></div> quam servīre.”</div>
+      <div>12. Intrā sex vel <div class="area" data-key="potius"></div> octō diēs amīcī nostrī in Graeciam <div class="area" data-key="pervenient"></div>.</div>
+      <div>13. Haud facile fuit Lȳdiae <div class="area" data-key="persuādēre"></div> ut Rōmā proficīscerētur.</div>
+      <div>14. Gubernātor pulchritūdinem ānulī <div class="area" data-key="admīrātur"></div>.</div>
+      <div>15. Discipulus <div class="area" data-key="attentus"></div> est quī studiōsē audit.</div>
+      <div>16. <div class="area" data-key="pecūlium"></div> est pecūnia dominī quae servō datur.</div>
+    </div>
+  `
+});
