45ec58bbf707065c0554b687b3946136a18041bd
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-03-09 17:49:51 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/capitulum/capit-30lgwm75.js
@@ -0,0 +1,140 @@
+$_GET({
+  title: 'Exercitia Latina 30',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>I</div>
+      <div><br></div>
+      <div><b>Exercitium 1</b></div>
+      <div><br></div>
+      <div><div class="area">-enter</div> <div class="area">-anter</div></div>
+      <div><br></div>
+      <div>1. Vir prūdēns negōtia sua prūd<div class="area" data-key="enter"></div> agit.</div>
+      <div>2. Agricola dīligēns agrōs dīlig<div class="area" data-key="enter"></div> colit.</div>
+      <div>3. Pāstor neglegēns ovēs suās negleg<div class="area" data-key="enter"></div> cūrat.</div>
+      <div>4. Sextus nōn rārō, sed frequ<div class="area" data-key="enter"></div> laudātur.</div>
+      <div>5. Homō cōnstāns et patiēns dolōrēs cōnst<div class="area" data-key="anter"></div> ac pati<div class="area" data-key="enter"></div> fert.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 2</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">balneum</div> <div class="area">cēna</div> <div class="area">circiter</div> <div class="area">contrahere</div> <div class="area">dēmum</div> <div class="area">dīligēns</div> <div class="area">diū</div> <div class="area">fruī</div> <div class="area">hospes</div> <div class="area">iter</div> <div class="area">iūcundus</div> <div class="area">molestus</div> <div class="area">nūntiāre</div> <div class="area">praeesse</div> <div class="area">recipere</div> <div class="area">requiēscere</div> <div class="area">salvēre</div> <div class="area">tardus</div> <div class="area">vīsere</div></div></div>
+      <div><br></div>
+      <div>1. <div class="area" data-key="cēna"></div> dīcitur cibus quī <div class="area" data-key="circiter"></div> hōrā decimā sūmitur.</div>
+      <div>2. Cornēlius et Orontēs, amīcī et <div class="area" data-key="hospitēs"></div> Iūliī, ab Aemiliā <div class="area" data-key="recipiuntur"></div>, dum Iūlius in <div class="area" data-key="balneō"></div> lavātur.</div>
+      <div>3. Aemilia marītum suum <div class="area" data-key="tardum"></div> excūsat: “Iūlius tardē revertit ex agrīs, quod nimis <div class="area" data-key="diū"></div> ambulāvit.”</div>
+      <div>4. Tum <div class="area" data-key="dēmum"></div> Iūlius intrat et amīcōs <div class="area" data-key="salvēre"></div> iubet: “Salvēte, amīcī!”</div>
+      <div>5. Cornēlius Iūlium prius <div class="area" data-key="vīsere"></div> nōn potuit prae negōtiīs urbānīs.</div>
+      <div>6. In vīllā suā dominus ōtiō <div class="area" data-key="fruitur"></div> atque post negōtia <div class="area" data-key="requiēscit"></div>.</div>
+      <div>7. Iūlius, quī ipse in agrīs nōn labōrat, vītam rūsticam nōn <div class="area" data-key="molestam"></div>, sed <div class="area" data-key="iūcundam"></div> esse exīstimat.</div>
+      <div>8. Quī ex longō <div class="area" data-key="itinere"></div> revertit rēs novās <div class="area" data-key="nūntiāre"></div> potest.</div>
+      <div>9. Quī omnia negōtia sua bene cūrat homō <div class="area" data-key="dīligēns"></div> est.</div>
+      <div>10. Dux magnī exercitūs multīs mīlitibus <div class="area" data-key="praeest"></div>.</div>
+      <div>11. Quī īrātus est frontem <div class="area" data-key="contrahit"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>II</div>
+      <div><br></div>
+      <div><b>Exercitium 4</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Discipulus scrīb<u>it</u> id quod magister dictāv<b>it</b>.</span></div>
+      <div><span class="ff"><span style="white-space: pre;">		</span>Discipulus scrīb<u>ēbat</u> id quod magister dictāv<b>erat</b>. </span></div>
+      <div><span class="ff"><span style="white-space: pre;">		</span>Discipulus scrīb<u>et</u> id quod magister dictāv<b>erit</b>. </span></div>
+      <div><br></div>
+      <div>1. Dominus servum laudat quod industriē labōrāv<div class="area" data-key="it"></div>.</div>
+      <div>2. Herī servus laudātus est quod industriē labōrāv<div class="area" data-key="erat"></div>.</div>
+      <div>3. Crās rūrsus laudābitur sī industriē labōrāv<div class="area" data-key="erit"></div>.</div>
+      <div>4. Dominus servōs laudat quod industriē labōrāv<div class="area" data-key="ērunt"></div>.</div>
+      <div>5. Herī servī laudātī sunt quod industriē labōrāv<div class="area" data-key="erant"></div>.</div>
+      <div>6. Crās rūrsus laudābuntur sī industriē labōrāv<div class="area" data-key="erint"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 5</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>futūrum perfectum</i></span></div>
+      <div><br></div>
+      <div>1. Cēnābimus, cum prīmum cocus cēnam parāv<div class="area" data-key="erit"></div> et servī triclīnium exōrnāv<div class="area" data-key="erint"></div>.</div>
+      <div>2. Cēnābimus, cum prīmum triclīnium exōrnāt<div class="area" data-key="um"></div> <div class="area" data-key="erit"></div>.</div>
+      <div>3. Puerī ūmidī erunt, sī per imbrem ambulāv<div class="area" data-key="erint"></div>.</div>
+      <div>4. Puellae plōrābunt, sī ā puerīs pulsāt<div class="area" data-key="ae"></div> <div class="area" data-key="erunt"></div>.</div>
+      <div>5. Cum Iūlius ūnam hōram dormīv<div class="area" data-key="erit"></div>, ambulābit; cum satis diū ambulāv<div class="area" data-key="erit"></div>, lavātum ībit.</div>
+      <div>6. Mīlitēs ā duce laudābuntur, sī fortiter pugnāv<div class="area" data-key="erint"></div>.</div>
+      <div>7. Pater gaudēbit, sī fīlius industrius fu<div class="area" data-key="erit"></div> et ā magistrō laudāt<div class="area" data-key="us"></div> <div class="area" data-key="erit"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 6</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">accubāre</div> <div class="area">bīnī</div> <div class="area">bonum</div> <div class="area">cēnāre</div> <div class="area">cocus</div> <div class="area">convīva</div> <div class="area">convīvium</div> <div class="area">coquere</div> <div class="area">culīna</div> <div class="area">ēligere</div> <div class="area">equidem</div> <div class="area">exōrnāre</div> <div class="area">famēs</div> <div class="area">īmus</div> <div class="area">minister</div> <div class="area">parāre</div> <div class="area">paulisper</div> <div class="area">prīdem</div> <div class="area">singulī</div> <div class="area">sitis</div> <div class="area">sternere</div> <div class="area">ternī</div> @triclīnium@@</div></div>
+      <div><br></div>
+      <div>1. Rōmānī circiter hōrā decimā <div class="area" data-key="cēnant"></div>.</div>
+      <div>2. Sī quis nihil ēst nec bibit, <div class="area" data-key="famem"></div> et <div class="area" data-key="sitim"></div>patitur.</div>
+      <div>3. Sine aquā <div class="area" data-key="paulisper"></div> vīvere possumus.</div>
+      <div>4. Orontēs: “<div class="area" data-key="equidem"></div> sine vīnō vīvere nōn possum. Magnum <div class="area" data-key="bonum"></div> est vīnum.”</div>
+      <div>5. Ex malīs minimum <div class="area" data-key="ēligere"></div> oportet.</div>
+      <div>6. <div class="area" data-key="culīna"></div> est locus ubi cibus <div class="area" data-key="coquitur"></div> et cēna parātur.</div>
+      <div>7. Quī cibum coquit et cēnam <div class="area" data-key="parat"></div> in culīnā, <div class="area" data-key="cocus"></div> dīcitur.</div>
+      <div>8. <div class="area" data-key="ministrī"></div> vocantur servī quī cibum et vīnum in triclīnium ferunt.</div>
+      <div>9. Ante <div class="area" data-key="convīvium"></div> triclīnium flōribus <div class="area" data-key="exōrnātur"></div> et vestis pretiōsa super lectōs <div class="area" data-key="sternitur"></div>.</div>
+      <div>10. In <div class="area" data-key="triclīniō"></div> sunt trēs lectī: lectus summus, medius, <div class="area" data-key="īmus"></div>.</div>
+      <div>11. Quot <div class="area" data-key="convīvae"></div> in singulīs lectīs <div class="area" data-key="accubant"></div>? <div class="area" data-key="singulī"></div> aut <div class="area" data-key="bīnī"></div> aut <div class="area" data-key="ternī"></div>.</div>
+      <div>12. Iūlius: “Cēnam iam <div class="area" data-key="prīdem"></div> parātam esse oportuit!”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>III</div>
+      <div><br></div>
+      <div><b>Exercitium 8</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>futūrum perfectum</i></span></div>
+      <div><br></div>
+      <div>1. Discipulus verberābitur, sī piger fu<div class="area" data-key="erit"></div>: sī male computāv<div class="area" data-key="erit"></div>, foedē scrīps<div class="area" data-key="erit"></div> atque in lūdō dormīv<div class="area" data-key="erit"></div>.</div>
+      <div>2. Magister: “Tē verberābō, sī piger fu<div class="area" data-key="eris"></div> ac dormīv<div class="area" data-key="eris"></div>!”</div>
+      <div>3. Discipulus: “Quid mihi dabis, sī industrius fu<div class="area" data-key="erō"></div>? sī bene computāv<div class="area" data-key="erō"></div>, pulchrē scrīps<div class="area" data-key="erō"></div> nec in lūdō dormīv<div class="area" data-key="erō"></div>?”</div>
+      <div>4. Magister: “Nihil tibi dabō, sed laudābō tē, sī bene computāv<div class="area" data-key="eris"></div> et pulchrē scrīps<div class="area" data-key="eris"></div>.”</div>
+      <div>5. Discipulī verberābuntur, sī pigrī fu<div class="area" data-key="erint"></div>: sī male computāv<div class="area" data-key="erint"></div>, foedē scrīps<div class="area" data-key="erint"></div> atque in lūdō dormīv<div class="area" data-key="erint"></div>.</div>
+      <div>6. Magister: “Vōs verberābō, sī pigrī fu<div class="area" data-key="eritis"></div> ac dormīv<div class="area" data-key="eritis"></div>!”</div>
+      <div>7. Discipulī: “Quid nōbīs dabis, sī industriī fu<div class="area" data-key="erimus"></div>? sī bene computāv<div class="area" data-key="erimus"></div> et pulchrē scrīps<div class="area" data-key="erimus"></div>?”</div>
+      <div>8. Magister: “Nihil vōbīs dabō, sed laudābō vōs, sī bene computāv<div class="area" data-key="eritis"></div> et pulchrē scrīps<div class="area" data-key="eritis"></div>.”</div>
+      <div>9. Quīntus: “Quid tū faciēs, Mārce, sī ab aliō puerō pulsāt<div class="area" data-key="us"></div> <div class="area" data-key="eris"></div>?”</div>
+      <div>10. Mārcus: “Sī ego ab aliō puerō pulsāt<div class="area" data-key="us"></div> <div class="area" data-key="erō"></div>, ipse eum puerum pulsābō.”</div>
+      <div>11. Quīntus: “Quid vōs faciētis, Mārce et Tite, sī ab aliīs puerīs pulsāt<div class="area" data-key="ī"></div> <div class="area" data-key="eritis"></div>?”</div>
+      <div>12. Mārcus: “Sī ab aliīs puerīs pulsāt<div class="area" data-key="ī"></div> <div class="area" data-key="erimus"></div>, ipsī eōs pulsābimus.”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 9</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>coniūnctīvus hortātīvus</i></span></div>
+      <div><br></div>
+      <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Discēdāmus! = Quīn discēdimus?</span></div>
+      <div><br></div>
+      <div>1. “Quīn ambulāmus?” = “Ambul<div class="area" data-key="ēmus"></div>!”</div>
+      <div>2. “Quīn gaudēmus?” = “Gaud<div class="area" data-key="eāmus"></div>!”</div>
+      <div>3. “Quīn bibimus atque canimus?” = “Bib<div class="area" data-key="āmus"></div> atque can<div class="area" data-key="āmus"></div>!”</div>
+      <div>4. “Quīn pōcula exhaurīmus?” = “Pōcula exhaur<div class="area" data-key="iāmus"></div>!”</div>
+      <div>5. “Quīn equōs cōnscendimus ac proficīscimur?” = “Equōs cōnscend<div class="area" data-key="āmus"></div> ac proficīsc<div class="area" data-key="āmur"></div>!”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 10</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">accumbere</div> <div class="area">acerbus</div> <div class="area">acūtus</div> <div class="area">appōnere</div> <div class="area">apportāre</div> <div class="area">argenteus</div> <div class="area">argentum</div> <div class="area">aspergere</div> <div class="area">carō</div> <div class="area">complēre</div> <div class="area">dulcis</div> <div class="area">exhaurīre</div> <div class="area">fundere</div> <div class="area">genus</div> <div class="area">glōriōsus</div> <div class="area">gustāre</div> <div class="area">holus</div> <div class="area">līberāre</div> <div class="area">lībertīnus</div> <div class="area">mel</div> <div class="area">merus</div> <div class="area">miscēre</div> <div class="area">placēre</div> <div class="area">pōtāre</div> <div class="area">prōferre</div> <div class="area">sāl</div> <div class="area">sānē</div> <div class="area">sententia</div> <div class="area">vās</div></div></div>
+      <div><br></div>
+      <div>1. Convīvae triclīnium intrant atque in lectīs <div class="area" data-key="accumbunt"></div>.</div>
+      <div>2. In mēnsā sunt <div class="area" data-key="vāsa"></div> et pōcula <div class="area" data-key="argentea"></div> (= ex <div class="area" data-key="argentō"></div> facta).</div>
+      <div>3. Ministrī optimum cibum <div class="area" data-key="apportant"></div>.</div>
+      <div>4. Ōva, piscēs cum <div class="area" data-key="holeribus"></div>, <div class="area" data-key="carō"></div> porcī, varia <div class="area" data-key="genera"></div> mālōrum convīvīs <div class="area" data-key="appōnuntur"></div>.</div>
+      <div>5. Servus carnem secat cultrō <div class="area" data-key="acūtō"></div>.</div>
+      <div>6. Orontēs, cum prīmum carnem <div class="area" data-key="gustat"></div>, salem <div class="area" data-key="aspergit"></div>; nec enim carō sine <div class="area" data-key="sale"></div> Orontī <div class="area" data-key="placet"></div>.</div>
+      <div>7. Rōmānī vīnum cum aquā <div class="area" data-key="miscēre"></div> solent; Orontēs vērō vīnum <div class="area" data-key="merum"></div> <div class="area" data-key="pōtat"></div>.</div>
+      <div>8. <div class="area" data-key="lībertīnus"></div> est vir quī servus fuit et <div class="area" data-key="līberātus"></div> est.</div>
+      <div>9. <div class="area" data-key="mel"></div> est quod apēs ex flōribus quaerunt.</div>
+      <div>10. Vīnum cum melle mixtum nōn <div class="area" data-key="acerbum"></div>, sed <div class="area" data-key="dulce"></div> est.</div>
+      <div>11. Iūlius ministrum Falernum <div class="area" data-key="prōferre"></div> et in pōcula <div class="area" data-key="fundere"></div> iubet.</div>
+      <div>12. Orontēs iam prīdem pōculum suum <div class="area" data-key="exhausit"></div>.</div>
+      <div>13. Iam pōcula Falernō <div class="area" data-key="complentur"></div>.</div>
+      <div>14. Omnēs sentiunt vīnum Falernum <div class="area" data-key="sānē"></div> melius esse Albānō, nec quisquam <div class="area" data-key="sententiam"></div> suam dīcere audet.</div>
+      <div>15. Quī dīvitiās suās ostendit et ipse sē laudat <div class="area" data-key="glōriōsus"></div> est.</div>
+    </div>
+  `
+});
