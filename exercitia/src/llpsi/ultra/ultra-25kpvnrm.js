3bd88c6b363e098961865072153f7c88cc16ec27
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-03-27 17:59:07 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-25kpvnrm.js
@@ -0,0 +1,17 @@
+$_GET({
+  title: 'Ultra 25',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Заменить императивы обычных глаголов на императивы аналогичных по смыслу отложительных.</b><br><br>1. Tribūnus: “<div class="area" data-key="verēminī">Timēte</div>, Quirītēs, īram deōrum!”</div>
+      <div>2. Cōnsul: “Ō Rōma, <div class="area" data-key="laetāre">gaudē</div>, perīculum enim praeteriit.”</div>
+      <div>3. “Sōlus intrābō,” ait ūnus duōrum. “Tū eautem hīc mē <div class="area" data-key="opperīre">exspectā</div>.</div>
+      <div>4. Cicerō: “<div class="area" data-key="ēgredere">Exī</div> ex urbe, Catilīna; in exsilium <div class="area" data-key="proficīscere">abī</div>.”</div>
+      <div>5. “Explōrātor castrīs hostium appropinquābō. Vōs, signō datō, <div class="area">īte post mē</div>!”</div>
+      <div>6. Seneca: “Animum <div class="area" data-key="intuēre">spectā</div>, quālis quantusque sit, aliēnō an suō magnus” (Epistulae Mōrālēs ad Lūcilium. 76, 32).</div>
+      <div>7. Patrōnus clientī: “Quid vēnistī? Quid tibi vīs? <div class="area" data-key="loquere">Dīc</div>, audiō.”</div>
+      <div>8. Nāvarchus nautīs: “Ecce praeda vestra. Aequē eam <div class="area" data-key="partīminī">dīvidite</div>.</div>
+      <div>9. “<div class="area" data-key="revertere">Revenī</div>, <div class="area" data-key="revertere">revenī</div>!” ōrat Orpheus, nec revenit umbra Eurydicēs.</div>
+      <div>10. Uxor virō īrāta: “Sī culpam tuam redimere vīs, <div class="area" data-key="morere">mortuus estō</div>!”</div>
+    </div>
+  `
+});
