93654fd2f37dbaf5be2f12feba2ea5117eb405d3
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-03-27 19:37:25 +0300

    Add files via upload

--- exercitia/src/llpsi/ultra/ultra-25kpvnrm.js
+++ exercitia/src/llpsi/ultra/ultra-25kpvnrm.js
@@ -4,9 +4,9 @@ $_GET({
     <div class="row">
       <div><b>1. Заменить императивы обычных глаголов на императивы аналогичных по смыслу отложительных.</b><br><br>1. Tribūnus: “<div class="area" data-key="verēminī">Timēte</div>, Quirītēs, īram deōrum!”</div>
       <div>2. Cōnsul: “Ō Rōma, <div class="area" data-key="laetāre">gaudē</div>, perīculum enim praeteriit.”</div>
-      <div>3. “Sōlus intrābō,” ait ūnus duōrum. “Tū eautem hīc mē <div class="area" data-key="opperīre">exspectā</div>.</div>
+      <div>3. “Sōlus intrābō,” ait ūnus duōrum. “Tū autem hīc mē <div class="area" data-key="opperīre">exspectā</div>.</div>
       <div>4. Cicerō: “<div class="area" data-key="ēgredere">Exī</div> ex urbe, Catilīna; in exsilium <div class="area" data-key="proficīscere">abī</div>.”</div>
-      <div>5. “Explōrātor castrīs hostium appropinquābō. Vōs, signō datō, <div class="area">īte post mē</div>!”</div>
+      <div>5. “Explōrātor castrīs hostium appropinquābō. Vōs, signō datō, <div class="area" data-key="sequiminī">īte post mē</div>!”</div>
       <div>6. Seneca: “Animum <div class="area" data-key="intuēre">spectā</div>, quālis quantusque sit, aliēnō an suō magnus” (Epistulae Mōrālēs ad Lūcilium. 76, 32).</div>
       <div>7. Patrōnus clientī: “Quid vēnistī? Quid tibi vīs? <div class="area" data-key="loquere">Dīc</div>, audiō.”</div>
       <div>8. Nāvarchus nautīs: “Ecce praeda vestra. Aequē eam <div class="area" data-key="partīminī">dīvidite</div>.</div>
