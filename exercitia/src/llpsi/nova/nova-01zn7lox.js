782a16dc03c1b4164198ce4d80fa99fce34ba406
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-04-18 18:45:39 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-01zn7lox.js
@@ -0,0 +1,67 @@
+$_GET({
+  title: 'Nova Exercitia 1',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">singulāris</div> | <div class="area">-us</div> <div class="area">-a</div> <div class="area">-um</div></div>
+      <div><div class="area">plūrālis</div> | <div class="area">-ī</div> <div class="area">-ae</div> <div class="area">-a</div></div></div>
+      <div><br></div>
+      <div>1. <i>Fluvi<b>us</b></i> singulāris est. <i>Fluvi<b>ī</b></i> <div class="area" data-key="plūrālis"></div> est.</div>
+      <div>2. <i>Īnsul<b>a</b></i> <div class="area" data-key="singulāris"></div> est. <i>Īnsul<b>ae</b></i> plūrālis est.</div>
+      <div>3. <i>Oppid<b>um</b></i> singulāris est. <i>Oppid<b>a</b></i> <div class="area" data-key="plūrālis"></div> est.</div>
+      <div>4. Singulāris: <i>prōvincia</i>; plūrālis: <div class="area" data-key="prōvinciae"></div>.</div>
+      <div>5. Plūrālis: <i>exempla</i>; singulāris: <div class="area" data-key="exemplum"></div>.</div>
+      <div>6. Plūrālis: <i>numerī</i>; singulāris: <div class="area" data-key="numerus"></div>.</div>
+      <div>7. Singulāris: <i>vocābulum</i>; plūrālis: <div class="area" data-key="vocābula"></div>.</div>
+      <div>8. Plūrālis: <i>litterae</i>; singulāris: <div class="area" data-key="littera"></div>.</div>
+      <div>9. Singulāris: <i>imperium</i>; plūrālis: <div class="area" data-key="imperia"></div>.</div>
+      <div>10. Singulāris: <i>syllaba</i>; plūrālis: <div class="area" data-key="syllabae"></div>.</div>
+      <div>11. Plūrālis: <i>capitula</i>; singulāris: <div class="area" data-key="capitulum"></div>.</div>
+      <div>12. Singulāris: <i>pēnsum</i>; plūrālis: <div class="area" data-key="pēnsa"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>īnsula <div class="area">Graecus</div> &gt; īnsula <div class="area">Graeca</div></span></div>
+      </div>
+      <div><br></div>
+      <div>1. Fluviī <div class="area" data-key="magnī">magnus</div>.</div>
+      <div>2. <div class="area" data-key="multae">Multī</div> insulae.</div>
+      <div>3. <div class="area" data-key="pauca">paucī</div> oppida.</div>
+      <div>4. Imperium <div class="area" data-key="Rōmānum">Rōmānus</div>.</div>
+      <div>5. Litterae <div class="area" data-key="Graecī">Graecus</div>.</div>
+      <div>6. Vocābulum <div class="area" data-key="Latīnum">Latīnus</div>.</div>
+      <div>7. Prōvincia <div class="area" data-key="Rōmāna">Rōmānus</div>.</div>
+      <div>8. Numerus <div class="area" data-key="magnus">magnus</div>.</div>
+      <div>9. <div class="area" data-key="multa">Multī</div> exempla.</div>
+      <div>10. Capitulum <div class="area" data-key="prīmum">prīmus</div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Rōma in Italiā est. →</span></div>
+        <div><span class="ff"><span class="ff"><span style="white-space: pre;">		</span></span><span style="white-space: pre;">	</span>Ubi est Rōma? / Num Rōma in Graeciā est? / Estne Rōma in Italiā?</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Brundisium in Italiā est. →</div>
+      <div>2. Sicilia īnsula est. →</div>
+      <div>3. Britannia nōn īnsula parva, sed īnsula magna est. →</div>
+      <div>4. Hispānia in Eurōpā est. →</div>
+      <div>5. Aegyptus et Gallia in Eurōpā sunt. →</div>
+      <div>6. Gallia prōvincia Rōmāna est. →</div>
+      <div>7. Aegyptus nōn in Eurōpā, sed in Āfricā est. →</div>
+      <div>8. Tiberis fluvius est. →</div>
+      <div>9. In Arabiā nōn multī, sed paucī fluviī sunt. →</div>
+      <div>10. Tūsculum oppidum Rōmānum est. →</div>
+      <div>11. Nīlus fluvius magnus est. →</div>
+      <div>12. Sparta oppidum est. →</div>
+      <div>13. Brundisium et Tūsculum nōn in Graeciā, sed in Italiā sunt. →</div>
+      <div>14. Īnsula vocābulum Latīnum est. →</div>
+      <div>15. A, B, C nōn litterae Graecae, sed litterae Latīnae sunt. →</div>
+    </div>
+  `
+});
