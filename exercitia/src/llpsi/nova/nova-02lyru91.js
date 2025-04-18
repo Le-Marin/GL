2ccb47716900bad4584a79bf987efacead2d9fdf
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-04-18 19:11:57 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-02lyru91.js
@@ -0,0 +1,76 @@
+$_GET({
+  title: 'Nova 1',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">m</div> <div class="area">f</div> <div class="area">n</div></div></div>
+      <div><br></div>
+      <div>1. puella: <div class="area" data-key="f"></div></div>
+      <div>2. oppidum: <div class="area" data-key="n"></div></div>
+      <div>3. capitula: <div class="area" data-key="n"></div></div>
+      <div>4. dominae: <div class="area" data-key="f"></div></div>
+      <div>5. dominus: <div class="area" data-key="m"></div></div>
+      <div>6. liber: <div class="area" data-key="m"></div></div>
+      <div>7. litterae: <div class="area" data-key="f"></div></div>
+      <div>8. vocābula: <div class="area" data-key="n"></div></div>
+      <div>9. fluviī: <div class="area" data-key="m"></div></div>
+      <div>10. pater: <div class="area" data-key="m"></div></div>
+      <div>11. līberī: <div class="area" data-key="m"></div></div>
+      <div>12. ancilla: <div class="area" data-key="f"></div></div>
+      <div>13. exempla: <div class="area" data-key="n"></div></div>
+      <div>14. servus: <div class="area" data-key="m"></div></div>
+      <div>15. pāgina: <div class="area" data-key="f"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>genetīvus</i></span></div>
+      <div><br></div><div class="fill-ins"><div><span class="ff">masc.<span style="white-space: pre;">	</span><div class="area">-ī</div> <div class="area">-ōrum</div></span></div>
+      <div><span class="ff">fēm<span style="white-space: pre;">	</span><span style="white-space: pre;">	</span><div class="area">-ae</div> <div class="area">-ārum</div></span></div>
+      <div><span class="ff">neutr.<span style="white-space: pre;">	</span><div class="area">-ī</div> <div class="area">-ōrum</div></span></div></div>
+      <div><br></div>
+      <div>1. Mārcus fīlius <div class="area" data-key="Iūliī">Iūlius</div> est.</div>
+      <div>2. Aemilia māter <div class="area" data-key="līberōrum">līberī</div> est.</div>
+      <div>3. Aemilia domina <div class="area" data-key="multārum">multae</div> <div class="area" data-key="ancillārum">ancillae</div> est.</div>
+      <div>4. Dāvus Mēdusque servī <div class="area" data-key="Iūliī">Iūlius</div> sunt.</div>
+      <div>5. In imperiō Rōmānō magnus numerus <div class="area" data-key="oppidōrum">oppida</div> est.</div>
+      <div>6. Iūlius pater est <div class="area" data-key="Iūliae">Iūlia</div> et <div class="area" data-key="Quīntī">Quīntus</div> et <div class="area" data-key="Mārcī">Mārcus</div>.</div>
+      <div>7. Syra Dēliaque ancillae <div class="area" data-key="Aemiliae">Aemilia</div> sunt.</div>
+      <div>8. Mārcus, Quīntus Iūliaque līberī <div class="area" data-key="Iūliī">Iūlius</div> et <div class="area" data-key="Aemiliae">Aemilia</div> sunt.</div>
+      <div>9. Iūlius dominus <div class="area" data-key="multōrum">multī</div> <div class="area" data-key="servōrum">servī</div> est.</div>
+      <div>10. In familiā <div class="area" data-key="Iūliī">Iūlius</div> est magnus numerus <div class="area" data-key="servōrum">servī</div>.</div>
+      <div>11. Mēdus nōn servus <div class="area" data-key="Cornēliī">Cornēlius</div>, sed <div class="area" data-key="Iūliī">Iūlius</div> est.</div>
+      <div>12. Aemilia domina <div class="area" data-key="Dēliae">Dēlia</div> <div class="area" data-key="Syraeque">Syraque</div> est.</div>
+      <div>13. In Arabiā nōn magnus, sed parvus numerus <div class="area" data-key="fluviōrum">fluviī</div> est.</div>
+      <div>14. Numerus <div class="area" data-key="servōrum">servī</div> <div class="area" data-key="Iūliī">Iūlius</div> nōn parvus, sed magnus est.</div>
+      <div>15. Cornēlius dominus <div class="area" data-key="multōrum">multī</div> <div class="area" data-key="servōrum">servī</div> et <div class="area" data-key="paucārum">paucae</div> <div class="area" data-key="ancillārum">ancillae</div> est.</div>
+      <div>16. Titulus <div class="area" data-key="librī">liber</div> <div class="area" data-key="tuī">tuus</div> <div class="area" data-key="Latīnī">Latīnus</div> est Familia Rōmāna.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Δ littera Graeca est. →</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Quid est Δ? / Num Δ littera Latīna est? / Estne Δ littera Graeca?</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Mārcus fīlius Iūliī Aemiliaeque est. →</div>
+      <div>2. Quīntus nōn puella, sed puer est. →</div>
+      <div>3. Syra Dēliaque ancillae Aemiliae sunt. →</div>
+      <div>4. Cornēlius dominus Rōmānus est. →</div>
+      <div>5. In familiā Iūliī centum servī sunt. →</div>
+      <div>6. Iūlia fīlia Aemiliae est. →</div>
+      <div>7. Iūlius pater Iūliae est. →</div>
+      <div>8. Līberī sunt Mārcus, Quīntus Iūliaque. →</div>
+      <div>9. Tūsculum oppidum est. →</div>
+      <div>10. Tiberis nōn fluvius magnus, sed parvus est. →</div>
+      <div>11. Aemilia domina ancillārum est. →</div>
+      <div>12. In familiā Iūliī duo fīliī et ūna fīlia sunt. →</div>
+      <div>13. Mārcus Quīntusque fīliī Iūliī sunt. →</div>
+      <div>14. Iūlia puella parva est. →</div>
+      <div>15. Nīlus fluvius est. →</div>
+    </div>
+  `
+});
