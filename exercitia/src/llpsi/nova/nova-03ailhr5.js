61a3400cc0587a33fb2fcb95dce2d53bea4fcc70
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-04-25 15:55:42 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-03ailhr5.js
@@ -0,0 +1,131 @@
+$_GET({
+  title: 'Nova Exercitia 3',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-um</div> <div class="area">-am</div></div>
+      <div><div class="area">-at</div> <div class="area">-et</div> <div class="area">-it</div></div></div>
+      <div><br></div>
+      <div>1. Iūlia cant<div class="area" data-key="at"></div>.</div>
+      <div>2. Mārcus Iūli<div class="area" data-key="am"></div> puls<div class="area" data-key="at"></div>.</div>
+      <div>3. Iūlia plōr<div class="area" data-key="at"></div>; Mārcus rīd<div class="area" data-key="et"></div>; Quīntus Mārc<div class="area" data-key="um"></div> vid<div class="area" data-key="et"></div>.</div>
+      <div>4. Quīntus Mārc<div class="area" data-key="um"></div> puls<div class="area" data-key="at"></div>.</div>
+      <div>5. Iūlia Aemili<div class="area" data-key="am"></div> voc<div class="area" data-key="at"></div>; Mārcus Quīnt<div class="area" data-key="um"></div> puls<div class="area" data-key="at"></div>.</div>
+      <div>6. Iūlius dorm<div class="area" data-key="it"></div>.</div>
+      <div>7. Quīntus Iūli<div class="area" data-key="um"></div> voc<div class="area" data-key="at"></div>.</div>
+      <div>8. Iūlius Quīnt<div class="area" data-key="um"></div> aud<div class="area" data-key="it"></div> et ven<div class="area" data-key="it"></div>.</div>
+      <div>9. Iūlius Mārc<div class="area" data-key="um"></div> verber<div class="area" data-key="at"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Iūlia / Mārcus / pulsat =</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Num Iūlia Mārcum pulsat? Iūlia Mārcum nōn pulsat. Mārcus Iūliam pulsat.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Mārcus / Quīntus / videt =</div>
+      <div>2. Aemilia / Iūlia / vocat =</div>
+      <div>3. Iūlia / Aemilia / audit =</div>
+      <div>4. Quīntus / Aemilia / interrogat =</div>
+      <div>5. Mārcus / Iūlius / verberat =</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-us</div> <div class="area">-a</div></div>
+      <div><div class="area">-um</div> <div class="area">-am</div> <div class="area">eum</div> <div class="area">eam</div> <div class="area">mē</div> <div class="area">tē</div></div>
+      <div><div class="area">-at</div> <div class="area">-et</div> <div class="area">-it</div></div></div>
+      <div><br></div>
+      <div>1. Mārcus Iūliam puls<div class="area" data-key="at"></div>.</div>
+      <div>2. Quīntus Mārcum vid<div class="area" data-key="et"></div>.</div>
+      <div>3. Iūlia Aemili<div class="area" data-key="am"></div> nōn videt et e<div class="area" data-key="am"></div> vocat.</div>
+      <div>4. Māter fīli<div class="area" data-key="um | am"></div> interrogat.</div>
+      <div>5. Aemilia: “Quis m<div class="area" data-key="ē"></div> vocat?”</div>
+      <div>6. Quīntus respondet: “Iūlia t<div class="area" data-key="ē"></div> vocat.”</div>
+      <div>7. Aemili<div class="area" data-key="a"></div> Mārcum verber<div class="area" data-key="at"></div>.</div>
+      <div>8. Mārc<div class="area" data-key="us"></div> plōrat, quia Aemilia e<div class="area" data-key="um"></div> verberat.</div>
+      <div>9. Iūlius puer<div class="area" data-key="um"></div> audit.</div>
+      <div>10. Iūlius puer<div class="area" data-key="um"></div> improb<div class="area" data-key="um"></div> verberat.</div>
+      <div>11. Puer prob<div class="area" data-key="us"></div> parv<div class="area" data-key="am"></div> puell<div class="area" data-key="am"></div> nōn puls<div class="area" data-key="at"></div>.</div>
+      <div>12. Quīnt<div class="area" data-key="us"></div> Iūliam nōn pulsat.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">quī</div> <div class="area">quae</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Mārcus pulsat, Iūlia cantat. →</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Mārcus pulsat Iūli<b>am</b>, <u>quae</u> cantat.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Quīntus videt, Iūlia plōrat. →</div>
+      <div>Quīnt<div class="area" data-key="us"></div> videt Iūli<div class="area" data-key="am"></div>, <div class="area" data-key="quae"></div> plōrat.</div>
+      <div>2. Iūlia vocat, Aemilia venit. →</div>
+      <div>Iūli<div class="area" data-key="a"></div> vocat Aemili<div class="area" data-key="am"></div>, <div class="area" data-key="quae"></div> venit.</div>
+      <div>3. Aemilia audit, Iūlia eam vocat. →</div>
+      <div>Aemili<div class="area" data-key="a"></div> audit Iūli<div class="area" data-key="am"></div>, <div class="area" data-key="quae"></div> e<div class="area" data-key="am"></div> vocat.</div>
+      <div>4. Aemilia interrogat, Quīntus respondet. →</div>
+      <div>Aemili<div class="area" data-key="a"></div> interrogat Quīnt<div class="area" data-key="um"></div>, <div class="area" data-key="quī"></div> respondet.</div>
+      <div>5. Iūlius dormit neque audit, puella plōrat. →</div>
+      <div>Iūli<div class="area" data-key="us"></div> dormit neque audit puell<div class="area" data-key="am"></div>, <div class="area" data-key="quae"></div> plōrat.</div>
+      <div>6. Aemilia verberat, Mārcus iam nōn rīdet. →</div>
+      <div>Aemili<div class="area" data-key="a"></div> verberat Mārc<div class="area" data-key="um"></div>, <div class="area" data-key="quī"></div> iam nōn ridet.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quīntum</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>puella Mārcus pulsat Iūlia est quam →</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Puella quam Mārcus pulsat Iūlia est.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. est quī improbus pulsat Iūliam puer →</div>
+      <div>2. quae cantat est puella Iūlia →</div>
+      <div>3. puellam vocat Mārcus quae Aemiliam verberat →</div>
+      <div>4. Quīntum venit et respondet quī interrogat Aemilia →</div>
+      <div>5. Iūlium dormit quī vocat Quīntus audit neque →</div>
+      <div>6. iam Mārcum nōn dormit Iūlius et audit →</div>
+      <div>7. venit Aemilia et verberat Iūlius quem Mārcum audit →</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium sextum</b></div>
+      <div><br></div>
+      <div>1. Dāvum Iūlius vocat. → <div class="area">d</div></div>
+      <div>2. Iūlia Aemiliam vocat. → <div class="area" data-key="c"></div></div>
+      <div>3. Iūliam Aemilia vocat. → <div class="area" data-key="f"></div></div>
+      <div>4. Quīntus Mārcum pulsat. → <div class="area" data-key="g"></div></div>
+      <div>5. Dēlia Syraque ancillae Aemiliae sunt. → <div class="area" data-key="e"></div></div>
+      <div>6. Iūliae pater est Iūlius. → <div class="area" data-key="h"></div></div>
+      <div>7. Iūlius est pater Quīntī. → <div class="area" data-key="b"></div></div>
+      <div>8. Dāvus et Mēdus sunt servī Iūliī. → <div class="area" data-key="a"></div></div>
+      <div><br></div>
+      <div>a. Quī sunt servī Iūliī?</div>
+      <div>b. Quis est pater Quīntī?</div>
+      <div>c. Quis Aemiliam vocat?</div>
+      <div>d. Quem Iūlius vocat?</div>
+      <div>e. Cuius ancillae sunt Dēlia Syraque?</div>
+      <div>f. Quem Aemilia vocat?</div>
+      <div>g. Quis Mārcum pulsat?</div>
+      <div>h. Cuius pater est Iūlius?</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium septimum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">audit</div> <div class="area">cantat</div> <div class="area">dormit</div> <div class="area">interrogat</div> <div class="area">plõrat</div> <div class="area">pulsat</div> <div class="area">respondet</div> <div class="area">rīdet</div> <div class="area">venit</div> <div class="area">verberat</div> <div class="area">videt</div> <div class="area">vocat</div></div></div>
+      <div><br></div>
+      <div>1. Puella, quae laeta est, <div class="area" data-key="cantat"></div>.</div>
+      <div>2. Puer quī īrātus est nōn <div class="area" data-key="rīdet"></div>.</div>
+      <div>3. Puer, quī improbus est, puellam <div class="area" data-key="pulsat"></div>.</div>
+      <div>4. Quīntus Mārcum, quī īrātus et improbus est, <div class="area" data-key="videt"></div>.</div>
+      <div>5. Iūlia, quae <div class="area" data-key="plōrat"></div>, Aemiliam <div class="area" data-key="vocat"></div>.</div>
+      <div>6. Quīntus, quem Aemilia <div class="area" data-key="interrogat"></div>, <div class="area" data-key="respondet"></div>.</div>
+      <div>7. Iūlius, quī <div class="area" data-key="dormit"></div>, Mārcum nōn <div class="area" data-key="audit"></div> neque <div class="area" data-key="venit"></div>.</div>
+      <div>8. Iam pater nōn dormit et puerum, quī puellam probam pulsat, <div class="area" data-key="verberat"></div>.</div>
+    </div>
+  `
+});
---
+++ exercitia/src/llpsi/nova/nova-04r8nm3p.js
@@ -0,0 +1,84 @@
+$_GET({
+  title: 'Nova Exercitia 4',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>imperātīvus &amp; indicātīvus</i></span></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-ā</div> <div class="area">-ē</div> <div class="area">-e</div> <div class="area">-ī</div></div>
+      <div><div class="area">-at</div> <div class="area">-et</div> <div class="area">-it</div> <div class="area">-it</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Iūlius: “Servum voc<b>ā</b>!” Aemilia servum voc<b>at</b>.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Iūlius: “Dominum salūt<div class="area" data-key="ā">āre</div>!” Dāvus dominum salūt<div class="area" data-key="at">āre</div>.</div>
+      <div>2. Iūlius: “Respond<div class="area" data-key="ē">ēre</div>!” Dāvus respond<div class="area" data-key="et">ēre</div>.</div>
+      <div>3. Iūlius: “Tac<div class="area" data-key="ē">ēre</div>!” Dāvus tac<div class="area" data-key="et">ēre</div>.</div>
+      <div>4. Iūlius: “Sacculum in mēnsā pōn<div class="area" data-key="e">ere</div>!” Dāvus sacculum in mēnsā pōn<div class="area" data-key="it">ere</div>.</div>
+      <div>5. Iūlius: “Baculum sūm<div class="area" data-key="e">ere</div>!” Aemilia baculum sūm<div class="area" data-key="it">ere</div>.</div>
+      <div>6. Iūlius: “Pecūniam numer<div class="area" data-key="ā">āre</div>!” Dāvus pecūniam numer<div class="area" data-key="at">āre</div>.</div>
+      <div>7. Iūlius: Discēd<div class="area" data-key="e">ere</div>!” Dāvus disēd<div class="area" data-key="it">ere</div>.</div>
+      <div>8. Iūlius: “Iūliam voc<div class="area" data-key="ā">āre</div>!” Aemilia Iūliam voc<div class="area" data-key="at">āre</div>.</div>
+      <div>9. Iūlius: “Quīntum aud<div class="area" data-key="ī">īre</div>!” Mārcus Quīntum aud<div class="area" data-key="it">īre</div>.</div>
+      <div>10. Iūlius: “Mēdum interrog<div class="area" data-key="ā">āre</div>!” Dāvus Mēdum interrog<div class="area" data-key="at">āre</div>.</div>
+      <div>11. Aemilia: “Improbum puerum verber<div class="area" data-key="ā">āre</div>!” Iūlius improbum puerum verber<div class="area" data-key="at">āre</div>.</div>
+      <div>12. Aemilia: “Ven<div class="area" data-key="ī">īre</div>!” Ancilla ven<div class="area" data-key="it">īre</div>.</div>
+      <div>13. Mārcus: “Tac<div class="area" data-key="ē">ēre</div> et discēd<div class="area" data-key="e">ere</div>!” Quīntus tac<div class="area" data-key="et">ēre</div> et discēd<div class="area" data-key="it">ere</div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>imperātīvus &amp; vocātīvus</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><i><span class="ff">Exemplum</span></i>:<span style="white-space: pre;">	</span>Dominus: “Ven<b>ī</b>, Dāv<b>e</b>!”</div>
+      </div>
+      <div><br></div>
+      <div>1. Dominus imperat: “Dominum salūt<div class="area" data-key="ā">at</div>, serv<div class="area" data-key="e">us</div>!”</div>
+      <div>2. Dominus imperat: “Tac<div class="area" data-key="ē">et</div> et aud<div class="area" data-key="ī">it</div>, serv<div class="area" data-key="e">us</div>!”</div>
+      <div>3. Dominus imperat: “Respond<div class="area" data-key="ē">et</div>, serv<div class="area" data-key="e">us</div>!”</div>
+      <div>4. Dominus imperat: “Sacculum in mēnsā pōn<div class="area" data-key="e">it</div>, Dāv<div class="area" data-key="e">us</div>!”</div>
+      <div>5. Dominus imperat: “Mēdum voc<div class="area" data-key="ā">at</div>, Dāv<div class="area" data-key="e">us</div>!”</div>
+      <div>6. Servus imperat: “Sacculum vacuum vid<div class="area" data-key="ē">et</div>, domin<div class="area" data-key="e">us</div>!”</div>
+      <div>7. Dominus imperat: “Nummum sūm<div class="area" data-key="e">it</div>, prob<div class="area" data-key="e">us</div> serv<div class="area" data-key="e">us</div>!”</div>
+      <div>8. Dominus imperat: “Discēd<div class="area" data-key="e">it</div>, serv<div class="area" data-key="e">us</div>!”</div>
+      <div>9. Mārcus imperat: “Ven<div class="area" data-key="ī">it</div>, Quīnt<div class="area" data-key="e">us</div>!”</div>
+      <div>10. Mārcus imperat: Iūliam puls<div class="area" data-key="ā">at</div>, Quīnt<div class="area" data-key="e">us</div>!”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Iūlius sacculum <u>suum</u> in mēnsā pōnit. →</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Dāvus sacculum <b>eius</b> (: Iūli<b>ī</b>) in mēnsā videt.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Iūlius pecūniam suam in sacculō pōnit. →</div>
+      <div>Dāvus pecūnuam <div class="area" data-key="eius"></div> (: Iūli<div class="area" data-key="ī">us</div>) in sacculō videt.</div>
+      <div>2. Aemilia baculum <div class="area" data-key="suum"></div> in mēnsā pōnit. →</div>
+      <div>Dāvus baculum eius (: Aemili<div class="area" data-key="ae">a</div>) in mēnsā videt.</div>
+      <div>3. Aemilia librum suum in mēnsā pōnit. →</div>
+      <div>Syra librum <div class="area" data-key="eius"></div> (: Aemili<div class="area" data-key="ae">a</div>) sūmit.</div>
+      <div>4. Iūlius nummum <div class="area" data-key="suum"></div> in sacculō pōnit. →</div>
+      <div>Mēdus nummum <div class="area" data-key="eius"></div> (: Iūli<div class="area" data-key="ī">us</div>) ē sacculō sūmit.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Sacculus Iūliī in mēnsā est. →</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Cuius est sacculus? Eius (: Iūliī) est. Iūlius sacculum suum in mēnsā videt.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Baculum Iūliī in mēnsā est. →</div>
+      <div>2. Pecūnia Iūliī in mēnsā est. →</div>
+      <div>3. Sacculus Dāvī in mēnsā est. →</div>
+      <div>4. Pecūnia Dāvī in sacculō est. →</div>
+      <div>5. Nummus in sacculō Dāvī est. →</div>
+    </div>
+  `
+});
