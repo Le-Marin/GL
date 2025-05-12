b1ed168e75741bfc4a8a6a22e0dc16bc530b694d
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-05-12 17:38:09 +0300

    Add files via upload

--- exercitia/src/llpsi/nova/nova-01zn7lox.js
+++ exercitia/src/llpsi/nova/nova-01zn7lox.js
@@ -24,7 +24,7 @@ $_GET({
       <div><b>Exercitium secundum</b></div>
       <div><br></div>
       <div class="sc">
-        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>īnsula <div class="area">Graecus</div> &gt; īnsula <div class="area">Graeca</div></span></div>
+      <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>īnsula <div class="area">Graecus</div> &gt; īnsula <div class="area">Graeca</div></span></div>
       </div>
       <div><br></div>
       <div>1. Fluviī <div class="area" data-key="magnī">magnus</div>.</div>
@@ -43,25 +43,25 @@ $_GET({
       <div><b>Exercitium tertium</b></div>
       <div><br></div>
       <div class="sc">
-        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Rōma in Italiā est. →</span></div>
-        <div><span class="ff"><span class="ff"><span style="white-space: pre;">		</span></span><span style="white-space: pre;">	</span>Ubi est Rōma? / Num Rōma in Graeciā est? / Estne Rōma in Italiā?</span></div>
+      <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Rōma in Italiā est. →</span></div>
+      <div><span class="ff"><span class="ff"><span style="white-space: pre;">		</span></span><span style="white-space: pre;">	</span>Ubi est Rōma? / Num Rōma in Graeciā est? / Estne Rōma in Italiā?</span></div>
       </div>
       <div><br></div>
-      <div>1. Brundisium in Italiā est. →</div>
-      <div>2. Sicilia īnsula est. →</div>
-      <div>3. Britannia nōn īnsula parva, sed īnsula magna est. →</div>
-      <div>4. Hispānia in Eurōpā est. →</div>
-      <div>5. Aegyptus et Gallia in Eurōpā sunt. →</div>
-      <div>6. Gallia prōvincia Rōmāna est. →</div>
-      <div>7. Aegyptus nōn in Eurōpā, sed in Āfricā est. →</div>
-      <div>8. Tiberis fluvius est. →</div>
-      <div>9. In Arabiā nōn multī, sed paucī fluviī sunt. →</div>
-      <div>10. Tūsculum oppidum Rōmānum est. →</div>
-      <div>11. Nīlus fluvius magnus est. →</div>
-      <div>12. Sparta oppidum est. →</div>
-      <div>13. Brundisium et Tūsculum nōn in Graeciā, sed in Italiā sunt. →</div>
-      <div>14. Īnsula vocābulum Latīnum est. →</div>
-      <div>15. A, B, C nōn litterae Graecae, sed litterae Latīnae sunt. →</div>
+      <div>1. Brundisium in Italiā est. → U<div class="area" data-key="bi"></div> <div class="area" data-key="est"></div> B<div class="area" data-key="rundisium"></div>? / N<div class="area" data-key="um"></div> B<div class="area" data-key="rundisium"></div> <div class="area" data-key="in"></div> G<div class="area" data-key="raeciā"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> B<div class="area" data-key="rundisium"></div> <div class="area" data-key="in"></div> <div class="area" data-key="Italiā"></div>?</div>
+      <div>2. Sicilia īnsula est. → Q<div class="area" data-key="uid"></div> <div class="area" data-key="est"></div> S<div class="area" data-key="icilia"></div>? / N<div class="area" data-key="um"></div> S<div class="area" data-key="icilia"></div> o<div class="area" data-key="ppidum"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> S<div class="area" data-key="icilia"></div> <div class="area" data-key="īnsula"></div>?</div>
+      <div>3. Britannia nōn īnsula parva, sed īnsula magna est. → N<div class="area" data-key="um"></div> B<div class="area" data-key="ritannia"></div> ī<div class="area" data-key="nsula"></div> <div class="area" data-key="parva"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> B<div class="area" data-key="ritannia"></div> ī<div class="area" data-key="nsula"></div> <div class="area" data-key="parva"></div>?</div>
+      <div>4. Hispānia in Eurōpā est. → U<div class="area" data-key="bi"></div> <div class="area" data-key="est"></div> H<div class="area" data-key="ispānia"></div>? / <div class="area" data-key="Num"></div> H<div class="area" data-key="ispānia"></div> <div class="area" data-key="in"></div> Ae<div class="area" data-key="gyptō"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> H<div class="area" data-key="ispānia"></div> <div class="area" data-key="in"></div> Eu<div class="area" data-key="rōpā"></div>?</div>
+      <div>5. Hispānia et Gallia in Eurōpā sunt. → <div class="area" data-key="Ubi"></div> <div class="area" data-key="sunt"></div> H<div class="area" data-key="ispānia"></div> <div class="area" data-key="et"></div> G<div class="area" data-key="allia"></div>? / <div class="area" data-key="Num"></div> <div class="area" data-key="Hispānia"></div> <div class="area" data-key="et"></div> G<div class="area" data-key="allia"></div> <div class="area" data-key="sunt"></div> in Ā<div class="area" data-key="fricā"></div>? / <div class="area" data-key="Suntne"></div> H<div class="area" data-key="ispānia"></div> <div class="area" data-key="et"></div> <div class="area" data-key="Gallia"></div> <div class="area" data-key="in"></div> Eu<div class="area" data-key="rōpā"></div>?</div>
+      <div>6. Gallia prōvincia Rōmāna est. → Qu<div class="area" data-key="id"></div> <div class="area" data-key="est"></div> G<div class="area" data-key="allia"></div>? / N<div class="area" data-key="um"></div> G<div class="area" data-key="allia"></div> <div class="area" data-key="prōvincia"></div> G<div class="area" data-key="raeca"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> G<div class="area" data-key="allia"></div> p<div class="area" data-key="rōvincia"></div> <div class="area" data-key="Rōmāna"></div>?</div>
+      <div>7. Aegyptus nōn in Eurōpā, sed in Āfricā est. → <div class="area" data-key="Num"></div> Ae<div class="area" data-key="gyptus"></div> <div class="area" data-key="in"></div> <div class="area" data-key="Eurōpā"></div> e<div class="area" data-key="st"></div>? / E<div class="area" data-key="stne"></div> Ae<div class="area" data-key="gyptus"></div> <div class="area" data-key="in"></div> <div class="area" data-key="Eurōpā"></div>?</div>
+      <div>8. Tiberis fluvius est. → <div class="area" data-key="Quid"></div> <div class="area" data-key="est"></div> T<div class="area" data-key="iberis"></div>? / <div class="area" data-key="Num"></div> T<div class="area" data-key="iberis"></div> ī<div class="area" data-key="nsula"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> <div class="area" data-key="Tiberus"></div> f<div class="area" data-key="luvius"></div>?</div>
+      <div>9. In Arabiā nōn multī, sed paucī fluviī sunt. → <div class="area" data-key="Num"></div> m<div class="area" data-key="ultī"></div> <div class="area" data-key="sunt"></div> f<div class="area" data-key="luviī"></div> <div class="area" data-key="in"></div> A<div class="area" data-key="rabiā"></div>? / S<div class="area" data-key="untne"></div> <div class="area" data-key="multī"></div> f<div class="area" data-key="luviī"></div> <div class="area" data-key="in"></div> <div class="area" data-key="Arabiā"></div>?</div>
+      <div>10. Tūsculum oppidum Rōmānum est. → <div class="area" data-key="Quid"></div> <div class="area" data-key="est"></div> T<div class="area" data-key="ūsculum"></div>? / N<div class="area" data-key="um"></div> <div class="area" data-key="Tūsculum"></div> o<div class="area" data-key="ppidum"></div> G<div class="area" data-key="raecum"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> T<div class="area" data-key="ūsculum"></div> <div class="area" data-key="oppidum"></div> R<div class="area" data-key="ōmānum"></div>?</div>
+      <div>11. Nīlus fluvius magnus est. → <div class="area" data-key="Quid"></div> <div class="area" data-key="est"></div> N<div class="area" data-key="īlus"></div>? / <div class="area" data-key="Num"></div> N<div class="area" data-key="īlus"></div> f<div class="area" data-key="luvius"></div> <div class="area" data-key="magnus"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> <div class="area" data-key="Nīlus"></div> f<div class="area" data-key="luvius"></div> <div class="area" data-key="magnus"></div>?</div>
+      <div>12. Sparta oppidum est. → <div class="area" data-key="Quid"></div> <div class="area" data-key="est"></div> S<div class="area" data-key="parta"></div>? / N<div class="area" data-key="um"></div> <div class="area" data-key="Sparta"></div> ī<div class="area" data-key="nsula"></div> <div class="area" data-key="est"></div>? / E<div class="area" data-key="stne"></div> S<div class="area" data-key="parta"></div> <div class="area" data-key="oppidum"></div>?</div>
+      <div>13. Brundisium et Tūsculum nōn in Graeciā, sed in Italiā sunt. → <div class="area" data-key="Num"></div> B<div class="area" data-key="rundisium"></div> <div class="area" data-key="et"></div> T<div class="area" data-key="ūsculum"></div> in <div class="area" data-key="Graecia"></div> <div class="area" data-key="sunt"></div>? / <div class="area" data-key="Suntne"></div> B<div class="area" data-key="rundisium"></div> <div class="area" data-key="et"></div> <div class="area" data-key="Tūsculum"></div> <div class="area" data-key="in"></div> <div class="area" data-key="Italiā"></div>?</div>
+      <div>14. <i>Īnsula</i> vocābulum Latīnum est. → Q<div class="area" data-key="uid"></div> <div class="area" data-key="est"></div> <i>ī<div class="area" data-key="nsula"></div></i>? / N<div class="area" data-key="um"></div> <i><div class="area" data-key="īnsula"></div></i> v<div class="area" data-key="ocābulum"></div> <div class="area" data-key="Graecum"></div> <div class="area" data-key="est"></div>? / <div class="area" data-key="Estne"></div> <i>ī<div class="area" data-key="nsula"></div></i> <div class="area" data-key="vocābulum"></div> <div class="area" data-key="Latīnum"></div>?</div>
+      <div>15. A, B, C nōn litterae Graecae, sed litterae Latīnae sunt. → <div class="area" data-key="Num"></div> A, B, C l<div class="area" data-key="itterae"></div> <div class="area" data-key="Graecae"></div> <div class="area" data-key="sunt"></div>? / S<div class="area" data-key="untne"></div> A, B, C <div class="area" data-key="litterae"></div> <div class="area" data-key="Latīnae"></div>?</div>
     </div>
   `
 });
