0910d3b011ef54cc7dec65ff59c543f1b9525ffa
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-05-26 14:24:05 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-07chg57l.js
@@ -0,0 +1,106 @@
+$_GET({
+  title: 'Nova exercitia 7',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">num</div> <div class="area">nōnne</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Iūlius Dāvī dominus est. → <u>Nōnne</u> Iūlius Dāvī dominus est?</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span>Dāvus servus Cornēliī nōn est. → <u>Num</u> Dāvus servus Cornēliī est?</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Iūlius nōn Tūsculō, sed Rōmā venit. → N<div class="area" data-key="um"></div> I<div class="area" data-key="ūlius"></div> <div class="area" data-key="Tūsculō"></div> v<div class="area" data-key="enit"></div>?</div>
+      <div>2. Iūlius nōn ambulat, sed lectīcā vehitur. → N<div class="area" data-key="um"></div> I<div class="area" data-key="ūlius"></div> <div class="area" data-key="ambulat"></div>?</div>
+      <div>3. Iūlia in cubiculō lacrimat. → N<div class="area" data-key="ōnne"></div> I<div class="area" data-key="ūlia"></div> in <div class="area" data-key="cubiculō"></div> <div class="area" data-key="lacrimat"></div>?</div>
+      <div>4. Mēdus in vīllā nōn est, sed Rōmam it. → N<div class="area" data-key="um"></div> M<div class="area" data-key="ēdus"></div> in <div class="area" data-key="vīllā"></div> <div class="area" data-key="est"></div>?</div>
+      <div>5. Iūlius nōn rosam, sed ōsculum Iūliae dat. → N<div class="area" data-key="um"></div> I<div class="area" data-key="ūlius"></div> <div class="area" data-key="rosam"></div> I<div class="area" data-key="ūliae"></div> <div class="area" data-key="dat"></div>?</div>
+      <div>6. Sparta in Graeciā est. → N<div class="area" data-key="ōnne"></div> <div class="area" data-key="Sparta"></div> in <div class="area" data-key="Graeciā"></div> <div class="area" data-key="est"></div>?</div>
+      <div>7. In capitulō tertiō Mārcus Iūliam pulsat. → N<div class="area" data-key="ōnne"></div> in <div class="area" data-key="capitulō"></div> <div class="area" data-key="tertiō"></div> M<div class="area" data-key="ārcus"></div> I<div class="area" data-key="ūliam"></div> <div class="area" data-key="pulsat"></div>?</div>
+      <div>8. Dāvus pecūniam dominī nōn habet. → N<div class="area" data-key="um"></div> D<div class="area" data-key="āvus"></div> <div class="area" data-key="pecūniam"></div> d<div class="area" data-key="ominī"></div> <div class="area" data-key="habet"></div>?</div>
+      <div>9. Dāvus ab Iūliō nōn verberātur. → N<div class="area" data-key="um"></div> D<div class="area" data-key="āvus"></div> <div class="area" data-key="ab"></div> I<div class="area" data-key="ūliō"></div> <div class="area" data-key="verberātur"></div>?</div>
+      <div>10. Via Appia inter Rōmam et Brundisium est. → N<div class="area" data-key="ōnne"></div> v<div class="area" data-key="ia"></div> <div class="area" data-key="Appia"></div> <div class="area" data-key="inter"></div> R<div class="area" data-key="ōmam"></div> <div class="area" data-key="et"></div> <div class="area" data-key="Brundisium"></div> <div class="area" data-key="est"></div>?</div>
+      <div>11. Immō fōrmōsus est nāsus Iūliae. → N<div class="area" data-key="um"></div> n<div class="area" data-key="āsus"></div> <div class="area" data-key="Iūliae"></div> f<div class="area" data-key="oedus"></div> <div class="area" data-key="est"></div>?</div>
+      <div>12. Iūlia rosīs dēlectātur. → N<div class="area" data-key="ōnne"></div> I<div class="area" data-key="ūlia"></div> <div class="area" data-key="rosīs"></div> <div class="area" data-key="dēlectātur"></div>?</div>
+      <div>13. Ōstiārius lectīcam nōn portat. → N<div class="area" data-key="um"></div> <div class="area" data-key="ōstiārius"></div> l<div class="area" data-key="ectīcam"></div> <div class="area" data-key="portat"></div>?</div>
+      <div>14. Aemilia Iūliō ōsculum dat. → N<div class="area" data-key="ōnne"></div> <div class="area" data-key="Aemilia"></div> I<div class="area" data-key="ūliō"></div> <div class="area" data-key="ōsculum"></div> <div class="area" data-key="dat"></div>?</div>
+      <div>15. In saccīs māla et pira īnsunt. → N<div class="area" data-key="ōnne"></div> m<div class="area" data-key="āla"></div> <div class="area" data-key="et"></div> p<div class="area" data-key="ira"></div> <div class="area" data-key="īnsunt"></div> in <div class="area" data-key="saccīs"></div>?</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>accūsātīvus vs ablātīvus</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Quō it Mārcus? Mārcus in ātrium it.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span>Ubi est Mārcus? Mārcus in ātriō est.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Quō it Mārcus? Mārcus in <div class="area" data-key="peristȳlum">peristȳlum</div> it.</div>
+      <div>2. Mārcus, quī <div class="area" data-key="peristȳlō">peristȳlum</div> est, in <div class="area" data-key="hortum">hortus</div> it.</div>
+      <div>3. Ubi est Mārcus? Mārcus in <div class="area" data-key="hortō">hortus</div> est. </div>
+      <div>4. Aemilia: “Vēnī in <div class="area" data-key="vīllam">vīlla</div>, Mārce!”</div>
+      <div>5. Mārcus in <div class="area" data-key="vīllā">vīlla</div> est, et in <div class="area" data-key="cubiculum">cubiculum</div> intrat.</div>
+      <div>6. Iūlius nōn est in <div class="area" data-key="vīllā">vīlla</div>, sed in <div class="area" data-key="oppidō">oppidum</div> est.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">sē</div> <div class="area">eum</div> <div class="area">eam</div></div></div>
+      <div><br></div>
+      <div>1. Mārcus Iūliam audit <div class="area" data-key="eam"></div>que pulsat.</div>
+      <div>2. Aemilia Iūlium amat et <div class="area" data-key="eum"></div> exspectat.</div>
+      <div>3. Iūlia <div class="area" data-key="sē"></div> in speculō, quod ante <div class="area" data-key="sē"></div> tenet, videt.</div>
+      <div>4. Dāvus in ātriō est; Iūlius intrat, et <div class="area" data-key="eum"></div> ad <div class="area" data-key="sē"></div> vocat.</div>
+      <div>5. Iūlius ūnum nummum sūmit <div class="area" data-key="eum"></div>que Dāvō dat.</div>
+      <div>6. Syra in cubiculum intrat, et Iūlia <div class="area" data-key="eam"></div> in speculō post <div class="area" data-key="sē"></div> videt.</div>
+      <div>7. Domina ancillam vocat <div class="area" data-key="eam"></div>que interrogat.</div>
+      <div>8. Quīntus ā servō vocātur et <div class="area" data-key="sē"></div> vertit.</div>
+      <div>9. Ancilla fenestram, quam ante <div class="area" data-key="sē"></div> videt, aperit.</div>
+      <div>10. Aemilia sōla est, et <div class="area" data-key="sē"></div> interrogat: “Advenitne Iūlius?”</div>
+      <div>11. Dominus intrat, et ōstiārius ōstium post <div class="area" data-key="eum"></div> claudit.</div>
+      <div>12. Iūlia rosās carpit, quia rosae <div class="area" data-key="eam"></div> dēlectant.</div>
+      <div>13. Pecūnia ab Iūliō sūmitur, quī <div class="area" data-key="eam"></div> numerat.</div>
+      <div>14. Mēdus Cornēlium videt, neque <div class="area" data-key="eum"></div> salūtat.</div>
+      <div>15. Ursus ante lectīcam, Dāvus Syrusque autem post <div class="area" data-key="eam"></div> ambulant.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>dātīvus</i></span></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-ō</div> <div class="area">-ae</div> <div class="area">-īs</div></div>
+      <div><div class="area">eī</div> <div class="area">iīs</div> <div class="area">cui</div> </div></div>
+      <div><br></div>
+      <div>1. Dominus serv<div class="area" data-key="ō"></div> nummum dat. <div class="area" data-key="cui"></div> nummum dat Iūlius? Dāv<div class="area" data-key="ō"></div> nummum dat: servus <div class="area" data-key="cui"></div> Iūlius nummum dat Dāvus est.</div>
+      <div>2. Iūli<div class="area" data-key="ō"></div> Iūlia rosam dat. <div class="area" data-key="cui"></div> rosam dat Iūlia? Iūlia <div class="area" data-key="eī"></div> rosam dat.</div>
+      <div>3. Iūlius serv<div class="area" data-key="īs"></div> et līber<div class="area" data-key="īs"></div> su<div class="area" data-key="īs"></div> māla et pira, Aemili<div class="area" data-key="ae"></div> autem ōsculum dat.</div>
+      <div>4. Iūli<div class="area" data-key="ae"></div> Iūlius nōn sōlum ōsculum, sed etiam mālum dat. <div class="area" data-key="eī"></div> ōsculum et mālum Iūlius dat.</div>
+      <div>5. Iūlius Cornēli<div class="area" data-key="ō"></div>, amīc<div class="area" data-key="ō"></div> su<div class="area" data-key="ō"></div>, magnum librum dat. <div class="area" data-key="cui"></div> Iūlius librum dat? <div class="area" data-key="eī"></div> librum dat.</div>
+      <div>6. Domina Syr<div class="area" data-key="ae"></div> Dēl<div class="area" data-key="ae"></div>que, bon<div class="area" data-key="īs"></div> ancill<div class="area" data-key="īs"></div>, rosās et līlia dat.</div>
+      <div>7. Fēmina <div class="area" data-key="cui"></div> Iūlius ōsculum dat Aemilia est. Amīcus <div class="area" data-key="cui"></div> librum dat Cornēlius est.</div>
+      <div>8. Iūlia māl<div class="area" data-key="ō"></div> su<div class="area" data-key="ō"></div> ōsculum dat.</div>
+      <div>9. Dominus serv<div class="area" data-key="ō"></div> prob<div class="area" data-key="ō"></div>, quī aliōs servōs nōn accūsat, pecūniam dat.</div>
+      <div>10. Domina ancillam ad sē vocat <div class="area" data-key="eī"></div>que mālum dat.</div>
+      <div>11. Dominae ancillās ad sē vocant <div class="area" data-key="iīs"></div>que māla dant.</div>
+      <div>12. Vir fēmin<div class="area" data-key="ae"></div> su<div class="area" data-key="ae"></div> ōsculum dat.</div>
+      <div>13. Virī fēmin<div class="area" data-key="īs"></div> su<div class="area" data-key="īs"></div> ōscula dant.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quīntum</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Iūlius / ōsculum / Aemilia =</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Cui Iūlius ōsculum dat? Iūlius Aemiliae ōsculum dat. Fēmina cui Iūlius ōsculum dat Aemilia est.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Iūlius / nummus / Dāvus = <div class="area" data-key="cui"></div> I<div class="area" data-key="ūlius"></div> <div class="area" data-key="nummum"></div> <div class="area" data-key="dat"></div>? I<div class="area" data-key="ūlius"></div> <div class="area" data-key="Dāvō"></div> <div class="area" data-key="nummum"></div> <div class="area" data-key="dat"></div>. V<div class="area" data-key="ir"></div> <div class="area" data-key="cui"></div> I<div class="area" data-key="ūlius"></div> <div class="area" data-key="nummum"></div> <div class="area" data-key="dat"></div> D<div class="area" data-key="āvus"></div> <div class="area" data-key="est"></div>.</div>
+      <div>2. Aemilia / ōsculum / Iūlius = <div class="area" data-key="cui"></div> Ae<div class="area" data-key="milia"></div> <div class="area" data-key="ōsculum"></div> <div class="area" data-key="dat"></div>? <div class="area" data-key="Aemilia"></div> <div class="area" data-key="Iūliō"></div> ō<div class="area" data-key="sculum"></div> <div class="area" data-key="dat"></div>. V<div class="area" data-key="ir"></div> <div class="area" data-key="cui"></div> Ae<div class="area" data-key="milia"></div> <div class="area" data-key="ōsculum"></div> <div class="area" data-key="dat"></div> I<div class="area" data-key="ūlius"></div> <div class="area" data-key="est"></div>.</div>
+      <div>3. dominus / mālum / Syra = <div class="area" data-key="cui"></div> <div class="area" data-key="dominus"></div> m<div class="area" data-key="ālum"></div> <div class="area" data-key="dat"></div>? D<div class="area" data-key="ominus"></div> <div class="area" data-key="Syrae"></div> <div class="area" data-key="mālum"></div> <div class="area" data-key="dat"></div>. <div class="area" data-key="fēmina"></div> <div class="area" data-key="cui"></div> d<div class="area" data-key="ominus"></div> <div class="area" data-key="mālum"></div> <div class="area" data-key="dat"></div> S<div class="area" data-key="yra"></div> <div class="area" data-key="est"></div>.</div>
+      <div>4. Iūlia / rosa / Iūlius = <div class="area" data-key="cui"></div> I<div class="area" data-key="ūlia"></div> <div class="area" data-key="rosam"></div> <div class="area" data-key="dat"></div>? <div class="area" data-key="Iūlia"></div> I<div class="area" data-key="ūliō"></div> <div class="area" data-key="rosam"></div> <div class="area" data-key="dat"></div>. <div class="area" data-key="Vir"></div> <div class="area" data-key="cui"></div> I<div class="area" data-key="ūlia"></div> <div class="area" data-key="rosam"></div> <div class="area" data-key="dat"></div> <div class="area" data-key="est"></div> I<div class="area" data-key="ūlius"></div>.</div>
+      <div>5. dominus / pirum / Dāvus = <div class="area" data-key="cui"></div> <div class="area" data-key="dominus"></div> p<div class="area" data-key="irum"></div> <div class="area" data-key="dat"></div>? <div class="area" data-key="dominus"></div> <div class="area" data-key="Dāvō"></div> <div class="area" data-key="pirum"></div> <div class="area" data-key="dat"></div>. V<div class="area" data-key="ir"></div> <div class="area" data-key="cui"></div> d<div class="area" data-key="ominus"></div> <div class="area" data-key="pirum"></div> <div class="area" data-key="dat"></div> <div class="area" data-key="est"></div> D<div class="area" data-key="āvus"></div>.</div>
+    </div>
+  `
+});
