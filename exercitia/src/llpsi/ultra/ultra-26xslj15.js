4590cc47e3dba3040085a70d0d1c7b41a6a99036
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-06-13 13:37:49 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-26xslj15.js
@@ -0,0 +1,49 @@
+$_GET({
+  title: 'Ultra 26',
+  html: /*html*/`
+    <div class="row">
+      <div><div><b>1. Converte in sermōnem Latīnum gerundiō ūtēns.</b></div>
+      <div><br></div>
+      <div><span style="white-space: pre;">	</span><b>GEN</b></div>
+      <div><br></div>
+      <div><i>бремя царствования</i> — onus r<div class="area" data-key="egendī"></div></div>
+      <div><i>попытка взять город</i> — cōnātus u<div class="area" data-key="rbem"></div> c<div class="area" data-key="apiendī"></div></div>
+      <div><i>план спасти заложников</i> — cōnsilium <div class="area" data-key="obsidēs">obsidēs</div> s<div class="area" data-key="ervandī"></div></div>
+      <div><i>ради переговоров</i> — col<div class="area" data-key="loquendī"></div> c<div class="area" data-key="ausā"></div></div>
+      <div><i>жаждущий вернуться сюда</i> — h<div class="area" data-key="ūc"></div> r<div class="area" data-key="edeundī | evertendī"></div> cupidus</div>
+      <div><i>стремящий узнать больше</i> — studiōsus p<div class="area" data-key="lūra | lūs"></div> cog<div class="area" data-key="nōscendī"></div></div>
+      <div><br></div>
+      <div><span style="white-space: pre;">	</span><b>ACC</b></div>
+      <div><br></div>
+      <div><i>для размышления</i> — <div class="area" data-key="ad"></div> c<div class="area" data-key="ōgitandum"></div></div>
+      <div><i>чтобы лучше понять</i> — <div class="area" data-key="ad"></div> m<div class="area" data-key="elius"></div> <div class="area" data-key="intellegendum"></div></div>
+      <div><i>для наилучшего воспитания</i> — <div class="area" data-key="ad"></div> o<div class="area" data-key="ptimē"></div> ē<div class="area" data-key="ducandō"></div></div>
+      <div><i>для длительного молчания</i> — <div class="area" data-key="ad"></div> d<div class="area" data-key="iū"></div> <div class="area" data-key="tacendum"></div></div>
+      <div><i>чтобы меньше сомневаться</i> — <div class="area" data-key="ad"></div> <div class="area" data-key="minus"></div> d<div class="area" data-key="ubitandum"></div></div>
+      <div><i>чтобы отважнее сражаться</i> — <div class="area" data-key="ad"></div> f<div class="area" data-key="ortius"></div> <div class="area" data-key="pugnandum"></div></div>
+      <div><br></div>
+      <div><span style="white-space: pre;">	</span><b>ABL</b></div>
+      <div><br></div>
+      <div><i>о купле и продаже</i> — <div class="area" data-key="dē"></div> <div class="area" data-key="emendō"></div> et <div class="area" data-key="vēndendō"></div></div>
+      <div><i>плохо управляя кораблем</i> — n<div class="area" data-key="āvem"></div> <div class="area" data-key="male"></div> g<div class="area" data-key="ubernandō"></div></div>
+      <div><i>не зная этого</i> — h<div class="area" data-key="oc | aec"></div> n<div class="area" data-key="esciendō"></div></div>
+      <div><i>от неправильного верования</i> — <div class="area" data-key="ā"></div> p<div class="area" data-key="rāvē"></div> c<div class="area" data-key="redendō"></div></div>
+      <div><i>из непослушания родителям</i> — <div class="area" data-key="ē | ex"></div> <div class="area" data-key="parentibus"></div> nōn <div class="area" data-key="pārendō"></div></div>
+      <div><i>в позорном поведении (~ делании)</i> — <div class="area" data-key="in"></div> t<div class="area" data-key="urpiter"></div> a<div class="area" data-key="gendō"></div><br><br></div></div>
+    </div>
+    <div class="row">
+      <div><b>2. Dā gerundium prō īnfīnītīvō: gen, abl aut “ad” + acc. Obiectum gerundiī pōne in cāsū rēctō.</b></div>
+      <div><br></div>
+      <div>1. Pauper miserque, spērābat sē deōs <div class="area" data-key="flendō">flēre</div> mollīre posse.</div>
+      <div>2. Quamquam multī malum fierī vidēbant, nēmō <div class="area" data-key="ad iuvandum">iuvāre</div> vēnit.</div>
+      <div>3. Gāius: “Nōn <div class="area" data-key="pecūniam">pecūnia</div> <div class="area" data-key="augendī">augēre</div> causā, sed amōre marītus tuus factus sum.”</div>
+      <div>4. <div class="area" data-key="gemmās">Gemmae</div> <div class="area" data-key="habendī">habēre</div> cupida, Gāia fūrtum fēcit.</div>
+      <div>5. Frātrēs Iōsēphī cēpērunt cōnsilium <div class="area" data-key="illum">ille</div> <div class="area" data-key="occīdendī">occīdere</div>.</div>
+      <div>6. Ultimīs verbīs in scaenā dictīs, spectātōrēs <div class="area" data-key="ad clāmandum">clāmāre</div> <div class="area" data-key="plaudendum">plaudere</div>que surrēxērunt.</div>
+      <div>7. Recūsātus, adulēscēns iam magis īnflammātur cupiditāte <div class="area" data-key="amōrem">amor</div> puellae <div class="area" data-key="adipīscendī">adipīscī</div>.</div>
+      <div>8. Inter aliās, sunt Mūsae artium <div class="area" data-key="scrībendī">scrībere</div>, <div class="area" data-key="canendī">canere</div>, <div class="area" data-key="saltandī">saltāre</div>.</div>
+      <div>9. Sāga multum temporis cōnsūmpsit in <div class="area" data-key="pōtiōnēs">pōtiōnēs</div> <div class="area" data-key="magicās">magicae</div> <div class="area" data-key="compōnendō">compōnere</div>.</div>
+      <div>10. Mārcus Porcius Catō: “Ōrātor est vir bonus, <div class="area" data-key="dīcendī">dīcere</div> perītus.”</div>
+    </div>
+  `
+});
