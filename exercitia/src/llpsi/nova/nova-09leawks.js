073a0963c92008d863aaea3ff9661456c6df5b13
Le-Marin <119621011+Le-Marin@users.noreply.github.com>
2025-07-16 18:45:56 +0300

    ~nipaa

---
+++ exercitia/src/llpsi/nova/nova-09leawks.js
@@ -0,0 +1,135 @@
+$_GET({
+  title: 'Nova exercitia 9',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">— / -is</div> <div class="area">-em</div> <div class="area">-is</div> <div class="area">-ī</div> <div class="area">-e</div></div>
+      <div><div class="area">-ēs</div> <div class="area">-ēs</div> <div class="area">-um / -ium</div> <div class="area">-ibus</div> <div class="area">-ibus</div></div></div>
+      <div><br></div>
+      <div>1. Pāstor ov<div class="area" data-key="ēs"></div> numerat. Ov<div class="area" data-key="ēs"></div> ā pāstōr<div class="area" data-key="e"></div> numerantur.</div>
+      <div>2. Ov<div class="area" data-key="is"></div> ā pāstōre aquam et cibum accipit: pāstor ov<div class="area" data-key="ī"></div> aquam et cibum dat.</div>
+      <div>3. Ov<div class="area" data-key="ēs"></div> herbam edunt: cibus ov<div class="area" data-key="ium"></div> herba est. Pāstōr<div class="area" data-key="ēs"></div> pānem edunt: cibus pāstōr<div class="area" data-key="um"></div> pānis est.</div>
+      <div>4. Pāstor in umbrā arboris iacet cum can<div class="area" data-key="e"></div> et ov<div class="area" data-key="ibus"></div>.</div>
+      <div>5. Pāstor, quī dormit, ov<div class="area" data-key="em"></div>  nigram, quae rīvum petit, nōn videt.</div>
+      <div>6. Ov<div class="area" data-key="is"></div> nigra ov<div class="area" data-key="ēs"></div> albās relinquit.</div>
+      <div>7. Numerus ov<div class="area" data-key="ium"></div> albārum nōn centum, sed ūndēcentum est.</div>
+      <div>8. Pāstor, quī ov<div class="area" data-key="em"></div> nigram quaerit, ab ov<div class="area" data-key="ibus"></div> albīs cum can<div class="area" data-key="e"></div> discēdit.</div>
+      <div>9. Ovis nigra procul ā pāstōr<div class="area" data-key="e"></div> in silvā errat.</div>
+      <div>10. Canis vestīgia ov<div class="area" data-key="is"></div> in terrā reperit neque ov<div class="area" data-key="em"></div> ipsam videt.</div>
+      <div>11. Canis pāstōr<div class="area" data-key="em"></div> per silvam ad ov<div class="area" data-key="em"></div> nigram dūcit. Pāstor ā can<div class="area" data-key="e"></div> dūcitur.</div>
+      <div>12. Lupus ov<div class="area" data-key="em"></div> videt et ab ov<div class="area" data-key="e"></div> vidētur.</div>
+      <div>13. Canis accurrit, et lupus ov<div class="area" data-key="em"></div> relinquit.</div>
+      <div>14. Pāstor ovem nigram ad cēterās ov<div class="area" data-key="ēs"></div> dūcit.</div>
+      <div>15. Pāstor can<div class="area" data-key="ī"></div> cibum dat. Dominī pāstōr<div class="area" data-key="ibus"></div> suīs cibum dant.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div><i>arbor magna</i>, <i>arborēs magnae</i></div>
+      <div>arborem <div class="area" data-key="magnam"></div>, <div class="area" data-key="arborēs"></div> magnās</div>
+      <div><div class="area" data-key="arboris"></div> magnae, arborum <div class="area" data-key="magnārum"></div></div>
+      <div><div class="area" data-key="arborī"></div> magnae, <div class="area" data-key="arboribus"></div> magnīs</div>
+      <div><div class="area" data-key="arbore"></div> magnā, arboribus <div class="area" data-key="magnīs"></div></div>
+      <div><br></div>
+      <div><i>pāstor bonus</i>, <div class="area" data-key="pāstōrēs"></div> <div class="area" data-key="bonī"></div></div>
+      <div><div class="area" data-key="pāstōrem"></div> bonum, pāstōrēs <div class="area" data-key="bonōs"></div></div>
+      <div>pāstōris <div class="area" data-key="bonī"></div>, <div class="area" data-key="pāstōrum"></div> bonōrum</div>
+      <div><div class="area" data-key="pāstōrī"></div> bonō, pāstōribus <div class="area" data-key="bonīs"></div></div>
+      <div><div class="area" data-key="pāstōre"></div> bonō, <div class="area" data-key="pāstōribus"></div> bonīs</div>
+      <div><br></div>
+      <div><i>magna nūbēs</i>, <div class="area" data-key="magnae"></div> <div class="area" data-key="nūbēs"></div></div>
+      <div>magnam <div class="area" data-key="nūbem"></div>, <div class="area" data-key="magnās"></div> nūbēs</div>
+      <div><div class="area" data-key="magnae"></div> nūbis, magnārum <div class="area" data-key="nūbium"></div></div>
+      <div>magnae <div class="area" data-key="nūbī"></div>, magnīs <div class="area" data-key="nūbibus"></div></div>
+      <div><div class="area" data-key="magnā"></div> nūbe, magnīs <div class="area" data-key="nūbibus"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>: Iūlius / servī = Iūlius servōrum dominus est.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Iūlius / pāstor = I<div class="area" data-key="ūlius"></div> p<div class="area" data-key="āstōris"></div> d<div class="area" data-key="ominus"></div> <div class="area" data-key="est"></div>.</div>
+      <div>2. pāstor / canis = P<div class="area" data-key="āstor"></div> c<div class="area" data-key="anis"></div> d<div class="area" data-key="ominus"></div> <div class="area" data-key="est"></div>.</div>
+      <div>3. pāstor / ovēs albae ovisque nigra = P<div class="area" data-key="āstor"></div> o<div class="area" data-key="ium"></div> a<div class="area" data-key="lbārum"></div> o<div class="area" data-key="vis"></div>que n<div class="area" data-key="igrae"></div> d<div class="area" data-key="ominus"></div> <div class="area" data-key="est"></div>.</div>
+      <div>4. Iūlius et Cornēlius / multī pāstōrēs = I<div class="area" data-key="ūlius"></div> <div class="area" data-key="et"></div> C<div class="area" data-key="ornēlius"></div> m<div class="area" data-key="ultōrum"></div> <div class="area" data-key="pāstōrum"></div> d<div class="area" data-key="ominī"></div> <div class="area" data-key="sunt"></div>.</div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>: Iūlius / servus = Iūlius ā servō discēdit.</span></div>
+      </div>
+      <div><br></div>
+      <div>5. pāstor / canis = P<div class="area" data-key="āstor"></div> <div class="area" data-key="ā cane"></div> d<div class="area" data-key="iscēdit"></div>.</div>
+      <div>6. pāstōrēs / canēs = P<div class="area" data-key="āstōrēs"></div> <div class="area" data-key="ā canibus"></div> d<div class="area" data-key="iscēdunt"></div>.</div>
+      <div>7. ovis nigra / ovēs albae = O<div class="area" data-key="vis"></div> <div class="area" data-key="nigra"></div> <div class="area" data-key="ab ovibus"></div> a<div class="area" data-key="lbīs"></div> <div class="area" data-key="discēdit"></div>.</div>
+      <div>8. ovis nigra / probus pāstor = <div class="area" data-key="Ovis"></div> n<div class="area" data-key="igra"></div> <div class="area" data-key="ā probō"></div> p<div class="area" data-key="āstōre"></div> <div class="area" data-key="discēdit"></div>.</div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>: Iūlius / servus / mālum = Iūlius servō mālum dat.</span></div>
+      </div>
+      <div><br></div>
+      <div>9. pāstor / canis / cibus = P<div class="area" data-key="āstor"></div> c<div class="area" data-key="anī"></div> <div class="area" data-key="cibum"></div> d<div class="area" data-key="at"></div>.</div>
+      <div>10. Iūlius / probus pāstor / pānis = I<div class="area" data-key="ūlius"></div> p<div class="area" data-key="robō pāstōrī"></div> <div class="area" data-key="pānem"></div> d<div class="area" data-key="at"></div>.</div>
+      <div>11. dominī / pāstōrēs / pānis = <div class="area" data-key="Dominī"></div> <div class="area" data-key="pāstōribus"></div> p<div class="area" data-key="ānem"></div> <div class="area" data-key="dant"></div>.</div>
+      <div>12. pāstōrēs / ovēs albae / aqua = <div class="area" data-key="Pāstōrēs"></div> o<div class="area" data-key="vibus albīs"></div> a<div class="area" data-key="quam"></div> <div class="area" data-key="dant"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div>
+      <div>1. Iūlius mult<div class="area" data-key="ōs"></div> pāstōr<div class="area" data-key="ēs"></div> habet: mult<div class="area" data-key="ōrum"></div> pāstōr<div class="area" data-key="um"></div> dominus est.</div>
+      <div>2. Pāstor can<div class="area" data-key="ī"></div> su<div class="area" data-key="ō"></div> cibum dat. Dominī pāstōr<div class="area" data-key="ibus"></div> su<div class="area" data-key="īs"></div> pānem dant.</div>
+      <div>3. Pāstor arbor<div class="area" data-key="em"></div> magn<div class="area" data-key="am"></div> videt. Arbor magn<div class="area" data-key="a"></div> ā bon<div class="area" data-key="ō"></div> pāstōr<div class="area" data-key="e"></div> vidētur.</div>
+      <div>4. Pāstor ov<div class="area" data-key="em"></div> nigr<div class="area" data-key="am"></div> et ov<div class="area" data-key="ēs"></div> alb<div class="area" data-key="ās"></div> dūcit. Ov<div class="area" data-key="is"></div> nigr<div class="area" data-key="a"></div> et ov<div class="area" data-key="ēs"></div> alb<div class="area" data-key="ae"></div> ā pāstōre dūcuntur.</div>
+      <div>5. Puerī nōn nōn ūn<div class="area" data-key="am"></div> nūb<div class="area" data-key="em"></div>, sed mult<div class="area" data-key="ās"></div> nūb<div class="area" data-key="ēs"></div> in caelō vident.</div>
+      <div>6. Ov<div class="area" data-key="ium"></div> alb<div class="area" data-key="ārum"></div> numerus ūndēcentum est.</div>
+      <div>7. Iūlia h<div class="area" data-key="anc"></div> canem, nōn ill<div class="area" data-key="amm"></div> aspicit.</div>
+      <div>8. Inter magn<div class="area" data-key="ōs"></div> mont<div class="area" data-key="ēs"></div> mult<div class="area" data-key="ae"></div> vall<div class="area" data-key="ēs"></div> sunt.</div>
+      <div>9. Sub magn<div class="area" data-key="ā"></div> arbor<div class="area" data-key="e"></div> umbra est, quam pāstor cum ov<div class="area" data-key="ibus"></div> su<div class="area" data-key="īs"></div> petit.</div>
+      <div>10. Pāstor ab ov<div class="area" data-key="ibus"></div> alb<div class="area" data-key="īs"></div> discēdit et ov<div class="area" data-key="em"></div> nigr<div class="area" data-key="am"></div> in silvā quaerit.</div>
+      <div>11. Pāstor nōn cum ov<div class="area" data-key="ibus"></div> alb<div class="area" data-key="īs"></div>, sed cum ov<div class="area" data-key="e"></div> nigr<div class="area" data-key="ā"></div> est.</div>
+      <div>12. In parv<div class="area" data-key="ō"></div> coll<div class="area" data-key="e"></div> pāstor magn<div class="area" data-key="am"></div> arbor<div class="area" data-key="em"></div> videt.</div>
+      <div>13. In silvā mult<div class="area" data-key="ae"></div> arbor<div class="area" data-key="ēs"></div> sunt, quae umbram dant pāstōr<div class="area" data-key="ī"></div> et can<div class="area" data-key="ī"></div> et ov<div class="area" data-key="ī"></div> nigr<div class="area" data-key="ae"></div>.</div>
+      <div>14. Pāstor ov<div class="area" data-key="em"></div> nigr<div class="area" data-key="am"></div> in umerōs impōnit eamque ad parv<div class="area" data-key="um"></div> coll<div class="area" data-key="em"></div> portat, ubi ali<div class="area" data-key="ae"></div> ov<div class="area" data-key="ēs"></div> sine pāstōr<div class="area" data-key="e"></div> errant.</div>
+      <div>15. Iūlius ov<div class="area" data-key="is"></div> nigr<div class="area" data-key="ae"></div> ov<div class="area" data-key="ium"></div>que alb<div class="area" data-key="ārum"></div> dominus est.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quīntum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>Pāstor habet</i>: [<div class="area">x</div>] multīs ovibus, [<div class="area">v</div>] <u>multās ovēs</u>, [<div class="area">x</div>] multae ovēs, [<div class="area">x</div>] multam ovem.</span></div>
+      <div><br></div>
+      <div>1. <i>Pāstor cibum dat</i>: [<div class="area" data-key="x"></div>] canis niger, [<div class="area" data-key="x"></div>] canī nigrī, [<div class="area" data-key="v"></div>] canī nigrō, [<div class="area" data-key="x"></div>] canem nigrum.</div>
+      <div>2. <i>In colle est</i>: [<div class="area" data-key="x"></div>] magnā arbore, [<div class="area" data-key="x"></div>] magnam arborem, [<div class="area" data-key="x"></div>] magnae arboris, [<div class="area" data-key="v"></div>] magna arbor.</div>
+      <div>3. <i>Ovis nigra relinquit</i>: [<div class="area" data-key="x"></div>] ovis albās, [<div class="area" data-key="v"></div>] ovēs albās, [<div class="area" data-key="x"></div>] ovibus albīs, [<div class="area" data-key="x"></div>] ovium albium.</div>
+      <div>4. <i>Iūlius dominus est</i>: [<div class="area" data-key="x"></div>] multās ovēs, [<div class="area" data-key="x"></div>] multae ovēs, [<div class="area" data-key="x"></div>] multōrum ovium, [<div class="area" data-key="v"></div>] multārum ovium.</div>
+      <div>5. <i>In caelō vidētur</i>: [<div class="area" data-key="x"></div>] nūllam nūbem, [<div class="area" data-key="v"></div>] nūlla nūbēs, [<div class="area" data-key="x"></div>] nūllae nūbēs, [<div class="area" data-key="x"></div>] nūllās nūbēs.</div>
+      <div>6. <i>In silvā sunt</i>: [<div class="area" data-key="x"></div>] multī arborēs, [<div class="area" data-key="v"></div>] multae arborēs, [<div class="area" data-key="x"></div>] multās arborēs, [<div class="area" data-key="x"></div>] multae arbor.</div>
+      <div>7. <i>Iūlius habet</i>: [<div class="area" data-key="x"></div>] multōrum pāstōrum, [<div class="area" data-key="x"></div>] multās pāstōrēs, [<div class="area" data-key="v"></div>] multōs pāstōrēs, [<div class="area" data-key="x"></div>] multum pāstōrem.</div>
+      <div>8. <i>Magna arbor est in</i>: [<div class="area" data-key="v"></div>] parvō colle, [<div class="area" data-key="x"></div>] parvā colle, [<div class="area" data-key="x"></div>] parvum collem, [<div class="area" data-key="x"></div>] parvī collis.</div>
+      <div>9. <i>Arbor umbram dat</i>: [<div class="area" data-key="x"></div>] ovis albīs, [<div class="area" data-key="x"></div>] ovis albibus, [<div class="area" data-key="x"></div>] ovēs albās, [<div class="area" data-key="v"></div>] ovibus albīs.</div>
+      <div>10. <i>Iūlius dominus est</i>: [<div class="area" data-key="x"></div>] bonum pāstōrem, [<div class="area" data-key="v"></div>] bonī pāstōris, [<div class="area" data-key="x"></div>] bonō pāstōre.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium sextum</b></div>
+      <div><br></div>
+      <div>Quid videt Iūlius? Iūlis videt...</div>
+      <div><br></div>
+      <div><img class="nova-img" src="https://le-marin.github.io/GL/exercitia/src/llpsi/nova/img/09-1.jpg"></div>
+      <div><br></div>
+      <div>1. sōl<div class="area">em</div> in cael<div class="area">ō</div></div>
+      <div>2. mult<div class="area" data-key="ōs"></div> mont<div class="area" data-key="ēs"></div></div>
+      <div>3. arborem magn<div class="area" data-key="am"></div> in coll<div class="area" data-key="e"></div></div>
+      <div>4. mult<div class="area" data-key="ās"></div> ovēs alb<div class="area" data-key="ās"></div></div>
+      <div>5. silv<div class="area" data-key="am"></div> cum mult<div class="area" data-key="īs"></div> arboribus</div>
+      <div>6. coll<div class="area" data-key="em"></div> parv<div class="area" data-key="um"></div></div>
+      <div>7. rīv<div class="area" data-key="um"></div> prope silv<div class="area" data-key="am"></div></div>
+      <div>8. umbr<div class="area" data-key="am"></div> sub arbor<div class="area" data-key="e"></div></div>
+      <div>9. pāstōr<div class="area" data-key="em"></div> cum can<div class="area" data-key="e"></div></div>
+      <div>10. ūn<div class="area" data-key="am"></div> ovem nigr<div class="area" data-key="am"></div></div>
+    </div>
+  `
+});
--- lexicon/script.js
+++ lexicon/script.js
@@ -21,6 +21,9 @@
   const button = search.nextElementSibling;
   const list = button.nextElementSibling;
 
+  const htmlCache = {};
+  const style = document.head.appendChild(document.createElement('style'));
+
   const now = Date.now();
   let mv = script ? +(script.src.match(/\d+$/) || [now])[0] : now;
   let selectedListElem = null;
@@ -33,12 +36,21 @@
     if (!list.childElementCount) renderList();
   });
 
-  search.addEventListener('keydown', function(e) {
+  search.addEventListener('keydown', async function(e) {
     switch (e.key) {
       case 'Enter':
         e.preventDefault();
         if (selectedListElem) selectedListElem.click();
-        else searchWord();
+        else {
+          const word = this.value.trim();
+
+          if (!word) return;
+
+          const matches = await hasMatches(word);
+          const state = { word, aside: +!matches };
+          history.pushState(state, '', `#${word}`);
+          onPopState({state});
+        }
         break;
       case 'ArrowUp':
       case 'ArrowDown':
@@ -49,29 +61,64 @@
   });
 
   button.addEventListener('click', function() {
+    const Event = window.KeyboardEvent || window.Event;
     search.focus();
-    searchWord();
+    search.dispatchEvent(new Event('keydown', { key: 'Enter' }));
   });
 
   list.addEventListener('click', function(e) {
     const trg = e.target;
-    if (!trg.matches('.search-list__item')) return;
+    if (!trg.matches('a')) return;
 
+    e.preventDefault();
     rBox.innerHTML = '';
-    searchWord(trg.textContent, false);
+    const word = trg.textContent;
+    const state = { word, aside: 0 };
+    history.pushState(state, '', `#${word}`);
+    onPopState({state});
   });
 
   rBox.addEventListener('click', function(e) {
     const trg = e.target;
     if (!trg.matches('a')) return;
 
+    const word = trg.textContent;
+    const state = { word, aside: 1 };
+
     e.preventDefault();
-
-    [...rBox.children].forEach((el) => el.classList.remove('_active'));
-    trg.classList.add('_active');
-
-    searchWord(trg.textContent, false);
+    history.pushState(state, '', `#${word}`);
+    onPopState({state});
   });
+
+  onPopState(history);
+  window.addEventListener('popstate', onPopState);
+
+  async function onPopState(e) {
+    if (!e || !e.state) {
+      const word = location.hash.slice(1);
+
+      if (!word) return clearAll();
+
+      const matches = await hasMatches(word);
+      const state = { word, aside: +!matches };
+      history.replaceState(state, '', `#${word}`);
+      return onPopState({state});
+    }
+
+    const {word, aside} = e.state;
+
+    if (!word) return clearAll();
+
+    style.textContent = `a[href="#${word}"] { color: seagreen; }`;
+    searchWord(word, !aside);
+  }
+
+  function clearAll() {
+    clearList();
+    search.value = '';
+    lBox.innerHTML = '';
+    rBox.innerHTML = '';
+  }
 
   function selectListSibling(toNext) {
     if (!selectedListElem) {
@@ -112,7 +159,7 @@
     const dict = await getDict(val[0]);
     if (!dict) return;
 
-    const cb = ([key]) => `<div class="search-list__item">${key}</div>`;
+    const cb = ([k]) => `<a class="search-list__item" href="#${k}">${k}</a>`;
 
     list.innerHTML = getClosestWords(val, dict).map(cb).join('');
     list.scrollTop = 0;
@@ -153,12 +200,33 @@
     clearList();
 
     if (clear) rBox.innerHTML = '';
-    if (word) return lBox.innerHTML = word[1];
+    if (word) return lBox.innerHTML = htmlCache[word[0]] || rehtml(word);
 
     lBox.innerHTML = '';
     rBox.innerHTML = getClosestWords(ignoreCase(low(val)), dict)
-      .map(([key]) => `<a href="#">${key}</a>`)
+      .map(([key]) => `<a href="#${key}">${key}</a>`)
       .join('');
+  }
+
+  async function hasMatches(val) {
+    let k1 = '';
+    const hasVal = val != null;
+
+    if (hasVal) {
+      val = val.trim();
+      k1 = val && ignoreCase(low(val[0]));
+    } else {
+      val = ignoreCase(low(search.value.trim()));
+      k1 = val[0];
+    }
+
+    if (!val || !regDictKey.test(k1) || badDictKeys.includes(k1)) return !1;
+
+    const dict = await getDict(k1);
+    if (!dict) return !1;
+
+    const cb = ([key]) => (hasVal ? key : ignoreCase(low(key))) === val;
+    return dict.some(cb);
   }
 
   async function loadDict(key) {
@@ -169,6 +237,83 @@
 
     mv += 1;
     alert('Error: Cannot load dictionary!');
+  }
+
+  function rehtml(word) {
+    const reg = /[a-zāēīōūȳ]+/gi;
+    const regTag = /<\/?u>/g;
+    const regRomans = /^[IVX][IVX]*$/;
+    const regGens = /^[mfn][mfn, ]*$/;
+    const regGens2 = /\b[mfn]\b/g;
+    const regNums = /^(?:sg|pl)(?=\.)/;
+    const regNums2 = /\b(?:sg|pl)\b/;
+    const regCases = /^(?:nom|gen|dat|acc|abl|loc)(?=\.)/;
+    const regCases2 = /\b(?:nom|gen|dat|acc|abl|loc)\b/;
+    const tplConst = '<var>$&</var>';
+
+    const el = document.createElement('div');
+    el.innerHTML = word[1];
+
+    [...el.childNodes].forEach(function next(el) {
+      if (el.nodeType === 1) return [...el.childNodes].forEach(next);
+      if (el.nodeType !== 3) return;
+
+      const elem = document.createElement('div');
+      elem.innerHTML = el.data.replace(reg, '<u>$&</u>');
+      el.replaceWith(...elem.childNodes);
+    });
+
+    function clearLemma(node) {
+      node.innerHTML = node.innerHTML.replace(regTag, '');
+
+      let n = node;
+
+      while ((n = n.nextSibling)) {
+        if (n.nodeType === 3 && n.data.includes('\n')) break;
+        if (n.nodeType !== 1) continue;
+
+        if (n.tagName !== 'U') {
+          n.innerHTML = n.innerHTML.replace(regTag, '');
+          continue;
+        }
+
+        const e = n.lastChild;
+        n.replaceWith(...n.childNodes);
+        n = e;
+      }
+    }
+
+    const nodes = [...el.childNodes];
+
+    nodes.forEach((node, ind) => {
+      if (node.nodeType !== 1 || node.tagName !== 'B') return;
+      if (ind === 2) return clearLemma(node);
+
+      const elem = node.firstElementChild;
+
+      if (!(elem && elem.tagName === 'U')) return;
+      if (regRomans.test(elem.textContent)) clearLemma(node);
+    });
+
+    nodes.forEach((node) => {
+      if (node.nodeType !== 1 || node.tagName !== 'I') return;
+
+      const text = node.textContent;
+
+      if (regNums.test(text)) {
+        node.innerHTML = node.innerHTML.replace(regNums2, tplConst);
+      }
+
+      if (regCases.test(text)) {
+        node.innerHTML = node.innerHTML.replace(regCases2, tplConst);
+      }
+
+      if (regGens.test(text)) {
+        node.innerHTML = node.innerHTML.replace(regGens2, tplConst);
+      }
+    });
+
+    return htmlCache[word[0]] = el.innerHTML;
   }
 
   function debounce(callback, delay) {
