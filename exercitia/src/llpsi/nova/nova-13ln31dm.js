5f23c85a1233710f319620561895bd86dbe722b9
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-09-24 16:58:02 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-13ln31dm.js
@@ -0,0 +1,158 @@
+$_GET({
+  title: 'Nova exercitia 13',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>dēcl. V</i></span></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-ēs</div> <div class="area">-em</div> <div class="area">-ēī</div> <div class="area">-ēī</div> <div class="area">-ē</div></div>
+      <div><div class="area">-ēs</div> <div class="area">-ēs</div> <div class="area">-ērum</div> <div class="area">-ēbus</div> <div class="area">-ēbus</div></div></div>
+      <div><br></div>
+      <div>1. Di<div class="area" data-key="ēs"></div> est tempus ā māne ad vesperum.</div>
+      <div>2. Iānuārius ūnum et trīgintā di<div class="area" data-key="ēs"></div> habet.</div>
+      <div>3. Initium di<div class="area" data-key="ēī"></div> māne dīcitur.</div>
+      <div>4. Merīdi<div class="area" data-key="ē"></div> sōl altissimus in caelō est.</div>
+      <div>5. Mēnse Iūniō di<div class="area" data-key="ēs"></div> longissimī sunt.</div>
+      <div>6. December di<div class="area" data-key="ēs"></div> breviōrēs habet.</div>
+      <div>7. Di<div class="area" data-key="ēī"></div> prīmō mēnsis nōmen est ‘kalendae’.</div>
+      <div>8. Hieme puerī Germānī super glaci<div class="area" data-key="em"></div> lacuum lūdunt.</div>
+      <div>9. Merīdi<div class="area" data-key="ēs"></div> di<div class="area" data-key="em"></div> in duās aequās partēs dīvidit.</div>
+      <div>10. Vesper fīnis di<div class="area" data-key="ēī"></div> est atque initium noctis.</div>
+      <div>11. Di<div class="area" data-key="ēbus"></div> calidissimīs, quī mēnse Iūliō et mēnse Augustō sunt, Rōmānī urbem relinquunt. Iīs di<div class="area" data-key="ēbus"></div> Rōmānī in vīllīs suīs vīvvunt.</div>
+      <div>12. September trīgintā di<div class="area" data-key="ēs"></div> habet: numerus di<div class="area" data-key="ērum"></div> est trīgintā.</div>
+      <div>13. Iānus duās faci<div class="area" data-key="ēs"></div> habet: eī duae faci<div class="area" data-key="ēs"></div> sunt.</div>
+      <div>14. Prīmīs mēnsium di<div class="area" data-key="ēbus"></div> nōmina sunt: ‘kalendae Iānuāriae’, ‘Februāriae’, ‘Mārtiae’, cēt.</div>
+      <div>15. Puerī Germānī glaci<div class="area" data-key="ē"></div> dēlectantur.</div>
+      <div>16. Post merīdi<div class="area" data-key="em"></div> sōl calidior est quam ante merīdi<div class="area" data-key="em"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>Kalendae Iānuāriae est mēnsis Iānuāriī</i>: [<div class="area">x</div>] diēs prīmī, [<div class="area">v</div>] diēs prīmus, [<div class="area">x</div>] diēs prīmō, [<div class="area">x</div>] diēm prīmum.</span></div>
+      <div><br></div>
+      <div>1. Puerī lūdunt super: [<div class="area" data-key="v"></div>] glaciem frīgidissimam, [<div class="area" data-key="x"></div>] glaciem frīgidissimum, [<div class="area" data-key="x"></div>] glaciē frīgidissimō, [<div class="area" data-key="x"></div>] glaciēī frīgidissimā.</div>
+      <div>2. Quīntus putat: [<div class="area" data-key="x"></div>] diem longus esse, [<div class="area" data-key="x"></div>] diēs longus esse, [<div class="area" data-key="v"></div>] diēs longōs esse, [<div class="area" data-key="x"></div>] diēbus longīs esse.</div>
+      <div>3. Mēnse Decembrī sunt: [<div class="area" data-key="v"></div>] diēs brevissimī, [<div class="area" data-key="x"></div>] diēs brevissimus, [<div class="area" data-key="x"></div>] diēs brevissimōs, [<div class="area" data-key="x"></div>] diēs brevissimās.</div>
+      <div>4. ‘Kalendae’ nōmen est mēnsis: [<div class="area" data-key="x"></div>] diēī prīma, [<div class="area" data-key="x"></div>] diēs prīmī, [<div class="area" data-key="x"></div>] diē prīmō, [<div class="area" data-key="v"></div>] diēī prīmī.</div>
+      <div>5. Rōmānī vīllās petunt: [<div class="area" data-key="x"></div>] diēbus calidibus, [<div class="area" data-key="x"></div>] diēs calidās, [<div class="area" data-key="v"></div>] diēbus calidīs, [<div class="area" data-key="x"></div>] diēī calidō.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>ablātīvus temporis</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Quandō nix dē caelō cadit?</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Hiem<b>e</b>.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Quandō lūna et stēllae lūcent? = N<div class="area" data-key="octe"></div>.</div>
+      <div>2. Quandō Mārtius mēnsis prīmus erat? = A<div class="area" data-key="ntiquō | ntiquīs"></div> t<div class="area" data-key="empore | emporibus"></div>.</div>
+      <div>3. Quandō lūna dīmidia vidētur? = D<div class="area" data-key="iē"></div> sep<div class="area" data-key="timō"></div> vel oct<div class="area" data-key="āvō"></div> post lūnam novam.</div>
+      <div>4. Quandō diēs longissimī sunt? = Ae<div class="area" data-key="stāte"></div>, m<div class="area" data-key="ēnse"></div> Iū<div class="area" data-key="liō"></div>.</div>
+      <div>5. Quandō sōl altissimus in caelō est? = M<div class="area" data-key="erīdiē"></div>.</div>
+      <div>6. Quandō puerī Germānī super glaciem lacuum lūdere possunt? = H<div class="area" data-key="ieme"></div>.</div>
+      <div>7. Quandō campī novā herbā operiuntur? = V<div class="area" data-key="ēre"></div>.</div>
+      <div>8. Quandō multī Rōmānī urbem relinquunt et vīllās suās petunt? = M<div class="area" data-key="ēnsibus"></div> cal<div class="area" data-key="idissimīs"></div>, Iū<div class="area" data-key="liō"></div> et Au<div class="area" data-key="gustō"></div>.</div>
+      <div>9. Quandō diēs atque noctēs aequī sunt? = Ae<div class="area" data-key="quinoctiō"></div>.</div>
+      <div>10. Quandō hominēs dormiunt? = N<div class="area" data-key="octe"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>ablātīvus temporis</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Hieme nix dē caelō cadit.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= <b>Quandō</b> nix dē caelō cadit?</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Aestāte diēs longissimī sunt. = <div class="area" data-key="Quandō"></div> d<div class="area" data-key="iēs"></div> <div class="area" data-key="longissimī"></div> <div class="area" data-key="sunt"></div>?</div>
+      <div>2. Hieme montēs et campī nive operiuntur. = <div class="area" data-key="Quandō"></div> m<div class="area" data-key="ontēs"></div> et c<div class="area" data-key="ampī"></div> <div class="area" data-key="nive"></div> <div class="area" data-key="operiuntur"></div>?</div>
+      <div>3. Vēre arborēs novīs foliīs ōrnantur. = <div class="area" data-key="Quandō"></div> <div class="area" data-key="arborēs"></div> n<div class="area" data-key="ovīs"></div> <div class="area" data-key="foliīs"></div> <div class="area" data-key="ōrnantur"></div>?</div>
+      <div>4. Autumnō folia dē arboribus cadunt. = <div class="area" data-key="Quandō"></div> <div class="area" data-key="folia"></div> dē <div class="area" data-key="arboribus"></div> <div class="area" data-key="cadunt"></div>?</div>
+      <div>5. Diē quīntō decimō post lūnam novam lūna plēna est. = <div class="area" data-key="Quandō"></div> l<div class="area" data-key="ūna"></div> <div class="area" data-key="plēna"></div> <div class="area" data-key="est"></div>?</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quīntum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>accūsātīvus temporis</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Vir fortis mult<b>ōs</b> ann<b>ōs</b> vīvere potest.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Homō ducent<div class="area" data-key="ōs"></div> ann<div class="area" data-key="ōs"></div> vīvere nōn potest.</div>
+      <div>2. Mēnse Iūliō multī Rōmānī urbem relinquunt, neque enim tōt<div class="area" data-key="am"></div> aestāt<div class="area" data-key="em"></div> in urbe habitāre volunt.</div>
+      <div>3. Iūlia quattuor hōr<div class="area" data-key="ās"></div> in hortō lūdit.</div>
+      <div>4. Pāstor long<div class="area" data-key="um"></div> temp<div class="area" data-key="us"></div> in silvā ambulat.</div>
+      <div>5. Puer aeger mult<div class="area" data-key="ās"></div> hōr<div class="area" data-key="ās"></div> in lectō iacet.</div>
+      <div>6. Albīnus tōt<div class="area" data-key="um"></div> di<div class="area" data-key="em"></div> ōrnāmenta vēndit.</div>
+      <div>7. Quīntus tōt<div class="area" data-key="am"></div> noct<div class="area" data-key="em"></div> dormīre nōn potest.</div>
+      <div>8. Mīlitēs Rōmānī mult<div class="area" data-key="ōs"></div> mēns<div class="area" data-key="ēs"></div> in Germāniā mīlitant.</div>
+      <div>9. Pāstor pauc<div class="area" data-key="ās"></div> hōr<div class="area" data-key="ās"></div> sub arbore dormit.</div>
+      <div>10. Hostēs, quī contrā exercitum Rōmānum pugnant, mult<div class="area" data-key="ōs"></div> ann<div class="area" data-key="ōs"></div> vīvere nōn possunt.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium sextum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>superlātīvus</i></span></div>
+      <div><br></div>
+      <div>medicus stultissimus, medicī stultissimī</div>
+      <div>medicum <div class="area" data-key="stultissimum"></div>, <div class="area" data-key="medicōs"></div> <div class="area" data-key="stultissimōs"></div></div>
+      <div>medicī <div class="area" data-key="stultissimī"></div>, medicōrum <div class="area" data-key="stultissimōrum"></div></div>
+      <div>medicō <div class="area" data-key="stultissimō"></div>, <div class="area" data-key="medicīs"></div> <div class="area" data-key="stultissimīs"></div></div>
+      <div><div class="area" data-key="medicō"></div> <div class="area" data-key="stultissimō"></div>, medicīs <div class="area" data-key="stultissimīs"></div></div>
+      <div><br></div>
+      <div>puella trīstissima, <div class="area" data-key="puellae"></div> <div class="area" data-key="trīstissimae"></div></div>
+      <div><div class="area" data-key="puellam"></div> <div class="area" data-key="trīstissimam"></div>, puellās <div class="area" data-key="trīstissimās"></div></div>
+      <div>puellae <div class="area" data-key="trīstissimae"></div>, puellārum <div class="area" data-key="trīstissimārum"></div></div>
+      <div>puellae <div class="area" data-key="trīstissimae"></div>, <div class="area" data-key="puellīs"></div> <div class="area" data-key="trīstissimīs"></div></div>
+      <div><div class="area" data-key="puellā"></div> <div class="area" data-key="trīstissimā"></div>, puellīs <div class="area" data-key="trīstissimīs"></div></div>
+      <div><br></div>
+      <div>flūmen lātissimum, <div class="area" data-key="flūmina"></div> <div class="area" data-key="lātissima"></div></div>
+      <div>flūmen <div class="area" data-key="lātissimum"></div>, flūmina <div class="area" data-key="lātissima"></div></div>
+      <div>flūminis <div class="area" data-key="lātissimī"></div>, <div class="area" data-key="flūminum"></div> <div class="area" data-key="lātissimōrum"></div></div>
+      <div>flūminī <div class="area" data-key="lātissimō"></div>, <div class="area" data-key="flūminibus"></div> <div class="area" data-key="lātissimīs"></div></div>
+      <div><div class="area" data-key="flūmine"></div> <div class="area" data-key="lātissimō"></div>, flūminibus <div class="area" data-key="lātissimīs"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium septimum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>superlātīvus</i></span></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-issimus</div> <div class="area">-issima</div> <div class="area">-issimum</div></div></div>
+      <div><br></div>
+      <div>1. Sōl stēlla clār<div class="area" data-key="issima"></div> est.</div>
+      <div>2. Februārius mēnsis brev<div class="area" data-key="issimus"></div> est.</div>
+      <div>3. Mēdus Lȳdiam fēminam prob<div class="area" data-key="issimam"></div> et fōrmōs<div class="area" data-key="issimam"></div> esse putat.</div>
+      <div>4. Hostēs ‘mīlitēs Rōmānōs fort<div class="area" data-key="issimōs"></div> esse’ dīcunt.</div>
+      <div>5. Nīlus flūmen Āfricae long<div class="area" data-key="issimum"></div> est.</div>
+      <div>6. Iānuārius et Februārius mēnsēs frīgid<div class="area" data-key="issimī"></div> sunt.</div>
+      <div>7. Mēnsī brev<div class="area" data-key="issimō"></div> nōmen est ‘Februārius’.</div>
+      <div>8. Diēs octāvus ante kalendās Iānuāriās diēs annī brev<div class="area" data-key="issimus"></div> est.</div>
+      <div>9. Dux ‘mīlitēs suōs fort<div class="area" data-key="issimōs"></div> esse’ dīcit.</div>
+      <div>10. Iūlia puella crass<div class="area" data-key="issima"></div> nōn est, neque grav<div class="area" data-key="issima"></div>.</div>
+      <div>11. Montibus alt<div class="area" data-key="issimīs"></div> Italia ā Germāniā dīviditur.</div>
+      <div>12. Verba Latīna long<div class="area" data-key="issima"></div> nōn sunt.</div>
+      <div>13. Mēnsibus calid<div class="area" data-key="issimīs"></div> nōmina sunt ‘Iūlius’ et ‘Augustus’.</div>
+      <div>14. Iūlius fīliae trīst<div class="area" data-key="issimae"></div> ōsculum dat.</div>
+      <div>15. Syra nāsum foed<div class="area" data-key="issimum"></div> habet.</div>
+      <div>16. Hiems tempus annī frīgid<div class="area" data-key="issimum"></div> est.</div>
+      <div>17. Aemilius ducī fort<div class="area" data-key="issimō"></div> pāret.</div>
+      <div>18. Aetna mōns Siciliae alt<div class="area" data-key="issimus"></div> est.</div>
+      <div>19. Rhēnō, flūmine lāt<div class="area" data-key="issimō"></div>, Germānia ā Galliā prōvinciā dīviditur.</div>
+      <div>20. Aemilius ducis fort<div class="area" data-key="issimī"></div> mīles est.</div>
+      <div>21. Ante diem octāvum kalendās lānuāriās nox long<div class="area" data-key="issima"></div> est.</div>
+      <div>22. Syra medicum stult<div class="area" data-key="issimum"></div> esse putat.</div>
+      <div>23. Mīlitum Rōmānõrum arma grav<div class="area" data-key="issima"></div> sunt.</div>
+      <div>24. Nūbēs in caelō alt<div class="area" data-key="issimae"></div> sunt.</div>
+      <div>25. Dux Rōmānus mīlitibus fort<div class="area" data-key="issimīs"></div> imperat.</div>
+    </div>
+  `
+});
