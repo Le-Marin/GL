a10f554e7f3fcc60c53b3b14fb6470f2126a122d
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-09-25 18:19:05 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-14dtrl7z.js
@@ -0,0 +1,145 @@
+$_GET({
+  title: 'Nova exercitia 14',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-āns</div> | <div class="area">-antēs</div> <div class="area">-antia</div></div>
+      <div><div class="area">-ēns</div> | <div class="area">-entēs</div> <div class="area">-entia</div></div>
+      <div><div class="area">-ēns</div> | <div class="area">-entēs</div> <div class="area">-entia</div></div>
+      <div><div class="area">-iēns</div> | <div class="area">-ientēs</div> <div class="area">-ientia</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Puer, quī dormit, gallum nōn audit.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Puer dorm<u>iēns</u> gallum nōn audit.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Puella, quae in lectō iacet, dormit. = Puella <div class="area" data-key="in"></div> <div class="area" data-key="lectō"></div> <div class="area" data-key="iacēns"></div> dormit.</div>
+      <div>2. Gallus, quī canit, ā puerō nōn audītur. = Gallus <div class="area" data-key="canēns"></div> ā puerō nōn <div class="area" data-key="audītur"></div>.</div>
+      <div>3. Puerī, quī frīgent, vestīmenta ā servīs poscunt. = Puerī <div class="area" data-key="frīgentēs"></div> vestīmenta ā servīs <div class="area" data-key="poscunt"></div>.</div>
+      <div>4. Ancillae, quae clāmant, dominam vocant. = Ancillae <div class="area" data-key="clāmantēs"></div> dominam <div class="area" data-key="vocant"></div>.</div>
+      <div>5. Quīntus, quī nīdum videt, in arborem ascendere vult. = Quīntus, <div class="area" data-key="nīdum"></div> <div class="area" data-key="vidēns"></div>, in arborem <div class="area" data-key="ascendere"></div> v<div class="area" data-key="ult"></div>.</div>
+      <div>6. Parentēs, quī līberōs exspectant, in ātriō sedent. = Parentēs <div class="area" data-key="līberōs"></div> ex<div class="area" data-key="spectantēs"></div> in ātriō <div class="area" data-key="sedent"></div>.</div>
+      <div>7. Animālia quae volant canem Iūliae nōn dēlectant. = Animālia <div class="area" data-key="volantia"></div> canem Iūliae nōn <div class="area" data-key="dēlectant"></div>.</div>
+      <div>8. Domina, quae ē lectō surgit, ancillam vocat. = Domina, <div class="area" data-key="ē"></div> <div class="area" data-key="lectō"></div> <div class="area" data-key="surgēns"></div>, ancillam <div class="area" data-key="vocat"></div>.</div>
+      <div>9. Pāstor, quī ovis vestīgia reperit, gaudet. = Pāstor <div class="area" data-key="ovis"></div> v<div class="area" data-key="estīgia"></div> <div class="area" data-key="reperiēns"></div> <div class="area" data-key="gaudet"></div>.</div>
+      <div>10. Pāstōrēs, quī dormiunt, ovēs in silvā errantēs nōn vident. = Pāstōrēs <div class="area" data-key="dormientēs"></div> ovēs in silvā <div class="area" data-key="errantēs"></div> nōn <div class="area" data-key="vident"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-ntem</div> | <div class="area">-ntēs</div></div>
+      <div><div class="area">-ns</div> | <div class="area">-ntia</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Mārcus gallum, quī canit, nōn audit.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Mārcus gallum can<u>ent<b>em</b></u> nōn audit.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Servī dominum, quī advenit, nōn vident. = Servī <div class="area" data-key="dominum"></div> <div class="area" data-key="advenientem"></div> nōn <div class="area" data-key="vident"></div>.</div>
+      <div>2. Medicus puerī pedem, quī dolet, tangit. = Medicus puerī <div class="area" data-key="pedem"></div> <div class="area" data-key="dolentem"></div> t<div class="area" data-key="angit"></div>.</div>
+      <div>3. Pāstor ovem nigram, quae in silvam intrat, nōn videt. = Pāstor ovem nigram <div class="area" data-key="in"></div> <div class="area" data-key="silvam"></div> <div class="area" data-key="intrantem"></div> nōn <div class="area" data-key="videt"></div>.</div>
+      <div>4. Medicus stultus puerōs quī aegrōtant sānāre nōn potest. = Medicus <div class="area" data-key="stultus"></div> p<div class="area" data-key="uerōs"></div> <div class="area" data-key="aegrōtantēs"></div> s<div class="area" data-key="ānāre"></div> nōn <div class="area" data-key="potest"></div>.</div>
+      <div>5. Aemilia ōstiārium, quī ōstium aperit, audit. = Aemilia ōstiārium ō<div class="area" data-key="stium"></div> <div class="area" data-key="aperientem"></div> <div class="area" data-key="audit"></div>.</div>
+      <div>6. Ancillae stēllās, quae in caelō lūcent, aspiciunt. = Ancillae stēllās <div class="area" data-key="in"></div> <div class="area" data-key="caelō"></div> <div class="area" data-key="lūcentēs"></div> a<div class="area" data-key="spiciunt"></div>.</div>
+      <div>7. Servī puerōs, quī dormiunt, excitant. = Servī <div class="area" data-key="puerōs"></div> <div class="area" data-key="dormientēs"></div> ex<div class="area" data-key="citant"></div>.</div>
+      <div>8. Medicus cor Quīntī, quod palpitat, sentit. = Medicus <div class="area" data-key="cor"></div> Quīntī <div class="area" data-key="palpitāns"></div> <div class="area" data-key="sentit"></div>.</div>
+      <div>9. Iūlia avēs, quae inter rāmōs sē occultant, videt. = Iūlia avēs <div class="area" data-key="inter"></div> <div class="area" data-key="rāmōs"></div> sē <div class="area" data-key="occultantēs"></div> <div class="area" data-key="videt"></div>.</div>
+      <div>10. Aemilia puellam, quae plōrat et clāmat, audit. = Aemilia puellam <div class="area" data-key="plōrantem"></div> <div class="area" data-key="et"></div> <div class="area" data-key="clāmantem"></div> au<div class="area" data-key="dit"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-ntis</div> | <div class="area">-ntium</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Mārcus vōcem gallī, quī canit, nōn audit.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Mārcus vōcem gallī can<u>ent<b>is</b></u> nōn audit.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Medicus faciem puerī, quī aegrōtat, aspicit. = Medicus faciem <div class="area" data-key="puerī"></div> <div class="area" data-key="aegrōtantis"></div> <div class="area" data-key="aspicit"></div>.</div>
+      <div>2. Medicus vōcem līberōrum, quī clāmant, audit. = Medicus vōcem <div class="area" data-key="līberōrum"></div> <div class="area" data-key="clāmantium"></div> <div class="area" data-key="audit"></div>.</div>
+      <div>3. Mercurius imperia deōrum, quī in caelō vīvunt, ad hominēs portāre solet. = Mercurius imperia deōrum <div class="area" data-key="in"></div> <div class="area" data-key="caelō"></div> <div class="area" data-key="vīventium"></div> ad hominēs <div class="area" data-key="portāre"></div> <div class="area" data-key="solet"></div>.</div>
+      <div>4. Mīlitēs vōcem ducis, quī pugnat, nōn audiunt. = Mīlitēs vōcem <div class="area" data-key="ducis"></div> <div class="area" data-key="pugnantis"></div> nōn <div class="area" data-key="audiunt"></div>.</div>
+      <div>5. Dux mīlitum quī castra dēfendunt fortissimus est. = Dux mīlitum <div class="area" data-key="castra"></div> <div class="area" data-key="dēfendentium"></div> fortissimus <div class="area" data-key="est"></div>.</div>
+      <div>6. Aemilia sanguinem dē bracchiō Quīntī, quī in lectō iacet, dēterget. = Aemilia <div class="area" data-key="sanguinem"></div> dē bracchiō <div class="area" data-key="Quīntī"></div> in <div class="area" data-key="lectō"></div> <div class="area" data-key="iacentis"></div> <div class="area" data-key="dēterget"></div>.</div>
+      <div>7. Domina vōcem ancillārum, quae lūdent et rīdent, audit. = Domina vōcem <div class="area" data-key="ancillārum"></div> <div class="area" data-key="lūdentium"></div> <div class="area" data-key="et"></div> <div class="area" data-key="rīdentium"></div> au<div class="area" data-key="dit"></div>.</div>
+      <div>8. Iūlius lacrimās fīliae, quae in cubiculō plōrat, nōn videt. = Iūlius lacrimās <div class="area" data-key="fīliae"></div> <div class="area" data-key="in"></div> <div class="area" data-key="cubiculō"></div> <div class="area" data-key="plōrantis"></div> nōn <div class="area" data-key="videt"></div>.</div>
+      <div>9. Lȳdia vōcem amīcī, quī advenit, audit et laeta est. = Lȳdia vōcem <div class="area" data-key="amīcī"></div> <div class="area" data-key="advenientis"></div> <div class="area" data-key="audit"></div> et laeta <div class="area" data-key="est"></div>.</div>
+      <div>10. Mārcus vōcem Iūliae, quae canit et rīdet, audit et īrātus est. = Mārcus vōcem <div class="area" data-key="Iūliae"></div> <div class="area" data-key="canentis"></div> <div class="area" data-key="et"></div> <div class="area" data-key="rīdentis"></div> au<div class="area" data-key="dit"></div> et <div class="area" data-key="īrātus"></div> <div class="area" data-key="est"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-ntī</div> | <div class="area">-ntibus</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Mīlitēs ducī, quī imperat, pārent.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Mīlitēs ducī imper<u>ant<b>ī</b></u> pārent.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Dāvus puerō, quī ē lectō surgit, aquam dat. = Dāvus puerō <div class="area" data-key="ē"></div> <div class="area" data-key="lectō"></div> <div class="area" data-key="surgentī"></div> aquam <div class="area" data-key="dat"></div>.</div>
+      <div>2. Mēdus Lȳdiae, quae gaudet atque rīdet, ānulum dat. = Mēdus Lȳdiae <div class="area" data-key="gaudentī"></div> <div class="area" data-key="atque"></div> <div class="area" data-key="rīdentī"></div> ānulum <div class="area" data-key="dat"></div>.</div>
+      <div>3. Albīnus Mēdō, quī eī pecūniam dat, ānulum vēndit. = Albīnus Mēdō <div class="area" data-key="eī"></div> p<div class="area" data-key="ecūniam"></div> <div class="area" data-key="dantī"></div> ānulum <div class="area" data-key="vēndit"></div>.</div>
+      <div>4. Dux mīlitibus, quī contrā hostēs pugnant, imperat. = Dux mīlitibus <div class="area" data-key="contrā"></div> <div class="area" data-key="hostēs"></div> <div class="area" data-key="pugnantibus"></div> i<div class="area" data-key="mperat"></div>.</div>
+      <div>5. Dāvus puerīs, quī in cubiculīs dormiunt, aquam affert. = Dāvus puerīs <div class="area" data-key="in"></div> <div class="area" data-key="cubiculīs"></div> <div class="area" data-key="dormientibus"></div> aquam <div class="area" data-key="affert"></div>.</div>
+      <div>6. Hominibus, quī natant, spīrāre necesse est. = <div class="area" data-key="Hominibus"></div> <div class="area" data-key="natantibus"></div> spīrāre <div class="area" data-key="necesse"></div> <div class="area" data-key="est"></div>.</div>
+      <div>7. Iūlius fīliae, quae rosās carpit, mālum pirumque affert. = Iūlius <div class="area" data-key="fīliae"></div> <div class="area" data-key="rosās"></div> <div class="area" data-key="carpentī"></div> mālum pirumque <div class="area" data-key="affert"></div>.</div>
+      <div>8. Iūlius servō, quī alium servum nōn accūsat, nummum dat. = Iūlius servō <div class="area" data-key="alium"></div> <div class="area" data-key="servum"></div> nōn <div class="area" data-key="accūsantī"></div> nummum <div class="area" data-key="dat"></div>.</div>
+      <div>9. Ancillīs dominae quae imperat necesse est pārēre. = Ancill<div class="area" data-key="īs"></div> <div class="area" data-key="dominae"></div> <div class="area" data-key="imperantī"></div> necesse <div class="area" data-key="est"></div> <div class="area" data-key="pārēre"></div>.</div>
+      <div>10. Aemilia Quīntō, quī in lectō iacet, aquam dat. = Aemilia Quīntō <div class="area" data-key="in"></div> <div class="area" data-key="lectō"></div> <div class="area" data-key="iacentī"></div> aquam <div class="area" data-key="dat"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quīntum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-nte</div> | <div class="area">-ntibus</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Gallus ā puerō, quī dormit, nōn audītur.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Gallus ā puerō dorm<u>ient<b>e</b></u> nōn audītur.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Mārcus ā servō, quī clāmat, excitātur. = Mārcus <div class="area" data-key="ā"></div> <div class="area" data-key="servō"></div> <div class="area" data-key="clāmante"></div> ex<div class="area" data-key="citātur"></div>.</div>
+      <div>2. Domina ab ancillīs, quae intrant, salūtātur. = Domina <div class="area" data-key="ab"></div> <div class="area" data-key="ancillīs"></div> <div class="area" data-key="intrantibus"></div> s<div class="area" data-key="alūtātur"></div>.</div>
+      <div>3. Ovis nigra ā pāstōre, quī in umbrā dormit, abit. = Ovis nigra ā <div class="area" data-key="pāstōre"></div> <div class="area" data-key="in"></div> <div class="area" data-key="umbrā"></div> <div class="area" data-key="dormiente"></div> ab<div class="area" data-key="it"></div>.</div>
+      <div>4. Mārcus ā sorōre, quae plōrat, discēdit. = Mārcus ā <div class="area" data-key="sorōre"></div> <div class="area" data-key="plōrante"></div> <div class="area" data-key="discēdit"></div>.</div>
+      <div>5. Medicus ā puerīs, quī aegrōtant, nōn salūtātur. = Medicus <div class="area" data-key="ā"></div> <div class="area" data-key="puerīs"></div> <div class="area" data-key="aegrōtantibus"></div> nōn <div class="area" data-key="salūtātur"></div>.</div>
+      <div>6. Canis Iūliae animālibus quae volant nōn dēlectātur. = Canis Iūliae <div class="area" data-key="animālibus"></div> <div class="area" data-key="volantibus"></div> nōn <div class="area" data-key="dēlectātur"></div>.</div>
+      <div>7. Dominus ā servīs, quī tacent, audītur. = Dominus <div class="area" data-key="ā"></div> <div class="area" data-key="servīs"></div> <div class="area" data-key="tacentibus"></div> <div class="area" data-key="audītur"></div>.</div>
+      <div>8. Aemilia ā fīliō, quī in lectō iacet, nōn discēdit. = Aemilia ā <div class="area" data-key="fīliō"></div> <div class="area" data-key="in"></div> <div class="area" data-key="lectō"></div> <div class="area" data-key="iacente"></div> nōn dis<div class="area" data-key="cēdit"></div>.</div>
+      <div>9. Pāstor ab ovibus, quae aquam bibunt, nōn abit. = Pāstor ab <div class="area" data-key="ovibus"></div> <div class="area" data-key="aquam"></div> <div class="area" data-key="bibentibus"></div> nōn <div class="area" data-key="abit"></div>.</div>
+      <div>10. Aemilia ab ōstiāriō, quī ōstium claudit, aspicitur. = Aemilia ab ōstiāriō <div class="area" data-key="ōstium"></div> <div class="area" data-key="claudente"></div> <div class="area" data-key="aspicitur"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium sextum</b></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">-āns -antis</div> | <div class="area">-ēns -entis</div> | <div class="area">-ēns -entis (-iēns -ientis)</div> | <div class="area">-iēns -ientis</div></div></div>
+      <div><br></div>
+      <div>1. Puer dorm<div class="area" data-key="iēns"></div> gallum can<div class="area" data-key="entem"></div> nōn audit, sed puerī vigil<div class="area" data-key="antēs"></div> gallōs can<div class="area" data-key="entēs"></div> audiunt.</div>
+      <div>2. Pāstor canī cibum dat; canis autem pāstōrī cibum d<div class="area" data-key="antī"></div> pāret.</div>
+      <div>3. Dāvus puerō posc<div class="area" data-key="entī"></div> vestīmenta affert. Servī puerīs ē lectō surg<div class="area" data-key="entibus"></div> aquam afferunt.</div>
+      <div>4. Quīntus bracchium dol<div class="area" data-key="ēns"></div> aspicit et putat medicum, puerōs aegrōs nōn sān<div class="area" data-key="antem"></div>, stultum esse.</div>
+      <div>5. Ovis nigra, ā pāstōre dorm<div class="area" data-key="iente"></div> discēd<div class="area" data-key="ens"></div>, in silvam intrat.</div>
+      <div>6. Canis, ovis bāl<div class="area" data-key="antis"></div> vōcem aud<div class="area" data-key="iēns"></div>, lupum dentibus petit.</div>
+      <div>7. Mīlitēs, pār<div class="area" data-key="entēs"></div> ducī imper<div class="area" data-key="antī"></div>, in hostēs impetum faciunt.</div>
+      <div>8. Domina ancillās clām<div class="area" data-key="antēs"></div> audit et in ātrium intrat, ubi Quīntum videt aquam puls<div class="area" data-key="antem"></div>.</div>
+      <div>9. Canis avēs, animālia vol<div class="area" data-key="antia"></div>, aspicit et lātrat.</div>
+      <div>10. Parentēs ā līberīs in ātrium intr<div class="area" data-key="antibus"></div> salūtantur.</div>
+      <div>11. Servī clām<div class="area" data-key="antēs"></div> ā dominō dorm<div class="area" data-key="iente"></div> nōn audiuntur.</div>
+      <div>12. Māter vōcem fīliae rīd<div class="area" data-key="entis"></div> et cum cane in hortō lūd<div class="area" data-key="entis"></div> audit.</div>
+      <div>13. Mīlitēs inter pīla vol<div class="area" data-key="antia"></div> pugnant.</div>
+      <div>14. Mārcus frātrem dē arbore cad<div class="area" data-key="entem"></div> videt, patremque magnā vōce vocat.</div>
+      <div>15. Pāstor, ovem nigram quaer<div class="area" data-key="ēns"></div>, silvam petit.</div>
+      <div>16. Pāstōrēs dorm<div class="area" data-key="ientēs"></div> ā canibus lātr<div class="area" data-key="antibus"></div> excitantur.</div>
+      <div>17. Puer servō vestīmenta affer<div class="area" data-key="entī"></div> pāret.</div>
+      <div>18. Clāmor hostium castra oppugn<div class="area" data-key="antibus"></div> ā mīlitibus audītur.</div>
+      <div>19. Dux ā mīlitibus, in castrīs habit<div class="area" data-key="antibus"></div>, salūtātur.</div>
+      <div>20. Medicus puerī in lectō iac<div class="area" data-key="entis"></div> cor palpit<div class="area" data-key="āns"></div> manū sentit.</div>
+      <div>21. Servī puerīs frīg<div class="area" data-key="entibus"></div> vestīmenta afferunt.</div>
+      <div>22. Iūlia patrī adven<div class="area" data-key="ientī"></div> rosās dat.</div>
+      <div>23. Domina ancillās rīd<div class="area" data-key="entēs"></div> et can<div class="area" data-key="entēs"></div> audit.</div>
+      <div>24. Canis, vestīgia aspic<div class="area" data-key="iēns"></div>, ovem in silvā err<div class="area" data-key="antem"></div> quaerit.</div>
+      <div>25. Quīntus, in lectō vigil<div class="area" data-key="āns"></div>, membra dol<div class="area" data-key="entia"></div> movēre nōn potest.</div>
+    </div>
+  `
+});
