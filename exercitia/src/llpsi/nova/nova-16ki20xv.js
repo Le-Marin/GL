e7a94fef0cd5e5327f24df4013cb3215832d8710
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-10-31 22:39:27 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-16ki20xv.js
@@ -0,0 +1,134 @@
+$_GET({
+  title: 'Nova exercitia 16',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>verba dēpōnentia</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>verē<b>rī</b> = timēre</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>verē<b>tur</b> = timet, verentur = timent</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>verē<b>ns</b> = timēns</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Sōl māne or<div class="area" data-key="itur"></div>. Mēdus et Lȳdia sōlem or<div class="area" data-key="ientem"></div> intu<div class="area" data-key="entur"></div>.</div>
+      <div>2. Dum nautae loqu<div class="area" data-key="untur"></div>, gubernātor ventum secundum opper<div class="area" data-key="ītur"></div>.</div>
+      <div>3. Mēdus laet<div class="area" data-key="ātur"></div>, quod in Graeciam, patriam suam, proficīsc<div class="area" data-key="itur"></div>.</div>
+      <div>4. Lȳdia autem laet<div class="area" data-key="ārī"></div> nōn potest, quod multās amīcās Rōmae relinquit.</div>
+      <div>5. Dum nāvis ē portū ēgred<div class="area" data-key="itur"></div>, dē oculīs Lȳdiae lacrimae lāb<div class="area" data-key="untur"></div>.</div>
+      <div>6. Mēdus Lȳdiam complect<div class="area" data-key="itur"></div> et eam cōnsōl<div class="area" data-key="ārī"></div> cōn<div class="area" data-key="ātur"></div>.</div>
+      <div>7. Mēdus, quī in nāve est et in Graeciam proficīsc<div class="area" data-key="ī"></div> vult, iam dominum nōn ver<div class="area" data-key="ētur"></div>.</div>
+      <div>8. Nūllus amīcus Mēdum proficīsc<div class="area" data-key="entem"></div> sequ<div class="area" data-key="itur"></div>.</div>
+      <div>9. Multī hominēs nāvem ē portū ēgredientem et nautās proficīsc<div class="area" data-key="entēs"></div> salūtant.</div>
+      <div>10. Discipulī dormientēs magistrum loqu<div class="area" data-key="entem"></div> audīre nōn possunt.</div>
+      <div>11. Syra Iūliam cōnsōl<div class="area" data-key="ātur"></div>, quae trīstis in speculō sē intu<div class="area" data-key="ētur"></div>.</div>
+      <div>12. Pāstor, eius vestīgia intu<div class="area" data-key="ēns"></div>, ovem in silvā sequ<div class="area" data-key="itur"></div>.</div>
+      <div>13. Iūlius cum Mēdō loqu<div class="area" data-key="ī"></div> vult, sed Mēdus ē vīllā ēgred<div class="area" data-key="itur"></div>.</div>
+      <div>14. Iūlius et servī Rōmam proficīsc<div class="area" data-key="untur"></div>; Aemilia autem et ancillae eōs in vīllā opper<div class="area" data-key="iuntur"></div>.</div>
+      <div>15. Quīntus in arborem ascendere cōn<div class="area" data-key="ātur"></div>, sed dē eā lāb<div class="area" data-key="itur"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">complectī</div> <div class="area">ēgredī</div> <div class="area">intuērī</div> <div class="area">lābī</div> <div class="area">laetārī</div> <div class="area">loquī</div> <div class="area">opperīrī</div> <div class="area">orīrī</div> <div class="area">proficīscī</div> <div class="area">sequī</div> <div class="area">verērī</div></div></div>
+      <div><br></div>
+      <div>Mēdus, quī dominum <div class="area" data-key="verētur">timet</div>, ē vīllā <div class="area" data-key="ēgreditur">exit</div> dum sōl <div class="area" data-key="oritur">in caelum ascendit</div> et Iūlius in cubiculō suō dormīt. Mēdus enim pecūniam dominī habet et ē vīllā fugere vult.</div>
+      <div><br></div>
+      <div>Quō <div class="area" data-key="proficīscitur">abit</div> Mēdus? Rōmam it, ubi amīca eum <div class="area" data-key="opperītur">exspectat</div>. Mēdus sē vertit et vīllam <div class="area" data-key="intuētur">aspicit</div>: vir eum <div class="area" data-key="sequitur">post eum venit</div>, ergō Mēdus currere incipit. In viā currēns <div class="area" data-key="lābitur">cadit</div>, sed statim surgit. Sēcum <div class="area" data-key="loquitur">verba facit</div>: “Quis est ille vir, quī post mē ambulat?”</div>
+      <div><br></div>
+      <div>Dum haec dīcit, Mēdus post arborem sē occultat, et caelum ātrum fit. Vir caelum spectat et ad vīllam redit; Mēdus autem <div class="area" data-key="laetātur">gaudet</div>. Paulō post Mēdus Rōmam advenit, ubi Lȳdia, amīca eius, multa ōscula eī dat eumque <div class="area" data-key="complectitur">bracchia circum eius corpus pōnit</div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>ablātīvus absolūtus</i></span></div>
+      <div><span class="ff"><i>(cum adiectīvō)</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span><u>Dum</u> ventus secundus est, nāvēs ē portū ēgrediuntur.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Vent<b>ō</b> secund<b>ō</b> nāvēs ē portū ēgrediuntur.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span><u>Quia</u> ōstium clausum est, Iūlius intrāre nōn potest.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Ōsti<b>ō</b> claus<b>ō</b>, Iūlius intrāre nōn potest.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Dum mare tranquillum est, nautae laetī nāvigant. = <div class="area" data-key="Mare"></div> <div class="area" data-key="tranquillō"></div>, n<div class="area" data-key="autae"></div> <div class="area" data-key="laetī"></div> n<div class="area" data-key="āvigant"></div>.</div>
+      <div>2. Quia fenestra aperta est, āēr in cubiculum intrat. = <div class="area" data-key="Fenestrā"></div> <div class="area" data-key="apertā"></div>, ā<div class="area" data-key="ēr"></div> in <div class="area" data-key="cubiculum"></div> <div class="area" data-key="intrat"></div>.</div>
+      <div>3. Dum caelum serēnum est, nāvis ē portū ēgreditur. = <div class="area" data-key="Caelō"></div> <div class="area" data-key="serēnō"></div>, <div class="area" data-key="nāvis"></div> ē <div class="area" data-key="portū"></div> <div class="area" data-key="ēgreditur"></div>.</div>
+      <div>4. Dum vīvī sunt hostēs, mīlitēs pugnāre nōn dēsinunt. = V<div class="area" data-key="īvīs"></div> <div class="area" data-key="hostibus"></div>, <div class="area" data-key="mīlitēs"></div> <div class="area" data-key="pugnāre"></div> nōn <div class="area" data-key="dēsinunt"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>ablātīvus absolūtus</i></span></div>
+      <div><span class="ff"><i>(cum participiō: -nte, -ntibus)</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span><u>Dum</u> / <u>Quia</u> gallus canit, Mārcus excitātur.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Gall<b>ō</b> canent<b>e</b>, Mārcus excitātur.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Quia Mārcus clāmat, pater accurrit. = <div class="area" data-key="Mārcō"></div> <div class="area" data-key="clāmante"></div>, pater accurrit.</div>
+      <div>2. Quia līberī in hortō lūdunt, Iūlius dormīre nōn potest. = <div class="area" data-key="Līberīs"></div> in hortō <div class="area" data-key="lūdentibus"></div>, Iūlius dormīre nōn potest.</div>
+      <div>3. Dum Iūlia rosās carpit, Mārcus Quīntusque rīdent. = <div class="area" data-key="Iūliā"></div> r<div class="area" data-key="osās"></div> <div class="area" data-key="carpente"></div>, Mārcus Quīntusque rīdent.</div>
+      <div>4. Dum medicus vēnam aperit, puer oculōs claudit. = <div class="area" data-key="Medicō"></div> <div class="area" data-key="vēnam"></div> <div class="area" data-key="aperiente"></div>, puer oculōs claudit.</div>
+      <div>5. Quia canis lātrat, lupus montēs petit. = <div class="area" data-key="Cane"></div> <div class="area" data-key="lātrante"></div>, lupus montēs petit.</div>
+      <div>6. Quia tabernāriī clāmant, multī hominēs apud tabernās cōnsistunt. = <div class="area" data-key="Tabernāriīs"></div> <div class="area" data-key="clāmantibus"></div>, multī hominēs apud tabernās cōnsistunt.</div>
+      <div>7. Dum hostēs impetum faciunt, mīlitēs arma sūmunt. = <div class="area" data-key="Hostibus"></div> <div class="area" data-key="impetum"></div> <div class="area" data-key="facientibus"></div>, mīlitēs arma sūmunt.</div>
+      <div>8. Dum sōl oritur, gallus canit. = <div class="area" data-key="Sōle"></div> <div class="area" data-key="oriente"></div> gallus canit.</div>
+      <div>9. Dum ovēs in umbrā iacent, pāstor in silvam intrat. = <div class="area" data-key="Ovibus"></div> <div class="area" data-key="in"></div> <div class="area" data-key="umbrā"></div> <div class="area" data-key="iacentibus"></div>, pāstor in silvam intrat.</div>
+      <div>10. Dum magister recitat, discipulī dormiunt. = <div class="area" data-key="Magistrō"></div> <div class="area" data-key="recitante"></div>, discipulī dormiunt.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quīntum</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span><u>Dum Sextus magistrum salūtat</u>, Mārcus ad lūdum advenit.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Sext<b>ō</b> magistrum salūtant<b>e</b>, Mārcus ad lūdum advenit.</span></div>
+      </div>
+      <div><br></div>
+      <div><div class="area" data-key="Sōle oriente">Dum sōl oritur</div> et <div class="area" data-key="Quīntō in lectō cubante">Quīntus in lectō cubat</div>, Mārcus sōlus ad lūdum it. Rēs suās sēcum ferēns, <div class="area" data-key="cane lātrante">dum canis lātrat</div>, ē vīllā exit. Mārcus sē puerum fortem esse putat, neque canēs timet.</div>
+      <div><br></div>
+      <div><div class="area" data-key="Mārcō in viā ambulante">Dum Mārcus in viā ambulat</div>, Sextus quoque et Titus lūdum petunt. Paulō post Mārcum ambulantem vident, et post arborēs sē occultant. <div class="area" data-key="Sextō Titōque sē occultantibus">Dum Sextus Titusque sē occultant</div>, Mārcus pāstōrēs procul in campō cum ovibus intuētur. Ovēs nunc perterritae currunt, <div class="area" data-key="lupīs in silvā ululantibus">quia lupī in silvā ululant</div>. Mārcus vērō rīdet: nam lupōs nōn timet.</div>
+      <div><br></div>
+      <div><div class="area" data-key="Puerō sē vertente">Dum puer sē vertit</div>, lupī vōx audītur. Vōx nōn procul, sed prope est. Mārcus quoque nunc timet, ut ovēs. <div class="area" data-key="Puerō circum sē spectante">Dum puer circum sē spectat</div>, Sextus et Titus folia quae post arborēs in terrā sunt movent. Mārcus horret, <div class="area" data-key="lupō adveniente">quia lupus audvenit</div>. Quid igitur facit Mārcus? In arborem ascendit, <div class="area" data-key="lupō prope ululante">dum lupus prope ululat</div>.</div>
+      <div><br></div>
+      <div><div class="area" data-key="Mārcō perterritō in arborem ascendente">Dum Mārcus perterritus in arborem ascendit</div>, Sextus Titusque multa folia movent atque magnā vōce ululant. Iam Mārcus multum timet et, dum arborem ascendere cōnātur, ad terram cadit. Tum oculōs claudit et clāmat, <div class="area" data-key="amīcīs rīdentibus sēque ostendentibus">dum amīcī rīdent sēque ostendunt</div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium sextum</b></div>
+      <div><br></div>
+      <div><span class="ff">multum, paulum, nihil <i>cum gen.</i></span></div>
+      <div><br></div>
+      <div>1. Quī multum pecūni<div class="area" data-key="ae"></div> habet pecūniōsus est.</div>
+      <div>2. In parvō rīvō paulum aqu<div class="area" data-key="ae"></div> est.</div>
+      <div>3. Quīntus nōn multum sanguin<div class="area" data-key="is"></div> habet.</div>
+      <div>4. Ovēs multum herb<div class="area" data-key="ae"></div> ēsse solent.</div>
+      <div>5. Pāstor paulum pecūni<div class="area" data-key="ae"></div>, sed multum pān<div class="area" data-key="is"></div> sēcum fert.</div>
+      <div>6. Nautae multum aqu<div class="area" data-key="ae"></div> ē nāve hauriunt.</div>
+      <div>7. Lȳdia nihil pecūni<div class="area" data-key="ae"></div> habet.</div>
+      <div>8. Mēdus multum pecūni<div class="area" data-key="ae"></div>, sed nihil cib<div class="area" data-key="ī"></div> habet.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium septimum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">multum</div> <div class="area">paulum</div></div>
+      <div><div class="area">multō</div> <div class="area">paulō</div></div></div>
+      <div><br></div>
+      <div>1. Pāstor ovem nigram quaerit; paulum<div class="area" data-key="ō"></div> post eam in silvā reperit.</div>
+      <div>2. Paul<div class="area" data-key="ō"></div> ante tempestātem, nāvis ē portū ēgredītur.</div>
+      <div>3. Iūlia nōn mult<div class="area" data-key="um"></div> ēsse solet, ergō Quīntus paul<div class="area" data-key="ō"></div> crassior est quam Iūlia.</div>
+      <div>4. Quīntus, quī mult<div class="area" data-key="um"></div> ēsse solet, hodiē nihil ēst, quod aegrōtat.</div>
+      <div>5. Puerī sānī mult<div class="area" data-key="ō"></div> fortiōrēs sunt quam puerī aegrī.</div>
+      <div>6. Corpus Diodōrī mult<div class="area" data-key="ō"></div> crassius est quam corpus Mārcī.</div>
+      <div>7. Pāstor paul<div class="area" data-key="um"></div> in umbrā dormit.</div>
+      <div>8. Iūlius mult<div class="area" data-key="ō"></div> pecūniōsior est quam Aemilius.</div>
+      <div>9. Arma Germānōrum paul<div class="area" data-key="ō"></div> graviōra sunt quam Rōmānōrum.</div>
+      <div>10. Mēdus pecūniam Iūliī sūmit et, paul<div class="area" data-key="ō"></div> post, ē vīllā discēdit.</div>
+    </div>
+  `
+});
