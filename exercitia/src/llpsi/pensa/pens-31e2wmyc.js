370b917ce2aadd462aabfcee1bf69e10aa8a6315
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-11-22 03:00:01 +0300

    Add files via upload

--- exercitia/src/llpsi/pensa/pens-31e2wmyc.js
+++ exercitia/src/llpsi/pensa/pens-31e2wmyc.js
@@ -2,20 +2,17 @@ $_GET({
   title: 'Pensa 31',
   html: /*html*/`
     <div class="row">
-      <div><b>PENSVM A</b></div>
-      <div><br></div>
       <div><span style="white-space: pre;">	</span>In hīs exemplīs syllabae quae dēsunt add<div class="area" data-key="endae"></div> sunt:</div>
       <div><span style="white-space: pre;">	</span>Mercēs ad diem solv<div class="area" data-key="enda"></div> est. Quī fūrtum fēcit pūn<div class="area" data-key="iendus"></div> est. Quidquid dux imperāvit mīlitibus faci<div class="area" data-key="endum"></div> est. Quid magis opt<div class="area" data-key="andum"></div> est quam vīta beāta? Ē malīs minimum ēlig<div class="area" data-key="endum"></div> est. In perīculīs dēspēr<div class="area" data-key="andum"></div> nōn est. Pater dīcit ‘fīlium pūn<div class="area" data-key="iendum"></div> esse, nōn laud<div class="area" data-key="andum"></div>’.</div>
       <div><span style="white-space: pre;">	</span>Verba: tangere <div class="area" data-key="tetig"></div>isse <div class="area" data-key="tāct"></div>um; accipere <div class="area" data-key="accēp"></div>isse <div class="area" data-key="accept"></div>um; auferre <div class="area" data-key="abstul"></div>isse <div class="area" data-key="ablāt"></div>um; statuere <div class="area" data-key="statu"></div>isse <div class="area" data-key="statūt"></div>um; intellegere <div class="area" data-key="intellēx"></div>isse <div class="area" data-key="intellēct"></div>um; bibere <div class="area" data-key="bib"></div>isse; audēre, <div class="area" data-key="aus"></div>um esse.</div>
       <div><span style="white-space: pre;">	</span>In exemplīs quae sequuntur vocābula add<div class="area" data-key="enda"></div> sunt.</div>
-      <div><br></div>
-      <div><b>PENSVM B</b></div>
-      <div><br></div>
+    </div>
+    <div class="row">
       <div><span style="white-space: pre;">	</span>Herī Mēdus ā dominō <div class="area" data-key="aufūgit"></div> aliquantum pecūniae sēcum <div class="area" data-key="auferēns"></div>. Mēdus dominum suum nōn amat, sed <div class="area" data-key="ōdit"></div>. Iūlius, quī eum Rōmae <div class="area" data-key="latēre"></div> putat, magnum praemium dabit eī quī eum invēnerit <div class="area" data-key="priusquam"></div> Italiam relīquerit. Iūlius dīcit ‘mulierēs nimis <div class="area" data-key="clēmentēs"></div> esse ac facile virīs nēquissimīs <div class="area" data-key="ignōscere"></div>’. <div class="area" data-key="quidquid"></div> dominus imperāvit servō faciendum est. Solō, vir <div class="area" data-key="sapiēns"></div>, Athēniēnsibus <div class="area" data-key="lēgēs"></div> optimās scrīpsit. Patrem suum necāre <div class="area" data-key="scelus"></div> inhūmānum est. Iūlius nōndum <div class="area" data-key="senex"></div> est ut Nestor, sed adhūc <div class="area" data-key="iuvenis"></div> ut Paris ille quī Helenam ā marītō <div class="area" data-key="abdūxit"></div>. “<div class="area" data-key="quisquis"></div> amat valeat!” cantat Orontēs, quī <div class="area" data-key="ēbrius"></div> est quod <div class="area" data-key="nimium"></div> vīnī pōtāvit.</div>
-      <div><br></div>
-      <div><b>PENSVM C</b></div>
-      <div><br></div>
+    </div>
+    <div class="row">
       <div>Quis fuit Midās?</div>
+      
       <div>Quamobrem Midās fame et sitī cruciābātur?</div>
       <div>Cūr Iūlius illam fābulam audīre nōn vult?</div>
       <div>Ubi Cornēlius servum Iūliī vīdit?</div>
