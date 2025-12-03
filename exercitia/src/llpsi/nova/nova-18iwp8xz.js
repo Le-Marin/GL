272233b13d54b533bacb8ea82133f3d4f8f45eb7
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2025-12-03 18:45:31 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-18iwp8xz.js
@@ -0,0 +1,185 @@
+$_GET({
+  title: 'Nova exercitia 18',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-er</div> <div class="area">-rior</div> <div class="area">-errimus</div></div></div>
+      <div><br></div>
+      <div>1. Mārcus pig<div class="area" data-key="er"></div>, Sextus vērō impig<div class="area" data-key="er"></div> est.</div>
+      <div>2. Mārcus pig<div class="area" data-key="rior"></div> est quam Sextus et Titus. Mārcus discipulus pig<div class="area" data-key="errimus"></div> est.</div>
+      <div>3. Cornēliī equus pulch<div class="area" data-key="er"></div> est.</div>
+      <div>4. Cornēliī equus pulch<div class="area" data-key="rior"></div> est quam Iūliī.</div>
+      <div>5. Cornēlius equum pulch<div class="area" data-key="riōrem"></div> quam Iūlius habet.</div>
+      <div>6. Mārcī oculī nig<div class="area" data-key="rī"></div> sunt.</div>
+      <div>7. Mārcī oculī nig<div class="area" data-key="riōrēs"></div> sunt quam cēterōrum discipulōrum.</div>
+      <div>8. Mārcī oculī nig<div class="area" data-key="errimī"></div> sunt.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>adverbium</i></span></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">-us (-er) -a -um</div> &gt; <div class="area">-ē</div></div>
+      <div><div class="area">-is -e</div> &gt; <div class="area">-iter</div></div>
+      <div><div class="area">bene</div> <div class="area">male</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Aemilius prōbus et fortis mīles est. Quōmodo pugnat Aemilius?</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Aemilius prob<u>ē</u> et fort<u>iter</u> pugnat.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Mārcus litterās turpēs scrībit. Quōmodo Mārcus scrībit? = T<div class="area" data-key="urpiter"></div>.</div>
+      <div>2. Stēllae lūce clārā lūcent. Quōmodo lūcent stēllae? = C<div class="area" data-key="lārē"></div>.</div>
+      <div>3. Discipulus indoctus prāvum respōnsum dat. Quōmodo discipulus indoctus respondet? = P<div class="area" data-key="rāvē"></div>.</div>
+      <div>4. Mīlitēs Rōmānī impigrī sunt. Quōmodo pugnant mīlitēs Rōmānī? = <div class="area" data-key="Impigrē"></div>.</div>
+      <div>5. Sextus pulchrās litterās scrībit. Quōmodo Sextus scrībit? = <div class="area" data-key="Pulchrē"></div>.</div>
+      <div>6. Improbus discipulus turpe, nōn pulchrum respōnsum dat. Quōmodo respondet improbus discipulus? = T<div class="area" data-key="urpiter"></div>, nōn p<div class="area" data-key="ulchrē"></div>.</div>
+      <div>7. Mārcus malās rēs dīcit. Quōmodo loquitur Mārcus? = <div class="area" data-key="Male"></div>.</div>
+      <div>8. Sextus vērō bonās rēs dīcit. Quōmodo loquitur Sextus? = <div class="area" data-key="Bene"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>comparātīvus adverbiī</i></span></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">-ius</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Titus nōn tam stultē nec tam turpiter scrībit quam Mārcus.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Mārcus stult<u>ius</u> et turp<u>ius</u> scrībit quam Titus.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Mārcus nōn tam rēctē scrībit quam Titus. = Titus <div class="area" data-key="rēctius"></div> scrībit quam Mārcus.</div>
+      <div>2. Titus nōn tam sevērē a magistrō reprehenditur quam Mārcus. = <div class="area" data-key="Mārcus"></div> <div class="area" data-key="evērius"></div> ā magistrō reprehenditur <div class="area" data-key="quam"></div> <div class="area" data-key="Titus"></div>.</div>
+      <div>3. Mārcus nōn tam graviter stilum in cēram premit quam magister. = <div class="area" data-key="Magister"></div> <div class="area" data-key="gravius"></div> stilum in cēram premit <div class="area" data-key="quam"></div> <div class="area" data-key="Mārcus"></div>.</div>
+      <div>4. Titus nōn tam foedē scrībit quam Mārcus. = <div class="area" data-key="Mārcus"></div> <div class="area" data-key="foedius"></div> scrībit quam <div class="area" data-key="Titus"></div>.</div>
+      <div>5. Germānī nōn tam fortiter pugnant quam Rōmānī. = <div class="area" data-key="Rōmānī"></div> <div class="area" data-key="fortius"></div> pugnant quam <div class="area" data-key="Germānī"></div>.</div>
+      <div>6. Stēllae nōn tam clārē lūcent quam lūna plēna. = <div class="area" data-key="Lūna"></div> <div class="area" data-key="plēna"></div> <div class="area" data-key="clārius"></div> lūce<div class="area" data-key="t"></div> quam <div class="area" data-key="stēllae"></div>.</div>
+      <div>7. Sextus nōn tam leviter scrībit quam Mārcus. = <div class="area" data-key="Mārcus"></div> <div class="area" data-key="levius"></div> <div class="area" data-key="scrībit"></div> <div class="area" data-key="quam"></div> <div class="area" data-key="Titus"></div>.</div>
+      <div>8. Improbī discipulī nōn tam rēctē respondent quam probī discipulī. = <div class="area" data-key="Probī"></div> d<div class="area" data-key="iscipulī"></div> <div class="area" data-key="rēctius"></div> respondent <div class="area" data-key="quam"></div> <div class="area" data-key="improbī"></div> d<div class="area" data-key="iscipulī"></div>.</div>
+      <div>9. Titus nōn tam graviter verberātur quam Mārcus. = <div class="area" data-key="Mārcus"></div> <div class="area" data-key="gravius"></div> <div class="area" data-key="verberātur"></div> <div class="area" data-key="quam"></div> <div class="area" data-key="Titus"></div>.</div>
+      <div>10. Titus nōn tam pulchrē scrībit quam Sextus. = <div class="area" data-key="Sextus"></div> <div class="area" data-key="pulchrius"></div> <div class="area" data-key="scrībit"></div> <div class="area" data-key="quam"></div> <div class="area" data-key="Titus"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>superlātīvus adverbiī</i></span></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">-issimē (-errimē, -illimē)</div></div></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Nūllus discipulus foedius quam Mārcus scrībit.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>= Mārcus foed<u>issimē</u> scrībit.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Nūllī mīlitēs fortius pugnant quam mīlitēs Rōmānī. = Mīlitēs Rōmānī <div class="area" data-key="fortissimē"></div> pugnant.</div>
+      <div>2. Nūllus discipulus stultius quam Mārcus respondet. = M<div class="area" data-key="ārcus"></div> <div class="area" data-key="stultissimē"></div> respondet.</div>
+      <div>3. Nūllus puer rēctius et pulchrius scrībit quam Sextus. = <div class="area" data-key="Sextus"></div> <div class="area" data-key="rēctissimē"></div> et <div class="area" data-key="pulcherrimē"></div> scrībit.</div>
+      <div>4. Nūllus magister discipulōs sevērius pūnit quam Diodōrus. = <div class="area" data-key="Diodōrus"></div> d<div class="area" data-key="iscipulōs"></div> <div class="area" data-key="sevērrimē"></div> p<div class="area" data-key="ūnit"></div>.</div>
+      <div>5. Nūlla stēlla clārius lūcet quam Sōl. = <div class="area" data-key="Sōl"></div> <div class="area" data-key="clārissimē"></div> <div class="area" data-key="lūcet"></div>.</div>
+      <div>6. Nūllus discipulus turpius quam Mārcus legit. = <div class="area" data-key="Mārcus"></div> <div class="area" data-key="turpissimē"></div> <div class="area" data-key="legit"></div>.</div>
+      <div>7. Nūllus discipulus facilius quam Sextus scrībit atque numerat. = <div class="area" data-key="Sextus"></div> <div class="area" data-key="facillimē"></div> s<div class="area" data-key="crībit"></div> atque n<div class="area" data-key="umerat"></div>.</div>
+      <div>8. Nūllus puer stilum in cēram levius premit quam Mārcus. = <div class="area" data-key="Mārcus"></div> <div class="area" data-key="levissimē"></div> <div class="area" data-key="stilum"></div> in <div class="area" data-key="cēram"></div> <div class="area" data-key="premit"></div>.</div>
+      <div>9. Nūlla puella pulchrius quam Iūlia cantat. = <div class="area" data-key="Iūlia"></div> <div class="area" data-key="pulcherrimē"></div> <div class="area" data-key="cantat"></div>.</div>
+      <div>10. Nūllus discipulus pigrius quam Mārcus computat. = <div class="area" data-key="Mārcus"></div> <div class="area" data-key="pigerrimē"></div> <div class="area" data-key="computat"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quīntum</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Stēllae <u>clārē</u> lūcent. Lūna <u>clārius</u> lūcet quam stēllae. Sōl <u>clārissimē</u> lūcet.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Germānī fort<div class="area" data-key="iter"></div> pugnant, sed Rōmānī fort<div class="area" data-key="ius"></div> quam iī pugnant: mīlitēs Rōmānī fort<div class="area" data-key="issimē"></div> pugnant.</div>
+      <div>2. Sextus pulchr<div class="area" data-key="ē"></div>, Titus vērō turp<div class="area" data-key="iter"></div> scrībit.</div>
+      <div>3. Titus pulchr<div class="area" data-key="ius"></div> quam Mārcus et foed<div class="area" data-key="ius"></div> quam Sextus scrībit.</div>
+      <div>4. Sextus pulcher<div class="area" data-key="rimē"></div>, Mārcus vērō foed<div class="area" data-key="issimē"></div> scrībit.</div>
+      <div>5. Mārcus prāv<div class="area" data-key="ē"></div> respondet. Titus rēct<div class="area" data-key="ius"></div> quam Mārcus respondet. Sextus rēct<div class="area" data-key="issimē"></div> respondet.</div>
+      <div>6. Mārcus lev<div class="area" data-key="iter"></div> stilum in cēram premit. Magister grav<div class="area" data-key="ius"></div> quam Mārcus stilum premit.</div>
+      <div>7. Nūllus discipulus turp<div class="area" data-key="ius"></div> scrībit quam Mārcus: is enim turp<div class="area" data-key="issimē"></div> scrībit.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium sextum</b></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">īdem</div> <div class="area">eadem</div> <div class="area">idem</div></div>
+      <div><div class="area">eundem</div> <div class="area">eandem</div> <div class="area">idem</div></div>
+      <div><div class="area">eiusdem</div></div>
+      <div><div class="area">eīdem</div></div>
+      <div><div class="area">eōdem</div> <div class="area">eādem</div> <div class="area">eōdem</div></div></div>
+      <div><br></div>
+      <div>1. Mārcus et Quīntus <div class="area" data-key="eundem"></div> magistrum habent.</div>
+      <div>2. Mārcus Quīntusque <div class="area" data-key="eiusdem"></div> magistrī discipulī sunt.</div>
+      <div>3. <div class="area" data-key="eadem"></div> domina Syrae Dēliaeque imperat.</div>
+      <div>4. Syrus et Lēander cum <div class="area" data-key="eōdem"></div> dominō in viā ambulant.</div>
+      <div>5. Servī Iūliī in <div class="area" data-key="eādem"></div> vīllā cum dominō habitant.</div>
+      <div>6. Syra et Lȳdia <div class="area" data-key="eīdem"></div> dominae nōn parent.</div>
+      <div>7. <div class="area" data-key="īdem"></div> magister Sextum, Mārcum et Titum docet.</div>
+      <div>8. Mēdus, Lȳdia et mercātor <div class="area" data-key="eandem"></div> nāvem cōnscendunt.</div>
+      <div>9. Diodōrus et Cornēlius in <div class="area" data-key="eōdem"></div> oppidō vīvunt.</div>
+      <div>10. Ventus et mare <div class="area" data-key="eīdem"></div> deō nōn pārent.</div>
+      <div>11. Mēdus et Dāvus <div class="area" data-key="eiusdem"></div> dominī <div class="area" data-key="eiusdem"></div>que dominae servī sunt.</div>
+      <div>12. In tabernīs <div class="area" data-key="idem"></div> ōrnāmentum saepe nōn <div class="area" data-key="eōdem"></div> pretiō emitur.</div>
+      <div>13. Pater et fīlius <div class="area" data-key="idem"></div> cognōmen habent.</div>
+      <div>14. Lupus et ovis in <div class="area" data-key="eādem"></div> silvā errant.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium septimum</b></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">quis<b>que</b></div> <div class="area">quae-</div> <div class="area">quod-</div></div>
+      <div><div class="area">quem-</div> <div class="area">quam</div> <div class="area">quod</div></div>
+      <div><div class="area">cuius-</div></div>
+      <div><div class="area">cui-</div></div>
+      <div><div class="area">quō-</div> <div class="area">quā-</div> <div class="area">quō-</div></div></div>
+      <div><br></div>
+      <div>1. Ōrnāmentum suum <div class="area" data-key="quamque"></div> fēminam dēlectat.</div>
+      <div>2. In suā <div class="area" data-key="quisque"></div> sellā discipulus sedet.</div>
+      <div>3. Sua ā <div class="area" data-key="quōque"></div> discipulō sententia scrībitur.</div>
+      <div>4. Magister suam <div class="area" data-key="cuique"></div> discipulō tabulam reddit.</div>
+      <div>5. Sua <div class="area" data-key="quaeque"></div> ancilla induit vestīmenta.</div>
+      <div>6. Dux suum <div class="area" data-key="quemque"></div> mīlitem gladium sūmere iubet.</div>
+      <div>7. Suās <div class="area" data-key="quodque"></div> oppidum tabernās habet.</div>
+      <div>8. Iūlius suum <div class="area" data-key="cuique"></div> servō mālum dat.</div>
+      <div>9. Fortissimī <div class="area" data-key="cuiusque"></div> exercitūs mīlitēs ā ducibus suīs laudantur.</div>
+      <div>10. Suum ā <div class="area" data-key="quāque"></div> fēminā ōrnāmentum laudātur.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium octāvum</b></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">quotiēs</div> <div class="area">totiēs</div> <div class="area">semel</div> etc.</div></div>
+      <div><br></div>
+      <div>1. Aemilia: “Mārce! Mārce!” <div class="area" data-key="Quotiēs"></div> Aemilia Mārcum vocat? Aemilia nōn <div class="area" data-key="semel"></div>, sed <div class="area" data-key="bis"></div> Mārcum vocat.</div>
+      <div>2. Aemilia Iūliam <div class="area" data-key="ter"></div> [3x] vocat. Quīntus quoque ab Aemiliā <div class="area" data-key="ter"></div> [3x] vocātur.</div>
+      <div>3. Discipulus impiger <div class="area" data-key="deciēs"></div> [10x] rēctē et <div class="area" data-key="semel"></div> [1x] tantum prāvē scrībit.</div>
+      <div>4. Discipulus vērō piger <div class="area" data-key="noviēs"></div> [9x] prāvē et <div class="area" data-key="bis"></div> [2x] tantum rēctē respondet.</div>
+      <div>5. <div class="area" data-key="Quotiēs"></div> Mārcus H litteram scrībit? Mārcus nōn <div class="area" data-key="quater"></div> [4x], neque <div class="area" data-key="quīnquiēs"></div> [5x], neque <div class="area" data-key="sexiēs"></div> [6x], sed <div class="area" data-key="deciēs"></div> [10x] H litteram scrībit.</div>
+      <div>6. Magister M litteram <div class="area" data-key="octiēs"></div> [8x] Mārcum scrībere iubet. Mārcus vērō, quī piger est, M litteram <div class="area" data-key="totiēs"></div> scrībere nōn vult.</div>
+      <div>7. Mārcus igitur nōn <div class="area" data-key="octiēs"></div> [8x], neque <div class="area" data-key="septiēs"></div> [7x], neque <div class="area" data-key="sexiēs"></div> [6x] M litteram scrībit, sed tantum <div class="area" data-key="quīnquiēs"></div> [5x].</div>
+      <div>8. <div class="area" data-key="Quotiēs"></div> Mārcus magistrō pāret? Mārcus <div class="area" data-key="semel"></div> [1x] tantum magistrō pāret.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium nōnum</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>magister / puerī / litterae / docēre = Magister puer<b>ōs</b> litter<b>ās</b> docet.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">			</span>magister / puerī / legere / docēre = Magister puer<b>ōs</b> <u>legere</u> docet.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. magister / puerī / numerī / docēre = Magister <div class="area" data-key="puerōs"></div> Magister <div class="area" data-key="numerōs"></div> docet.</div>
+      <div>2. magister / discipulī / multae rēs / docēre = Magister <div class="area" data-key="discipulōs"></div> <div class="area" data-key="multās rēs"></div> docet.</div>
+      <div>3. magister / puellae / litterae / docēre = Magister <div class="area" data-key="puellās"></div> <div class="area" data-key="litterās"></div> docet.</div>
+      <div>4. magister / iī / legere / docēre = Magister <div class="area" data-key="eōs"></div> <div class="area" data-key="legere"></div> docet.</div>
+      <div>5. magister / is / nihil / docēre posse = Magister <div class="area" data-key="eum"></div> <div class="area" data-key="nihil"></div> d<div class="area" data-key="ocēre"></div> p<div class="area" data-key="otest"></div>.</div>
+      <div>6. magister: “vōs / scrībere / docēre” = Magister: “<div class="area" data-key="vōs"></div> <div class="area" data-key="scrībere"></div> d<div class="area" data-key="oceō"></div>.”</div>
+      <div>7. magistrī / puerī / grammatica / docēre = <div class="area" data-key="Magistrī"></div> <div class="area" data-key="puerōs"></div> <div class="area" data-key="grammaticam"></div> <div class="area" data-key="docent"></div>.</div>
+      <div>8. magistrī: “discipulī / lingua Graeca / docēre” = <div class="area" data-key="Magistrī"></div>: “<div class="area" data-key="discipulōs"></div> <div class="area" data-key="linguam Graecam"></div> <div class="area" data-key="docēmus"></div>.”</div>
+    </div>
+  `
+});
