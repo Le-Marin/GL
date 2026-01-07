8fddef26c5d570aa9e0c29bb678b37c620734754
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2026-01-07 21:49:18 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-22pq54jo.js
@@ -0,0 +1,125 @@
+$_GET({
+  title: 'Nova exercitia 22',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>supīnum I</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Hostēs ad castra veniunt, quod ea oppugnāre volunt.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Hostēs castra oppugn<u>āt<b>um</b></u> veniunt.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Iūlius in hortum exit, quod ambulāre vult. = Iūlius in hortum exit ambul<div class="area" data-key="ātum"></div>.</div>
+      <div>2. Iūlia in cubiculum it, quod dormīre vult. = Iūlia in cubiculum d<div class="area" data-key="ormītum"></div> it.</div>
+      <div>3. Cornēlius ad vīllam Iūliī venit, quod eum salūtāre vult. = Cornēlius s<div class="area" data-key="alūtātum"></div> ad vīllam Iūliī <div class="area" data-key="venit"></div>.</div>
+      <div>4. Dominus ad servōs accēdit, quod pecūniam ab iīs postulāre vult. = Dominus ad <div class="area" data-key="servōs"></div> <div class="area" data-key="accēdit"></div> pe<div class="area" data-key="cūniam"></div> <div class="area" data-key="ab iīs"></div> p<div class="area" data-key="ostulātum"></div>.</div>
+      <div>5. Puerī ad lacum veniunt, quod lavārī volunt. = <div class="area" data-key="Puerī"></div> <div class="area" data-key="ad lacum"></div> <div class="area" data-key="veniunt"></div> l<div class="area" data-key="avātum"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>supīnum II</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Difficile est vōcem īnfantis audīre.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Vōx īnfantis difficilis est aud<u>īt<b>ū</b></u>.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Facile est hoc nōmen dīcere. = Hoc nōmen facile est dic<div class="area" data-key="tū"></div>.</div>
+      <div>2. Facile nōn est facere quod dīcis. = Facile nōn est f<div class="area" data-key="actū"></div> quod d<div class="area" data-key="īcis"></div>.</div>
+      <div>3. Litterās Graecās legere difficile est. = L<div class="area" data-key="itterae"></div> <div class="area" data-key="Graecae"></div> difficil<div class="area" data-key="ēs"></div> sunt l<div class="area" data-key="ēctū"></div>.</div>
+      <div>4. Facilius est hoc dīcere quam facere. = H<div class="area" data-key="oc"></div> <div class="area" data-key="facilius"></div> est f<div class="area" data-key="actū"></div> quam <div class="area" data-key="dictū"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>ablātīvus absolūtus</i></span></div>
+      <div><span class="ff"><i>(cum participiō perfectī)</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span><u>Postquam</u> / <u>Quia</u> canis vīnctus est, tabellārius in vīllam intrat.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Can<b>e</b> vīnct<b>ō</b>, tabellārius in vīllam intrat.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Postquam catēna ā cane rupta est (= c<div class="area" data-key="atēnā"></div> ā cane r<div class="area" data-key="uptā"></div>), tabellārius territus neque recēdere neque prōcēdere potest.</div>
+      <div>2. Quia canis solūtus est (= c<div class="area" data-key="ane"></div> s<div class="area" data-key="olūtō"></div>), tabellārius īrātus timet.</div>
+      <div>3. Postquam Mēdus ab Aemiliā vocātus est (= M<div class="area" data-key="ēdō"></div> ab Aemiliā v<div class="area" data-key="ocātō"></div>), Iūlius baculum sumit.</div>
+      <div>4. Quia ānulus ā Mēdō ēmptus est (= ā<div class="area" data-key="nulō"></div> ā Mēdō ē<div class="area" data-key="mptō"></div>), Lȳdia laeta est.</div>
+      <div>5. Postquam urbs ab hostibus oppugnāta est (=<div class="area" data-key="urbe"></div> ab <div class="area" data-key="hostibus"></div> op<div class="area" data-key="pugnātā"></div>), exercitus Rōmānus vocātus est.</div>
+      <div>6. Postquam discipulī verberātī sunt (=<div class="area" data-key="discipulīs"></div> v<div class="area" data-key="erberātīs"></div>), magister librum iterum recitat.</div>
+      <div>7. Postquam ancillae monitae sunt (=<div class="area" data-key="ancillīs"></div> <div class="area" data-key="monitīs"></div>), Aemilia in hortum exit.</div>
+      <div>8. Quia pallium ā cane scissum est (=<div class="area" data-key="palliō"></div> ā <div class="area" data-key="cane"></div> <div class="area" data-key="scissō"></div>), tabellārius īrātus discēdit.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>ablātīvus absolūtus</i></span></div>
+      <div><span class="ff"><i>(cum participiō perfectī)</i></span></div>
+      <div><br></div>
+      <div>1. Castr<div class="area" data-key="īs"></div> expugnāt<div class="area" data-key="īs"></div>, mīlitēs improbī fugiunt.</div>
+      <div>2. Mārc<div class="area" data-key="ō"></div> et Tit<div class="area" data-key="ō"></div> pūnīt<div class="area" data-key="īs"></div>, magister Sextum laudat.</div>
+      <div>3. Verb<div class="area" data-key="ō | īs"></div> rēctē scrīpt<div class="area" data-key="ō | īs"></div>, Sextus ā magistrō laudātur.</div>
+      <div>4. Can<div class="area" data-key="e"></div> iterum vīnct<div class="area" data-key="ō"></div>, tabellārius tranquillus est.</div>
+      <div>5. Tabul<div class="area" data-key="īs"></div> inter pugnam mūtāt<div class="area" data-key="īs"></div>, Mārcus domum redit.</div>
+      <div>6. Nōmin<div class="area" data-key="e"></div> medicī audīt<div class="area" data-key="ō"></div>, Quīntus horret.</div>
+      <div>7. Libr<div class="area" data-key="ō"></div> recitāt<div class="area" data-key="ō"></div>, magister discipulōs verberat.</div>
+      <div>8. Mult<div class="area" data-key="īs"></div> margarīt<div class="area" data-key="īs"></div> ā virīs ēmpt<div class="area" data-key="īs"></div>, fēminae gaudent.</div>
+      <div>9. Līber<div class="area" data-key="īs"></div> salūtāt<div class="area" data-key="īs"></div>, Iūlius in ātrium intrat.</div>
+      <div>10. Stil<div class="area" data-key="ō"></div> inter pugnam ā Sextō rupt<div class="area" data-key="ō"></div>, Mārcus scrībere nōn potest.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quīntum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>Perfecta in ablātīvōs absolūtōs mūtā</i></span></div>
+      <div><br></div>
+      <div>Novus diēs est. Discipulī in lūdō sedent, magistrō librum recitante.</div>
+      <div><br></div>
+      <div>Postquam initium librī recitātum est (= <div class="area" data-key="initiō librī recitātō"></div>), magister dīcit: “Hodiē novam sententiam in tabulīs vestrīs scrībētis”. Postquam haec verba dicta sunt (= <div class="area" data-key="hīs verbīs dictīs"></div>), magister sententiam dictat. Discipulī magistrum dictantem audiunt et tabulās suās, postquam sententiae scrīptae sunt (= <div class="area" data-key="sententiīs scrīptīs"></div>), eī dant. Sextus et Titus tōtam sententiam scrīpsērunt, sed Mārcus, quia calamus ā Sextō ruptus est (= <div class="area" data-key="calamō ā Sextō ruptō"></div>), initium tantum scrīpsit.</div>
+      <div><br></div>
+      <div>Magister sententiās Sextī et Titī aspicit et, postquam iī laudātī sunt (= <div class="area" data-key="iīs laudātīs"></div>), sententiam quoque Mārcī legit. Mārcus, quia Sextus et Titus ā magistrō nōn pūnītī sed laudātī sunt (= <div class="area" data-key="Sextō et Titō ā magistrō nōn pūnītīs sed laudātīs"></div>), īrātus tabulam ostendit, ‘sē quoque rēctē scrīpsisse’ dīcēns. Magister vērō, postquam Mārcī verba audīvit (= <div class="area" data-key="Mārcī verbīs audītīs"></div>), ‘eum nōn sōlum prāvē scrīpsisse, sed etiam falsa dīxisse’ dīcit. Tum ergō Mārcus, quī tabulās inter pugnam mūtāvit: “Nōnne vidēs nōmen Sextī in hāc tabulā scrīptum esse, magister?” inquit. “Sextī est haec tabula: ergō nōn ego, sed Sextus prāvē scrīpsit”.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium sextum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>ablātīvus māteriae</i></span></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>ānulus ferreus = ānulus ex ferrō</span></div>
+      </div>
+      <div><br></div>
+      <div>1. lectīca lignea = l<div class="area" data-key="ectīca"></div> <div class="area" data-key="ē | ex"></div> l<div class="area" data-key="ignō"></div></div>
+      <div>2. nummus aureus = n<div class="area" data-key="ummus"></div> <div class="area" data-key="ex"></div> au<div class="area" data-key="rō"></div></div>
+      <div>3. catēnae ferreae = <div class="area" data-key="catēnae"></div> <div class="area" data-key="ē | ex"></div> <div class="area" data-key="ferrō"></div></div>
+      <div>4. ōstium ligneum = <div class="area" data-key="ōstium ē lignō | ōstium ex lignō"></div></div>
+      <div>5. ānulī ex aurō = ā<div class="area" data-key="nulī"></div> au<div class="area" data-key="reī"></div></div>
+      <div>6. gladius ex ferrō = g<div class="area" data-key="ladius"></div> <div class="area" data-key="ferreus"></div></div>
+      <div>7. tabula ex lignō = <div class="area" data-key="tabula"></div> <div class="area" data-key="lignea"></div></div>
+      <div>8. speculum ex aurō = <div class="area" data-key="speculum aureum"></div></div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium septimum</b></div>
+      <div><br></div>
+      <div>1. <div class="area" data-key="dīcere"></div>, <div class="area" data-key="dīxisse"></div>, dictum</div>
+      <div>2. <div class="area" data-key="emere"></div>, ēmisse, <div class="area" data-key="ēmptum"></div></div>
+      <div>3. scindere, <div class="area" data-key="scidisse"></div>, <div class="area" data-key="scissum"></div></div>
+      <div>4. <div class="area" data-key="monēre"></div>, <div class="area" data-key="monuisse"></div>, monitum</div>
+      <div>5. <div class="area" data-key="terrēre"></div>, terruisse, <div class="area" data-key="territum"></div></div>
+      <div>6. posse, <div class="area" data-key="potuisse"></div></div>
+      <div>7. claudere, <div class="area" data-key="clausisse"></div>, <div class="area" data-key="clausum"></div></div>
+      <div>8. <div class="area" data-key="aperīre"></div>, <div class="area" data-key="aperuisse"></div>, apertum</div>
+      <div>9. solvere, <div class="area" data-key="solvisse"></div>, <div class="area" data-key="solūtum"></div></div>
+      <div>10. <div class="area" data-key="venīre"></div>, vēnisse</div>
+      <div>11. <div class="area" data-key="pellere"></div>, <div class="area" data-key="pepulisse"></div>, pulsum</div>
+      <div>12. <div class="area" data-key="rumpere"></div>, rūpisse, <div class="area" data-key="ruptum"></div></div>
+      <div>13. vincīre, <div class="area" data-key="vīnxisse"></div>, <div class="area" data-key="vīnctum"></div></div>
+    </div>
+  `
+});
