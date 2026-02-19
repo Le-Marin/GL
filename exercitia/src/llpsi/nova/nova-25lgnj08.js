bc70eb4fd9d7de38915778ebc5ff97a50ed47317
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2026-02-19 15:57:47 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/nova/nova-25lgnj08.js
@@ -0,0 +1,95 @@
+$_GET({
+  title: 'Nova exercitia 25',
+  html: /*html*/`
+    <div class="row">
+      <div><b>Exercitium prīmum</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>imperātīvus dēpōnentium</i></span></div>
+      <div><br></div><div class="fill-ins"><div><div class="area">-āre</div> <div class="area">-āminī</div> | <div class="area">-ēre</div> <div class="area">-ēminī</div></div>
+      <div><div class="area">-ere</div> <div class="area">-iminī</div> | <div class="area">-īre</div> <div class="area">-īminī</div></div></div>
+      <div><br></div>
+      <div>1. Iūlius: “Nōlī <div class="area" data-key="mentīrī">mentīrī</div>, Mārce; iam vērum <div class="area" data-key="fatēre">fatērī</div>!”</div>
+      <div>2. Iūlius: “Magistrī epistulam <div class="area" data-key="intuēre">intuērī</div>, Mārce! Magister ‘tē improbum discipulum fuisse’ scrīpsit. Iam nōlī falsum <div class="area" data-key="loquī">loquī</div>: Dāvum <div class="area" data-key="sequere">sequī</div>, quī tē in cubiculum inclūdet”.</div>
+      <div>3. Māter Sextī: “<div class="area" data-key="laetāre">laetārī</div>, fīlī mī, quod bonus discipulus in lūdō hodiē fuistī”.</div>
+      <div>4. Quīntus: “Nōlī ē cubiculō <div class="area" data-key="ēgredī">ēgredī</div>, Syra: hīc manē, mēque <div class="area" data-key="cōnsōlāre">cōnsōlārī</div>!”</div>
+      <div>5. Syra: “Nōlī <div class="area" data-key="oblīvīscī">oblīvīscī</div> mē Iūliam in hortum comitārī dēbēre, Quīnte. <div class="area" data-key="Opperīre">Opperīrī</div> paulum”.</div>
+      <div>6. Quīntus: “<div class="area" data-key="Pollicēre">Pollicērī</div> tē brevī reditūram esse, Syra!”</div>
+      <div>7. Iūlia: “Iam venī, Syra! Māla et pira <div class="area" data-key="intuēre">intuērī</div> quae tata mihi dedit”.</div>
+      <div>8. Syra: “Ō, quam fōrmōsa sunt! Ea cum Quīntō <div class="area" data-key="partīre">partīrī</div>, quī in lectō iacet neque surgere potest”.</div>
+      <div>9. Magister: “Post merīdiem in lūdum <div class="area" data-key="revertiminī">revertī</div>, discipulī!”</div>
+      <div>10. Mārcus: “Nōlīte magistrum timēre, amīcī, sed mē <div class="area" data-key="sequiminī">sequī</div>!”</div>
+      <div>11. Cornēlius: “Ad vīllam Iūliī mē <div class="area" data-key="comitāminī">comitārī</div>, servī!”</div>
+      <div>12. Cornēlius: “Mē ante hanc iānuam <div class="area" data-key="opperīminī">opperīrī</div>, servī!”</div>
+      <div>13. Iūlius: “<div class="area" data-key="Laetāminī">Laetārī</div>, līberī, quod novam sorōrem mox habēbitis”.</div>
+      <div>14. Iūlia: “Nōlī <div class="area" data-key="proficīscī">proficīscī</div>, pater! Mē in hortum <div class="area" data-key="comitāre">comitārī</div> et lūde mēcum!”</div>
+      <div>15. Mārcus: “Mē <div class="area" data-key="comitāminī">comitārī</div>, Tite et Sexte!” Titus: “<div class="area" data-key="Opperīre">Opperīrī</div>, Mārce! Quō īs?”</div>
+      <div>16. Mārcus: “Mē <div class="area" data-key="sequiminī">sequī</div>, Tite et Sexte; nōlīte ad lūdum statim <div class="area" data-key="revertiminī">revertī</div>!”</div>
+      <div>17. Mārcus: “<div class="area" data-key="Intuēminī">Intuērī</div> illum nīdum, quī inter folia et rāmōs est, amīcī!”</div>
+      <div>18. Sextus et Titus: “Ad lūdum īmus, Mārce: nōs <div class="area" data-key="sequere">sequī</div>!”</div>
+      <div>19. Mārcus: “Hūc <div class="area" data-key="revertiminī">revertī</div>, Sexte et Tite! Lūdum <div class="area" data-key="oblīvīsciminī">oblivīscī</div>!”</div>
+      <div>20. Mārcus: “Iam intellegō: magistrum timētis. Vōs igitur sequar, sed māla vestrā mēcum <div class="area" data-key="partīminī">partīrī</div>!”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium secundum</b></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">Athēnīs</div> <div class="area">Athēnās</div></div>
+      <div><div class="area">Naxī</div> <div class="area">Naxum</div> <div class="area">Naxō</div></div>
+      <div><div class="area">in Crētā</div> <div class="area">ad/in Crētam</div> <div class="area">ā/ē Crētā</div></div></div>
+      <div><br></div>
+      <div>1. Cornēlius <div class="area" data-key="Tūsculī">Tūsculum</div> habitat, Thēseus vērō <div class="area" data-key="Athēnīs">Athēnae</div> habitābat.</div>
+      <div>2. Cornēlius <div class="area" data-key="Tūsculō">Tūsculum</div> <div class="area" data-key="Rōmam">Rōma</div> it, Thēseus vērō <div class="area" data-key="Athēnīs">Athēnae</div> <div class="area" data-key="in Crētam | ad Crētam">Crēta</div> nāvigāvit.</div>
+      <div>3. Cornēlius nunc <div class="area" data-key="Rōmae">Rōma</div> est, Thēseus vērō <div class="area" data-key="in Crētā">Crēta</div>.</div>
+      <div>4. <div class="area" data-key="In urbe Rōmā">Urbs Rōma</div> multa aedificia sunt, <div class="area" data-key="in īnsulā Crētā">īnsula Crēta</div> labyrinthus erat.</div>
+      <div>5. Cornēlius <div class="area" data-key="Rōmā">Rōma</div> <div class="area" data-key="Tūsculum">Tūsculum</div> redit, quod <div class="area" data-key="in oppidō Tūsculō">oppidum Tūsculum</div> vīvit.</div>
+      <div>6. Thēseus <div class="area" data-key="ā Crētā | ē Crētā">Crēta</div> <div class="area" data-key="Naxum">Naxus</div> nāvigat.</div>
+      <div>7. Thēseus Ariadnam <div class="area" data-key="Naxī">Naxus</div> dēseruit. Deinde <div class="area" data-key="ab īnsulā Naxō | ex īnsulā Naxō">īnsula Naxus</div> <div class="area" data-key="Athēnās">Athēnae</div> rediit.</div>
+      <div>8. Thēseus <div class="area" data-key="in urbe Athēnīs">urbs Athēnae</div> iterum est, Ariadna vērō <div class="area" data-key="in īnsulā Naxō">īnsula Naxus</div> manet.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium tertium</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Thēseus fīlum secūtus est et labyrinthī exitum facile repperit.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Thēseus fīlum secūtus labyrinthī exitum facile repperit.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Iūlius haec locūtus est atque abiit. = Iūlius haec l<div class="area" data-key="ocūtus"></div> ab<div class="area" data-key="iit"></div>.</div>
+      <div>2. Thēseus Athēnīs profectus est et Crētam petīvit. = Thēseus Athēnīs p<div class="area" data-key="rofectus"></div> <div class="area" data-key="Crētam"></div> <div class="area" data-key="petīvit"></div>.</div>
+      <div>3. Multī virī prōmissa sua oblītī sunt et fēminās sine pecūniā dēseruērunt. = Multī virī p<div class="area" data-key="rōmissa"></div> <div class="area" data-key="sua"></div> <div class="area" data-key="oblītī"></div> f<div class="area" data-key="ēminās"></div> since pecūniā <div class="area" data-key="dēseruērunt"></div>.</div>
+      <div>4. Mēdus et Lȳdia ex Italiā profectī erant et in Graeciam nāvigābant. = Mēdus et Lȳdia <div class="area" data-key="ex Italiā"></div> p<div class="area" data-key="rofectī"></div> <div class="area" data-key="in Graeciam"></div> n<div class="area" data-key="āvigābant"></div>.</div>
+      <div>5. Syra Iūliam in hortum comitāta est et in Quīntī cubiculum rediit. = Syra <div class="area" data-key="Iūliam"></div> <div class="area" data-key="in hortum"></div> c<div class="area" data-key="omitāta"></div> <div class="area" data-key="in"></div> <div class="area" data-key="Quīntī"></div> c<div class="area" data-key="ubiculum"></div> <div class="area" data-key="rediit"></div>.</div>
+      <div>6. Mīlitēs ‘sē fortiter pugnātūrōs esse’ pollicitī sunt et in hostēs impetum fēcērunt. = Mīlitēs sē fortiter <div class="area" data-key="pugnātūrōs esse"></div> <div class="area" data-key="pollicitī"></div> in <div class="area" data-key="hostēs"></div> <div class="area" data-key="impetum"></div> <div class="area" data-key="fēcērunt"></div>.</div>
+      <div>7. Iūlius uxōrem ōsculātus est et Rōmam abiit. = Iūlius <div class="area" data-key="uxōrem"></div> <div class="area" data-key="ōsculātus"></div> <div class="area" data-key="Rōmam"></div> <div class="area" data-key="abiit"></div>.</div>
+      <div>8. Mārcus ‘sē improbum discipulum fuisse’ fassus est et ‘sē bonum puerum futūrum esse’ prōmīsit. = Mārcus sē improbum discipulum <div class="area" data-key="fuisse"></div> <div class="area" data-key="fassus"></div> <div class="area" data-key="sē"></div> bonum puerum <div class="area" data-key="futūrum esse"></div> <div class="area" data-key="prōmīsit"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium quārtum</b></div>
+      <div><br></div>
+      <div>1. iubēre, <div class="area" data-key="iussisse"></div>, <div class="area" data-key="iussum"></div></div>
+      <div>2. <div class="area" data-key="coepisse"></div>, coeptum</div>
+      <div>3. <div class="area" data-key="interficere"></div>, interfēcisse, <div class="area" data-key="interfectum"></div></div>
+      <div>4. trahere, <div class="area" data-key="trāxisse"></div>, <div class="area" data-key="tractum"></div></div>
+      <div>5. <div class="area" data-key="dēserere"></div>, dēseruisse, <div class="area" data-key="dēsertum"></div></div>
+      <div>6. <div class="area" data-key="iacere"></div>, <div class="area" data-key="iēcisse"></div>, iactum</div>
+      <div>7. regere, <div class="area" data-key="rēxisse"></div>, <div class="area" data-key="rēctum"></div></div>
+      <div>8. <div class="area" data-key="gerere"></div>, <div class="area" data-key="gessisse"></div>, gestum</div>
+      <div>9. <div class="area" data-key="petere"></div>, petīvisse, <div class="area" data-key="petītum"></div></div>
+      <div>10. reperīre, <div class="area" data-key="repperisse"></div>, <div class="area" data-key="repertum"></div></div>
+      <div>11. <div class="area" data-key="reddere"></div>, <div class="area" data-key="reddidisse"></div>, redditum</div>
+      <div>12. <div class="area" data-key="relinquere"></div>, relīquisse, <div class="area" data-key="relictum"></div></div>
+      <div>13. occīdere, <div class="area" data-key="occīdisse"></div>, <div class="area" data-key="occīsum"></div></div>
+      <div>14. quaerere, <div class="area" data-key="quaesīvisse"></div>, <div class="area" data-key="quaesītum"></div></div>
+      <div>15. <div class="area" data-key="cōnstituere"></div>, <div class="area" data-key="cōnstituisse"></div>, cōnstitūtum</div>
+      <div>16. <div class="area" data-key="accēdere"></div>, accessisse</div>
+      <div>17. cōnspicere, <div class="area" data-key="cōnspexisse"></div>, <div class="area" data-key="cōnspectum"></div></div>
+      <div>18. (a, cōn, dē)scendere, (a, cōn, dē)<div class="area" data-key="scendisse"></div>, (a, cōn, dē)<div class="area" data-key="scēnsum"></div></div>
+      <div>19. <div class="area" data-key="sequī"></div>, secūtum</div>
+      <div>20. oblīvīscī, <div class="area" data-key="oblītum"></div></div>
+      <div>21. proficīscī, <div class="area" data-key="profectum"></div></div>
+      <div>22. <div class="area" data-key="pollicērī"></div>, pollicitum</div>
+    </div>
+  `
+});
