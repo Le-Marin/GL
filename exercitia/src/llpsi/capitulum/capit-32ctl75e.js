f17be8f1c01cc4bf33b8e644a775fafb41a0807c
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2026-04-04 07:32:24 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/capitulum/capit-32ctl75e.js
@@ -0,0 +1,165 @@
+$_GET({
+  title: 'Exercitia Latina 32',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>I</div>
+      <div><br></div>
+      <div><b>Exercitium 1</b></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">adversus</div> <div class="area">aliquot</div> <div class="area">audācia</div> <div class="area">cārus</div> <div class="area">classis</div> <div class="area">commūnis</div> <div class="area">contemnere</div> <div class="area">cūnctus</div> <div class="area">ēgregius</div> <div class="area">gēns</div> <div class="area">grātus</div> <div class="area">incola</div> <div class="area">īnfēstus</div> <div class="area">inopia</div> <div class="area">mercātōrius</div> <div class="area">nūbilus</div> <div class="area">percurrere</div> <div class="area">populus</div> <div class="area">praepōnere</div> <div class="area">proximus</div> <div class="area">rēmigāre</div> <div class="area">servitūs</div> <div class="area">tuērī</div> <div class="area">ubīque</div> <div class="area">victor</div> <div class="area">victōria</div> <div class="area">vīlis</div> <div class="area">vīrēs</div> <div class="area">vīs</div></div></div>
+      <div><br></div>
+      <div>1. Ōlim <div class="area" data-key="cūncta"></div> maria <div class="area" data-key="īnfēsta"></div> erant praedōnibus.</div>
+      <div>2. Quī nāvigābant mortem aut <div class="area" data-key="servitūtem"></div> metuēbant.</div>
+      <div>3. Multae īnsulae ab <div class="area" data-key="incolīs"></div> perterritīs dēserēbantur.</div>
+      <div>4. Tanta erat <div class="area" data-key="vīs"></div> et <div class="area" data-key="audācia"></div> praedōnum ut etiam classem Rōmānam <div class="area" data-key="contemnerent"></div> .</div>
+      <div>5. Tum classī Rōmānae <div class="area" data-key="praepositus"></div> est Pompeius, dux <div class="area" data-key="ēgregius"></div>, quī praedōnēs <div class="area" data-key="ubīque"></div> vīcit.</div>
+      <div>6. Nūntius dē <div class="area" data-key="victōriā"></div> Pompeiī ēgregiā cūnctō <div class="area" data-key="populō"></div> Rōmānō <div class="area" data-key="grātus"></div> fuit.</div>
+      <div>7. Frūmentum, quod tam <div class="area" data-key="cārum"></div> fuerat ut <div class="area" data-key="aliquot"></div> hominēs pauperēs <div class="area" data-key="inopiā"></div> cibī perīrent, rūrsus <div class="area" data-key="vīle"></div> fuit.</div>
+      <div>8. Populus Pompeium <div class="area" data-key="victōrem"></div> maximīs laudibus affēcit.</div>
+      <div>9. Mare Tūscum est mare Rōmae <div class="area" data-key="proximum"></div>.</div>
+      <div>10. <div class="area" data-key="classis"></div> Rōmāna, quae cūncta maria <div class="area" data-key="percurrit"></div>, nāvēs <div class="area" data-key="mercātōriās"></div> ā praedōnibus <div class="area" data-key="tuētur"></div>.</div>
+      <div>11. Gallī, Hispānī, Graecī, Iūdaeī sunt <div class="area" data-key="gentēs"></div> imperiī Rōmānī.</div>
+      <div>12. Maria omnium gentium <div class="area" data-key="commūnia"></div> sunt.</div>
+      <div>13. Caelum <div class="area" data-key="nūbilum"></div> et ventus <div class="area" data-key="adversus"></div> est.</div>
+      <div>14. Iam necesse est omnibus <div class="area" data-key="vīribus"></div> <div class="area" data-key="rēmigāre"></div>.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>II</div>
+      <div><br></div>
+      <div><b>Exercitium 3</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Iūlius Cornēli<b>um</b> <u>interrogat</u> quid sentiat =</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Iūlius <u>ā</u> Corneli<b>ō</b> <u>quaerit</u> quid sentiat.</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Iūlia frātr<b>em</b> <u>ōrat/rogat</u> ut sē adiuvet =</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Iūlia <u>ā</u> frātr<b>e</b> <u>petit</u> ut sē adiuvet.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Iūlius servōs interrogat (= <div class="area" data-key="ā servīs quaerit"></div>) ubi nummī sint.</div>
+      <div>2. Rēx nautās interrogāvit (= <div class="area" data-key="ā nautīs quaesīvit"></div>) ubi esset Arīōn.</div>
+      <div>3. Syra Aemiliam interrogābat (= <div class="area" data-key="ab Aemiliā quaerēbat"></div>) quid medicus dīceret.</div>
+      <div>4. Quīntus Syram rogat (= <div class="area" data-key="ā Syrā petit"></div>) ut sibi fābulam nārret.</div>
+      <div>5. Mēdus amīcam ōrāvit (= <div class="area" data-key="ab amīcā petīvit"></div>) ut sēcum venīret.</div>
+      <div>6. “Sī nihil ā mē petis (= <div class="area" data-key="mē rogās"></div>), nihil tibi negō!”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 4</b></div>
+      <div><br></div>
+      <div><span class="ff"><i>coniūnctīvus perfectī</i></span></div>
+      <div><br></div>
+      <div>1. Pater ā fīliō quaerit cūr verberāt<div class="area" data-key="us sit"></div> ā magistrō: “Dīc mihi, fīlī, cūr tū ā magistrō verberāt<div class="area" data-key="us sīs"></div>?”</div>
+      <div>2. Fīlius respondet: “Nesciō cūr verberāt<div class="area" data-key="us sim"></div>, ego enim bonus discipulus fuī.”</div>
+      <div>3. Pater dubitat num fīlius vērum dīx<div class="area" data-key="erit"></div> eumque interrogat num vērē bonus fu<div class="area" data-key="erit"></div> ac magistrō pāru<div class="area" data-key="eit"></div>. Fīlius nōn respondet.</div>
+      <div>4. Pater: “Quīn respondēs? Quaerō ā tē num bonus discipulus fu<div class="area" data-key="erīs"></div> ас magistrō pāru<div class="area" data-key="erīs"></div>.”</div>
+      <div>5. Fīlius: “Cūr ā mē quaeris num bonus discipulus fu<div class="area" data-key="erim"></div> ac magistrō pāru<div class="area" data-key="erim"></div>? Iam tibi dīxī ‘mē bonum discipulum fuisse’.”</div>
+      <div>6. Pater: “Id quaerō quia dubitō num vērum dīx<div class="area" data-key="erīs"></div>.”</div>
+      <div>7. Fīlius: “Tūne dubitās num vērum dīx<div class="area" data-key="erim"></div>? Nōnne fīliō tuō crēdis?”</div>
+      <div>8. Pater ā fīliīs quaerit cūr verberāt<div class="area" data-key="ī sint"></div> ā magistrō: “Dīcite mihi, fīliī, cūr vōs ā magistrō verberāt<div class="area" data-key="ī sītis"></div>?”</div>
+      <div>9. Fīliī respondent: “Nescīmus cūr verberāt<div class="area" data-key="ī sīmus"></div>, nōs enim bonī discipulī fuimus.”</div>
+      <div>10. Pater dubitat num fīliī vērum dīx<div class="area" data-key="erint"></div> eōsque interrogat num vērē bonī fu<div class="area" data-key="erint"></div> ac magistrō pāru<div class="area" data-key="erint"></div>. Fīliī nōn respondent.</div>
+      <div>11. Pater: “Quīn respondētis? Quaerō ā vōbīs num bonī discipulī fu<div class="area" data-key="erītis"></div> ac magistrō pāru<div class="area" data-key="erītis"></div>.”</div>
+      <div>12. Fīliī: “Cūr ā nōbīs quaeris num bonī discipulī fu<div class="area" data-key="erīmus"></div> ac magistrō pāru<div class="area" data-key="erīmus"></div>? Iam tibi dīximus ‘nōs bonōs discipulōs fuisse’.”</div>
+      <div>13. Pater: “Id quaerō quia dubitō num vērum dīx<div class="area" data-key="erītis"></div>.”</div>
+      <div>14. Fīliī: “Quid? Tūne dubitās num nōs vērum dīx<div class="area" data-key="erīmus"></div>?”</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 5</b></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">amīcitia</div> <div class="area">deesse</div> <div class="area">dissuādēre</div> <div class="area">dōnec</div> <div class="area">ēdūcere</div> <div class="area">flectere</div> <div class="area">grātia</div> <div class="area">inermis</div> <div class="area">intereā</div> <div class="area">meminisse</div> <div class="area">neu</div> <div class="area">offerre</div> <div class="area">poēta</div> <div class="area">praeferre</div> <div class="area">redimere</div> <div class="area">reminīscī</div> <div class="area">resistere</div> <div class="area">voluntās</div></div></div>
+      <div><br></div>
+      <div>1. Dominus servōs monet ut labōrent <div class="area" data-key="neu"></div> quiēscant.</div>
+      <div>2. Mēdus gladium <div class="area" data-key="ēdūcit"></div>, nē <div class="area" data-key="inermis"></div> occīdātur, sed gubernātor eī <div class="area" data-key="dissuādet"></div> nē praedōnibus <div class="area" data-key="resistat"></div>.</div>
+      <div>3. Mēdus: “<div class="area" data-key="dōnec"></div> ego vīvam, nēmō tibi nocēbit, Lȳdia!”</div>
+      <div>4. <div class="area" data-key="intereā"></div> Lȳdia, genibus <div class="area" data-key="flexīs"></div>, Deum precātur: “Pater noster, quī es in caelīs! Fīat <div class="area" data-key="voluntās"></div> tua!”</div>
+      <div>5. Gubernātor praedōnibus magnam pecūniam <div class="area" data-key="offerre"></div> vult, namque is lībertātem omnibus rēbus <div class="area" data-key="praefert"></div>.</div>
+      <div>6. Sī ipse satis pecūniae nōn habet, amīcī eum ē servitūte <div class="area" data-key="redimere"></div> possunt; hanc <div class="area" data-key="grātiam"></div> ab amīcīs suīs petet.</div>
+      <div>7. Hoc audiēns mercātor duōs versūs <div class="area" data-key="reminīscitur"></div> quōs scrīpsit <div class="area" data-key="poēta"></div> quīdam Rōmānus.</div>
+      <div>8. Gubernātor nōmen poētae oblītus est, sed mercātor nōmen eius <div class="area" data-key="meminit"></div>.</div>
+      <div>9. Vērus amīcus numquam amīcō suō <div class="area" data-key="deest"></div>, sed vēra <div class="area" data-key="amīcitia"></div> rēs rāra est.</div>
+      <div>10. Quī tibi <div class="area" data-key="grātiam"></div> habet prō beneficiō, nōn sōlum tibi grātiās <div class="area" data-key="agere"></div>, sed etiam grātiam <div class="area" data-key="referre"></div> dēbet.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>III</div>
+      <div><br></div>
+      <div><b>Exercitium 7</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Nōlī dēspērāre! = Nē dēspērāverīs!</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Nōlīte dēspērāre! = Nē dēspērāverītis!</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Nōlī plōrāre! = <div class="area" data-key="nē plōrāverīs"></div>!</div>
+      <div>2. Nōlīte clāmāre! = <div class="area" data-key="nē clāmāverītis"></div>!</div>
+      <div>3. Nōlīte timēre! = <div class="area" data-key="nē timuerītis"></div>!</div>
+      <div>4. Nōlī fūrtum facere! = <div class="area" data-key="nē"></div> fūrtum <div class="area" data-key="fēcerīs"></div>!</div>
+      <div>5. Nē fūgerītis! = <div class="area" data-key="nōlīte fugere"></div>!</div>
+      <div>6. Nē mē relīquerīs! = <div class="area" data-key="nōlī"></div> mē <div class="area" data-key="relinquere"></div>!</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 8</b></div>
+      <div><br></div>
+      <div class="fill-ins"><div><div class="area">aliquandō</div> <div class="area">amphitheātrum</div> <div class="area">armāre</div> <div class="area">condiciō</div> <div class="area">cursus</div> <div class="area">dēsistere</div> <div class="area">etiamnunc</div> <div class="area">minārī</div> <div class="area">mūtuus</div> <div class="area">nē</div> <div class="area">pīrāta</div> <div class="area">superbus</div> <div class="area">talentum</div> <div class="area">utinam</div></div></div>
+      <div><br></div>
+      <div>1. Mēdus <div class="area" data-key="aliquandō"></div> amīcum captum redēmit.</div>
+      <div>2. Pecūniam quam ipse nōn habēbat <div class="area" data-key="mūtuam"></div> sūmpsit eā <div class="area" data-key="condiciōne"></div> ut annō post omnis pecūnia redderētur.</div>
+      <div>3. Mēdus optat ut patriam videat: “<div class="area" data-key="utinam"></div> patriam videam!”</div>
+      <div>4. <div class="area" data-key="pīrātae"></div> vīgintī <div class="area" data-key="talenta"></div> ā Caesare postulāvērunt.</div>
+      <div>5. Caesar, vir <div class="area" data-key="superbus"></div>, quīnquāgintā talenta pīrātīs obtulit, ac simul iīs supplicium <div class="area" data-key="minātus"></div> est.</div>
+      <div>6. Redēmptus Caesar nāvēs <div class="area" data-key="armāvit"></div> et ipse pīrātās cēpit.</div>
+      <div>7. Nautae rēmigāre <div class="area" data-key="dēsistunt"></div> ac rēmōs tollunt.</div>
+      <div>8. Servus fugitīvus timet <div class="area" data-key="nē"></div> ā mīlitibus capiātur, ut ad bēstiās mittātur in <div class="area" data-key="amphitheātrō"></div> Rōmānō.</div>
+      <div>9. In magnā tempestāte difficile est <div class="area" data-key="cursum"></div> tenēre.</div>
+      <div>10. Mēnsis decimus <div class="area" data-key="etiamnunc"></div> Octōber appellātur.</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 9</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exempla</i>:<span style="white-space: pre;">	</span>Diī nōs serv<b>ent</b>!</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Utinam pīrātae nōbīs parc<b>ant</b>!</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>Utinam nē ā pīrātīs interfici<b>āmur</b>!</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Quisquis amat vale<div class="area" data-key="at"></div>! Pere<div class="area" data-key="at"></div> quī nescit amāre!</div>
+      <div>2. Utinam dī omnia bona tibi d<div class="area" data-key="ent"></div>!</div>
+      <div>3. Vale<div class="area" data-key="ant"></div> cīvēs meī! Beātī s<div class="area" data-key="int"></div> omnēs!</div>
+      <div>4. Iuppiter tē perd<div class="area" data-key="at"></div>, homō scelestissime!</div>
+      <div>5. Bene ambulā, amīce! Utinam nē err<div class="area" data-key="ēs"></div>!</div>
+      <div>6. Utinam ego salvus in patriam revert<div class="area" data-key="ar"></div>!</div>
+      <div>7. Utinam nē nāvis merg<div class="area" data-key="ātur"></div>! Quod dī prohibe<div class="area" data-key="ant"></div>!</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 10</b></div>
+      <div><br></div>
+      <div class="sc">
+        <div><span class="ff"><i>Exemplum</i>:<span style="white-space: pre;">	</span>Nauta: “Utinam nē peream!”</span></div>
+        <div><span class="ff"><span style="white-space: pre;">		</span><span style="white-space: pre;">	</span>= Nauta timet <u>nē</u> per<b>eat</b>.</span></div>
+      </div>
+      <div><br></div>
+      <div>1. Pāstor: “Utinam nē ovēs aberrent!” Pāstor timet <div class="area" data-key="nē"></div> ovēs aberr<div class="area" data-key="ent"></div>.</div>
+      <div>2. Parentēs timent <div class="area" data-key="nē"></div> quid (: aliquid) malī līberīs suīs accid<div class="area" data-key="at"></div>.</div>
+      <div>3. Ariadna timēbat <div class="area" data-key="nē"></div> Thēseus ā Mīnōtaurō occīd<div class="area" data-key="erētur"></div>.</div>
+      <div>4. Daedalus metuēbat <div class="area" data-key="nē"></div> Īcarus sōlī appropinquā<div class="area" data-key="ret"></div>.</div>
+      <div>5. Vidēns ventum validum Petrus timuit <div class="area" data-key="nē"></div> merg<div class="area" data-key="erētur"></div>.</div>
+      <div>6. Nautae: “Utinam perīcula vītēmus!” Nautae verentur <div class="area" data-key="nē"></div> perīcula nōn vīt<div class="area" data-key="ent"></div> (= ut perīcula vīt<div class="area" data-key="ent"></div>).</div>
+      <div><br></div>
+    </div>
+    <div class="row">
+      <div><b>Exercitium 13</b></div>
+      <div><br></div>
+      <div>1. adesse, <div class="area" data-key="affu"></div>isse</div>
+      <div>2. agere, <div class="area" data-key="ēg"></div>iss, <div class="area" data-key="āct"></div>um esse</div>
+      <div>3. flectere, <div class="area" data-key="flex"></div>isse, <div class="area" data-key="flex"></div>um esse</div>
+      <div>4. offerre, <div class="area" data-key="obtul"></div>isse, <div class="area" data-key="oblāt"></div>um esse</div>
+      <div>5. redimere, <div class="area" data-key="redēm"></div>isse, <div class="area" data-key="redēmpt}um esse&lt;/div&gt;&lt;div&gt;6. tollere, $${{sustul"></div>isse, <div class="area" data-key="sublāt"></div>um esse</div>
+      <div>7. vincere, <div class="area" data-key="vīc"></div>isse, <div class="area" data-key="vict"></div>um esse</div>
+    </div>
+  `
+});
