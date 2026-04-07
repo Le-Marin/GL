653c04142edf00b0004ee24071af8b48c7650e2d
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2026-04-07 16:02:15 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/pensa/pens-33w4uf0h.js
@@ -0,0 +1,28 @@
+$_GET({
+  title: 'Pensa 33',
+  html: /*html*/`
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Magister epistulam ad Iūlium scrīpsit, cum Mārcus in lūdō dormīv<div class="area" data-key="isset"></div> nec magistrō pāru<div class="area" data-key="isset"></div>. Sī Mārcus bonus discipulus fu<div class="area" data-key="isset"></div>, magister eum laudāv<div class="area" data-key="isset"></div> nec epistulam scrīps<div class="area" data-key="isset"></div>.</div>
+      <div><span style="white-space: pre;">	</span>Mārcus: “Herī in cubiculō inclūsus sum, cum pater epistulam tuam lēg<div class="area" data-key="isset"></div>. Nisi tū eam epistulam scrīps<div class="area" data-key="issēs"></div>, ā patre laudātus es<div class="area" data-key="sem"></div>.” Magister: “Epistulam scrīpsī, cum in lūdō dormīv<div class="area" data-key="issēs"></div> nec mihi pāru<div class="area" data-key="issēs"></div>. Sī industrius fu<div class="area" data-key="issēs"></div>, tē laudāv<div class="area" data-key="issem"></div> nec epistulam scrīps<div class="area" data-key="issem"></div>.” Mārcus: “Etiam sī industrius fu<div class="area" data-key="issem"></div> et tibi pāru<div class="area" data-key="issem"></div>, mē nōn laudāv<div class="area" data-key="issēs"></div>!”</div>
+      <div><span style="white-space: pre;">	</span>Clēmēns es<div class="area" data-key="tō"></div>, domine! Patientiam habē<div class="area" data-key="tō"></div>! Servōs probōs laudā<div class="area" data-key="tō"></div>, sed improbōs pūnī<div class="area" data-key="tō"></div>!</div>
+      <div><span style="white-space: pre;">	</span>Industriī es<div class="area" data-key="tōte"></div>, servī! Cum dominus loquitur, tacē<div class="area" data-key="tōte"></div> et audī<div class="area" data-key="tōte"></div>! Semper officium faci<div class="area" data-key="tōte"></div>!</div>
+      <div><span style="white-space: pre;">	</span>Verba: īnstruere <div class="area" data-key="īnstru"></div>isse <div class="area" data-key="īnstrūct"></div>um; cōgere <div class="area" data-key="coēg"></div>isse <div class="area" data-key="coāct"></div>um; caedere <div class="area" data-key="cecīd"></div>isse <div class="area" data-key="caes"></div>um; convertere <div class="area" data-key="convert"></div>isse <div class="area" data-key="convers"></div>um; percutere <div class="area" data-key="percuss"></div>isse <div class="area" data-key="percuss"></div>um; prōcurrere <div class="area" data-key="prōcurr"></div>isse; dēsistere <div class="area" data-key="dēstit"></div>isse; prōgredī <div class="area" data-key="prōgress"></div>um esse.</div>
+    </div>
+    <div class="row">
+      <div><span style="white-space: pre;">	</span>Ūna <div class="area" data-key="legiō"></div> cōnstat ex V vel VI mīlibus hominum, quī in X <div class="area" data-key="cohortēs"></div> dīviduntur. Exercitus prōcēdēns <div class="area" data-key="agmen"></div> dīcitur. Exercitus ad <div class="area" data-key="proelium"></div> īnstrūctus <div class="area" data-key="aciēs"></div> appellātur. Post victōriam dux ā mīlitibus <div class="area" data-key="imperātor"></div> nōminātur. Sī mīlitēs fortiter pugnāvērunt, <div class="area" data-key="virtūs"></div> eōrum ab imperātōre laudātur. Officium mīlitis est sanguinem <div class="area" data-key="effundere"></div> prō patriā.</div>
+      <div><span style="white-space: pre;">	</span>Aemilius, quī in capitulō XII <div class="area" data-key="commemorātus"></div> est, ūnā cum Valeriō, adulēscente eiusdem <div class="area" data-key="aetātis"></div>, in Germāniā <div class="area" data-key="stipendia"></div> meruit. Tabellārius pūblicus XV <div class="area" data-key="ferē"></div> diēbus Rōmā in Germāniam <div class="area" data-key="properāre"></div> potest. Difficile est Alpēs <div class="area" data-key="trānsīre"></div>. Mīlitēs Rōmānī <div class="area" data-key="citrā"></div> Dānuvium sunt, hostēs sunt <div class="area" data-key="ultrā"></div> Dānuvium.</div>
+    </div>
+    <div class="row">
+      <div>Quae arma gerunt auxilia?</div>
+      <div>Quid est signum legiōnis?</div>
+      <div>Quōmodo mīlitēs in aciem īnstruuntur?</div>
+      <div>Ad quod studium pater Aemilium hortābātur?</div>
+      <div>Quō Aemilius adulēscēns missus est?</div>
+      <div>Cūr Aemilius epistulās legēns permovētur?</div>
+      <div>Quamobrem ipse paucās epistulās scrīpsit?</div>
+      <div>Quī nūntius nocte in castra allātus est?</div>
+      <div>Cūr hostēs castra Rōmāna nōn expugnāvērunt?</div>
+      <div>Num Tibullus vītam mīlitārem laudat?</div>
+    </div>
+  `
+});
