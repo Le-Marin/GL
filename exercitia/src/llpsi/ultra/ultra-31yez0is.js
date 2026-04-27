0f956022da12d84b7304bed0da311d21a1ad3831
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2026-04-27 17:40:53 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-31yez0is.js
@@ -0,0 +1,27 @@
+$_GET({
+  title: 'Ultra 31',
+  html: /*html*/`
+    <div class="row">
+      <b>1. Ūtere gerundīvīs prō <i>dēbēre</i>, <i>oportet</i>, <i>decet</i> et hōrum similibus.</b><br><br><div>1. “Carthāginem,” statuit senātus, “dēlērī oportet”. →</div>
+      <div>“Carthāg<div class="area" data-key="ō"></div>,” statuit senātus, “dēl<div class="area" data-key="enda est"></div>”.</div>
+      <div>2. Sī ōrātor fierī vīs, cōgitātiōnēs clārē exprimere dēbēs. →</div>
+      <div>Sī ōrātor fierī vīs, cōgitātiōn<div class="area" data-key="ēs"></div> clārē <div class="area" data-key="tibi"></div> exprim<div class="area" data-key="endae sunt"></div>.</div>
+      <div>3. Exercitum contrā patriam dūcere nōn licet. →</div>
+      <div>Exercit<div class="area" data-key="us"></div> contrā patriam dūc<div class="area" data-key="endus nōn est"></div>.</div>
+      <div>4. Est liber tam ēgregius ut eum in aliās linguās vertere opus sit.</div>
+      <div>Est lib<div class="area" data-key="er"></div> tam ēgregi<div class="area" data-key="us"></div> ut in aliās linguās vert<div class="area" data-key="endus sit"></div>.</div>
+      <div>5. Necesse erat mūrōs inter bellum dīrutōs dēnuō reficere. →</div>
+      <div>Mūr<div class="area" data-key="ī"></div> inter bellum dīrut<div class="area" data-key="ī"></div> dēnuō refic<div class="area" data-key="iednī erant"></div>.</div>
+      <div>6. “Stulte! Utinam haec nē dīxissēs! Tacēre dēbēbās!” →</div>
+      <div>“Stulte! Utinam haec nē dīxissēs! <div class="area" data-key="erat"></div> t<div class="area" data-key="ibi"></div> tac<div class="area" data-key="endum"></div>.</div>
+      <div>7. Lībertīnus: “Cum servī essēmus, arbitrābāmur nōs tacitē patī dēbēre iniūriās”. →</div>
+      <div>Lībertīnus: “Cum servī essēmus, arbitr<div class="area" data-key="ābāmur"></div> tacitē pat<div class="area" data-key="iendās esse"></div> iniūri<div class="area" data-key="ās"></div>”.</div>
+      <div>8. Māter fīliīs: “Tantae aetātis iam estis ut plōrāre vōs nōn deceat”. →</div>
+      <div>Māter fīliīs: “Tantae aetātis iam <div class="area" data-key="estis"></div> ut plōr<div class="area" data-key="andum"></div> v<div class="area" data-key="ōbīs"></div> nōn <div class="area" data-key="sit"></div>”.</div>
+      <div>9. Nē lībertātem meam āmitterem, pecūniam quam mūtuam sūmpseram reddere dēbuī. →</div>
+      <div>Nē lībertātem āmitterem, pecūni<div class="area" data-key="a"></div> qu<div class="area" data-key="am"></div> mūtu<div class="area" data-key="am"></div> sūm<div class="area" data-key="pseram"></div> <div class="area" data-key="mihi"></div> redd<div class="area" data-key="enda fuit"></div>.</div>
+      <div>10. “Nōlī,” inquit, “illūc redīre! Hanc urbem prō domō putā!” →</div>
+      <div>“Nōn <div class="area" data-key="est"></div> t<div class="area" data-key="ibi"></div> illūc red<div class="area" data-key="eundum"></div>! H<div class="area" data-key="aec"></div> urb<div class="area" data-key="s"></div> <div class="area" data-key="tibi"></div> prō domō put<div class="area" data-key="anda est"></div>!”</div>
+    </div>
+  `
+});
