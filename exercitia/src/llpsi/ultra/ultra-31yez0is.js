d79f4fbd81b1fd2d75d2978e88d746d0fde9f3c5
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2026-05-08 15:38:07 +0300

    Add files via upload

--- exercitia/src/llpsi/ultra/ultra-31yez0is.js
+++ exercitia/src/llpsi/ultra/ultra-31yez0is.js
@@ -5,7 +5,7 @@ $_GET({
       <b>1. Ūtere gerundīvīs prō <i>dēbēre</i>, <i>oportet</i>, <i>decet</i> et hōrum similibus.</b><br><br><div>1. “Carthāginem,” statuit senātus, “dēlērī oportet”. →</div>
       <div>“Carthāg<div class="area" data-key="ō"></div>,” statuit senātus, “dēl<div class="area" data-key="enda est"></div>”.</div>
       <div>2. Sī ōrātor fierī vīs, cōgitātiōnēs clārē exprimere dēbēs. →</div>
-      <div>Sī ōrātor fierī vīs, cōgitātiōn<div class="area" data-key="ēs"></div> clārē <div class="area" data-key="tibi"></div> exprim<div class="area" data-key="endae sunt"></div>.</div>
+      <div>Sī ōrātor fierī vīs, cōgitātiōn<div class="area" data-key="ēs"></div> clārē t<div class="area" data-key="ibi"></div> exprim<div class="area" data-key="endae sunt"></div>.</div>
       <div>3. Exercitum contrā patriam dūcere nōn licet. →</div>
       <div>Exercit<div class="area" data-key="us"></div> contrā patriam dūc<div class="area" data-key="endus nōn est"></div>.</div>
       <div>4. Est liber tam ēgregius ut eum in aliās linguās vertere opus sit.</div>
@@ -19,9 +19,33 @@ $_GET({
       <div>8. Māter fīliīs: “Tantae aetātis iam estis ut plōrāre vōs nōn deceat”. →</div>
       <div>Māter fīliīs: “Tantae aetātis iam <div class="area" data-key="estis"></div> ut plōr<div class="area" data-key="andum"></div> v<div class="area" data-key="ōbīs"></div> nōn <div class="area" data-key="sit"></div>”.</div>
       <div>9. Nē lībertātem meam āmitterem, pecūniam quam mūtuam sūmpseram reddere dēbuī. →</div>
-      <div>Nē lībertātem āmitterem, pecūni<div class="area" data-key="a"></div> qu<div class="area" data-key="am"></div> mūtu<div class="area" data-key="am"></div> sūm<div class="area" data-key="pseram"></div> <div class="area" data-key="mihi"></div> redd<div class="area" data-key="enda fuit"></div>.</div>
+      <div>Nē lībertātem āmitterem, pecūni<div class="area" data-key="a"></div> qu<div class="area" data-key="am"></div> mūtu<div class="area" data-key="am"></div> sūm<div class="area" data-key="pseram"></div> m<div class="area" data-key="ihi"></div> redd<div class="area" data-key="enda fuit"></div>.</div>
       <div>10. “Nōlī,” inquit, “illūc redīre! Hanc urbem prō domō putā!” →</div>
-      <div>“Nōn <div class="area" data-key="est"></div> t<div class="area" data-key="ibi"></div> illūc red<div class="area" data-key="eundum"></div>! H<div class="area" data-key="aec"></div> urb<div class="area" data-key="s"></div> <div class="area" data-key="tibi"></div> prō domō put<div class="area" data-key="anda est"></div>!”</div>
+      <div>“Nōn <div class="area" data-key="est"></div> t<div class="area" data-key="ibi"></div> illūc red<div class="area" data-key="eundum"></div>! H<div class="area" data-key="aec"></div> urb<div class="area" data-key="s"></div> <div class="area" data-key="tibi"></div> prō domō put<div class="area" data-key="anda est"></div>!”<br><br></div>
+    </div>
+    <div class="row">
+      <div><b>2. Ūtere gerundīvīs in locō <i>-ūrus/a/um</i> + <i>esse</i>.</b></div>
+      <div><br></div>
+      <div>1. Partem vīnī maiōrem Flaccus in Galliam vectūrus est.</div>
+      <div>Par<div class="area" data-key="s"></div> vīnī mai<div class="area" data-key="or"></div> Flacc<div class="area" data-key="ō"></div> in Galliam ve<div class="area" data-key="henda est"></div>.</div>
+      <div>2. Pāce prōmissā, uterque exercitus arma relictūrus est.</div>
+      <div>Pāce prōmissā, ut<div class="area" data-key="rī"></div>que exercit<div class="area" data-key="uī"></div> arm<div class="area" data-key="a"></div> re<div class="area" data-key="linquenda sunt"></div>.</div>
+      <div>3. Olīvīs dēnique collēctīs, oleum cōnfectūrae sunt mulierēs.</div>
+      <div>Olīvīs dēnique collēctīs, ole<div class="area" data-key="um"></div> cōn<div class="area" data-key="ficiendum est"></div> mulier<div class="area" data-key="ibus"></div>.</div>
+      <div>4. In senātū: “Classe nostrā partim dēlētā, novās factūrī sumus nāvēs”.</div>
+      <div>In senātū: “Classe nostrā partim dēlētā, nov<div class="area" data-key="ae"></div> fac<div class="area" data-key="iendae sunt"></div> nāv<div class="area" data-key="ēs"></div>”</div>
+      <div>5. Nauta: “Nōnne, ō sodālēs, naufragium passōs servātūrī estis?”</div>
+      <div>Nauta: “Nōnne, ō sodālēs, naufragium pa<div class="area" data-key="ssī"></div> v<div class="area" data-key="ōbīs"></div> serv<div class="area" data-key="andī sunt"></div>?”</div>
+      <div>6. Iūdex: “Vōs, scelerātōs, pūnītūrus sum, et rēctē quidem!”</div>
+      <div>Iūdex: “V<div class="area" data-key="ōs"></div>, scelerāt<div class="area" data-key="ī"></div>, pūn<div class="area" data-key="iendī estis"></div>, et rēctē quidem!”</div>
+      <div>7. “Sī quid malī” inquam “frātrī tuō accidit, nōnne auxilium lātūrus es?”</div>
+      <div>“Sī quid malī” inquam “frāt<div class="area" data-key="rī"></div> tu<div class="area" data-key="ō"></div> accid<div class="area" data-key="it"></div>, nōnne auxili<div class="area" data-key="um"></div> <div class="area" data-key="ferendum est"></div>?”</div>
+      <div>8. Mēdus: “Quod ad mē attinet, ego numquam sum reversūrus in patriam!”</div>
+      <div>Mēdus: “Quod ad mē attin<div class="area" data-key="et"></div>, <div class="area" data-key="mihi"></div> numquam <div class="area" data-key="est"></div> rever<div class="area" data-key="tendum"></div> in patriam!”</div>
+      <div>9. Vetera sacra renovātūrus erat novus rēx.</div>
+      <div>Veter<div class="area" data-key="a"></div> sacr<div class="area" data-key="a"></div> renov<div class="area" data-key="anda erant"></div> nov<div class="area" data-key="ō"></div> rē<div class="area" data-key="gī"></div>.</div>
+      <div>10. Alter cōnsul erat perrēctūrus bellum, alter pācem conciliātūrus.</div>
+      <div>Alt<div class="area" data-key="erī"></div> cōns<div class="area" data-key="ulī"></div> <div class="area" data-key="erat"></div> per<div class="area" data-key="gendum"></div> bell<div class="area" data-key="um"></div>, alt<div class="area" data-key="erī"></div> pā<div class="area" data-key="x"></div> concili<div class="area" data-key="anda"></div>.</div>
     </div>
   `
 });
