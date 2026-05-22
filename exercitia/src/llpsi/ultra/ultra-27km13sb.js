29471ec68d6252bcf07c40eb2fd10bbc2cc51a64
Λεωνίδας <119621011+Le-Marin@users.noreply.github.com>
2026-05-22 22:08:18 +0300

    Add files via upload

---
+++ exercitia/src/llpsi/ultra/ultra-27km13sb.js
@@ -0,0 +1,19 @@
+$_GET({
+  title: 'Ultra 27',
+  html: /*html*/`
+    <div class="row">
+      <div><b>1. Īnfīnītīva &gt; verba coniūnctīvī praesentis.</b></div>
+      <div><br></div>
+      <div>1. “Nūntiā,” inquit, “Rōmānīs deōs ita velle ut Rōma caput orbis terrārum <div class="area" data-key="sit">esse</div>”.</div>
+      <div>2. “Precor, dī bonī,” inquit gladiātor, “ut mē ā morte illā horrendā <div class="area" data-key="servētis">servāre</div>!”</div>
+      <div>3. Viātor: “Rogō, domine, ut mē et sorōrem meam domī tuae <div class="area" data-key="sinās">sinere</div> pernoctāre”.</div>
+      <div>4. Nauta gubernātōrī: “Moneō nē nōs <div class="area" data-key="proficīscāmur">proficīscī</div> crās!”</div>
+      <div>5. Post mediam noctem ad coemētērium īmus ut duo cadāvera <div class="area" data-key="effodiāmus">effodere</div>.</div>
+      <div>6. Dux assentītur, sed eā condiciōne ut omnēs captīvī <div class="area" data-key="reddantur">reddī</div>.</div>
+      <div>7. Spartacus timet nē <div class="area" data-key="abeant">abīre</div> sociī nēve exercitus suus sine eōrum auxiliō <div class="area" data-key="relinquātur">relinquī</div>.</div>
+      <div>8. Lupus in fābulā: “Accēde proprius, ut melius tē <div class="area" data-key="audiam">audīre</div>”.</div>
+      <div>9. Iōsēphus frātribus: “Persuādēte patrī ut in Aegyptum cum omnī familiā <div class="area" data-key="commigret">commigrāre</div>”.</div>
+      <div>10. Lēgātus “Rogō vōs” ait, “ut opem nōbīs <div class="area" data-key="ferātis">ferre</div>”.</div>
+    </div>
+  `
+});
